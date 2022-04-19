ee416158286e5509cb96aa41b7f1c5b704b0557d
openshwprojects <openshwprojects@gmail.com>
2022-04-20 01:20:45 +0200

    rewrote BL0942 driver to send only substantial changes (otherwise we get MQTT overheat)

--- src/driver/drv_bl0942.c
+++ src/driver/drv_bl0942.c
@@ -18,18 +18,56 @@ int raw_unscaled_current;
 int raw_unscaled_power;
 int raw_unscaled_freq;
 
-float realVoltage, realCurrent, realPower;
+int stat_updatesSkipped = 0;
+int stat_updatesSent = 0;
+
+typedef enum {
+	OBK_VOLTAGE,
+	OBK_CURRENT,
+	OBK_POWER,
+	OBK_NUM_MEASUREMENTS,
+};
+
+// Current values 
+float lastReadings[OBK_NUM_MEASUREMENTS];
+// 
+// Variables below are for optimization
+// We can't send a full MQTT update every second.
+// It's too much for Beken, and it's too much for LWIP 2 MQTT library,
+// especially when actively browsing site and using JS app Log Viewer.
+// It even fails to publish with -1 error (can't alloc next packet)
+// So we publish when value changes from certain threshold or when a certain time passes.
+//
+// what are the last values we sent over the MQTT?
+float lastSentValues[OBK_NUM_MEASUREMENTS];
+// how much update frames has passed without sending MQTT update of read values?
+int noChangeFrames[OBK_NUM_MEASUREMENTS];
+// how much of value have to change in order to be send over MQTT again?
+int changeSendThresholds[OBK_NUM_MEASUREMENTS] = { 
+	0.25f, // voltage - OBK_VOLTAGE
+	0.002f, // current - OBK_CURRENT
+	0.25f, // power - OBK_POWER
+};
+// how are they called in MQTT
+const char *mqttNames[OBK_NUM_MEASUREMENTS] = { 
+	"voltage",
+	"current",
+	"power"
+};
+
+int changeSendAlwaysFrames = 60;
+
 
 #define BL0942_BAUD_RATE 4800
 
 #define BL0942_READ_COMMAND 0x58
+
 
 int BL0942_TryToGetNextBL0942Packet() {
 	int cs;
 	int i;
 	int c_garbage_consumed = 0;
 	byte a;
-	float newPower, newCurrent, newVoltage;
 	byte checksum;
 	int BL0942_PACKET_LEN = 23;
 
@@ -96,29 +134,36 @@ int BL0942_TryToGetNextBL0942Packet() {
 	//addLogAdv(LOG_INFO, LOG_FEATURE_BL0942,"Unscaled current %d, voltage %d, power %d, freq %d\n", raw_unscaled_current, raw_unscaled_voltage,raw_unscaled_power,raw_unscaled_freq);
 
 	// those are final values, like 230V
-	newPower = (raw_unscaled_power / BL0942_PREF);
-	if(newPower!=realPower){
-		realPower = newPower;
-		MQTT_PublishMain_StringFloat("power",newPower);
-	}
-	newVoltage = (raw_unscaled_voltage / BL0942_UREF);
-	if(newVoltage!=realVoltage){
-		realVoltage = newVoltage;
-		MQTT_PublishMain_StringFloat("voltage",newVoltage);
-	}
-	newCurrent = (raw_unscaled_current / BL0942_IREF);
-	if(newCurrent!=realCurrent){
-		realCurrent = newCurrent;
-		MQTT_PublishMain_StringFloat("current",realCurrent);
+	lastReadings[OBK_POWER] = (raw_unscaled_power / BL0942_PREF);
+	lastReadings[OBK_VOLTAGE] = (raw_unscaled_voltage / BL0942_UREF);
+	lastReadings[OBK_CURRENT] = (raw_unscaled_current / BL0942_IREF);
+
+	for(i = 0; i < OBK_NUM_MEASUREMENTS; i++) {
+		// send update only if there was a big change or if certain time has passed
+		if(
+			(abs(lastSentValues[i]-lastReadings[i]) > changeSendThresholds[i])
+			||
+			noChangeFrames[i] > changeSendAlwaysFrames
+			){
+			noChangeFrames[i] = 0;
+			lastSentValues[i] = lastReadings[i];
+			MQTT_PublishMain_StringFloat(mqttNames[i],lastReadings[i]);
+			stat_updatesSent++;
+		} else {
+			// no change frame
+			noChangeFrames[i]++;
+			stat_updatesSkipped++;
+		}
 	}
 
+#if 0
 	{
 		char res[128];
 		// V=245.107925,I=109.921143,P=0.035618
-		sprintf(res,"V=%f,I=%f,P=%f\n",realVoltage,realCurrent,realPower);
+		sprintf(res,"V=%f,I=%f,P=%f\n",lastReadings[OBK_VOLTAGE],lastReadings[OBK_CURRENT],lastReadings[OBK_POWER]);
 		addLogAdv(LOG_INFO, LOG_FEATURE_BL0942,res );
-
 	}
+#endif
 
 	UART_ConsumeBytes(BL0942_PACKET_LEN);
 	
@@ -207,6 +252,12 @@ int BL0942_CurrentSet(const void *context, const char *cmd, const char *args) {
 	return 0;
 }
 void BL0942_Init() {
+	int i;
+
+	for(i = 0; i < OBK_NUM_MEASUREMENTS; i++) {
+		noChangeFrames[i] = 0;
+		lastReadings[i] = 0;
+	}
 
 	UART_InitUART(BL0942_BAUD_RATE);
 	UART_InitReceiveRingBuffer(256);
@@ -220,7 +271,7 @@ void BL0942_Init() {
 void BL0942_RunFrame() {
 	int len;
 
-	addLogAdv(LOG_INFO, LOG_FEATURE_BL0942,"UART buffer size %i\n", UART_GetDataSize());
+	//addLogAdv(LOG_INFO, LOG_FEATURE_BL0942,"UART buffer size %i\n", UART_GetDataSize());
 
 	len = BL0942_TryToGetNextBL0942Packet();
 	if(len > 0) {
@@ -231,7 +282,9 @@ void BL0942_RunFrame() {
 }
 void BL0942_AppendInformationToHTTPIndexPage(http_request_t *request) {
 	char tmp[128];
-	sprintf(tmp, "<h2>BL0942 Voltage=%f, Current=%f, Power=%f</h2>",realVoltage,realCurrent, realPower);
+	sprintf(tmp, "<h2>BL0942 Voltage=%f, Current=%f, Power=%f (changes sent %i, skipped %i)</h2>",
+		lastReadings[OBK_VOLTAGE],lastReadings[OBK_CURRENT], lastReadings[OBK_POWER],
+		stat_updatesSent, stat_updatesSkipped);
     hprintf128(request,tmp);
 
 }
