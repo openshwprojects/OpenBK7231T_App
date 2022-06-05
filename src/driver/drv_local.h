3042cdfa856e72ecda7b221f05b28745dbaa3e9f
openshwprojects <openshwprojects@gmail.com>
2022-06-05 10:07:57 +0200

    fx p2

--- src/driver/drv_local.h
+++ src/driver/drv_local.h
@@ -3,14 +3,14 @@
 
 void BL0942_Init();
 void BL0942_RunFrame();
-void BL0942_AppendInformationToHTTPIndexPage(http_request_t *request);
 
 void BL0937_Init();
 void BL0937_RunFrame();
-void BL0937_AppendInformationToHTTPIndexPage(http_request_t *request);
 
 void DRV_DGR_Init();
 void DRV_DGR_RunFrame();
 
 void BL_ProcessUpdate(float voltage, float current, float power);
+void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request);
+
 
