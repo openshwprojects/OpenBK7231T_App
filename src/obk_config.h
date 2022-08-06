02c69cdf3544aab9022ffc75add1064ce060b355
Indu Prakash <leolite1@gmail.com>
2022-08-06 11:55:10 +0000

    Fixed duplicate PLATFORM_XR809 and formatted

--- src/obk_config.h
+++ src/obk_config.h
@@ -2,35 +2,31 @@
 // specify which parts of the app we wish to be active
 //
 #ifndef OBK_CONFIG_H
-#define OBK_CONFIG_H
+	#define OBK_CONFIG_H
 
-#if PLATFORM_XR809
+	#if PLATFORM_XR809
+		///#define DEBUG_USE_SIMPLE_LOGGER
+		#define OBK_DISABLE_ALL_DRIVERS 1
 
+	#elif PLATFORM_W800
 
-#elif PLATFORM_W800
+		#define OBK_DISABLE_ALL_DRIVERS 1
+		
+	#elif WINDOWS
 
-#define OBK_DISABLE_ALL_DRIVERS 1
-#elif WINDOWS
-
-#elif PLATFORM_BL602
-///#define DEBUG_USE_SIMPLE_LOGGER
-#define OBK_DISABLE_ALL_DRIVERS 1
-
-
-#elif PLATFORM_XR809
-///#define DEBUG_USE_SIMPLE_LOGGER
-#define OBK_DISABLE_ALL_DRIVERS 1
-
-#else
-
-// comment out to remove component
-
-// comment out to remove littlefs
-#define BK_LITTLEFS
-
-// add further app wide defined here, and used them to control build inclusion.
-
-#endif
+	#elif PLATFORM_BL602
+		///#define DEBUG_USE_SIMPLE_LOGGER
+		#define OBK_DISABLE_ALL_DRIVERS 1
 
 
+	#else
+
+		// comment out to remove component
+
+		// comment out to remove littlefs
+		#define BK_LITTLEFS
+
+		// add further app wide defined here, and used them to control build inclusion.
+
+	#endif
 #endif
