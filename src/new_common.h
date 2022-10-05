4997b15c75eb85dc27a87f49035f4f946dcccccd
refuhr <moritz.ruffer@gmail.com>
2022-10-05 11:20:59 +0200

    added reference to function

--- src/new_common.h
+++ src/new_common.h
@@ -291,6 +291,6 @@ int PingWatchDog_GetTotalReceived();
 int LWIP_GetMaxSockets();
 int LWIP_GetActiveSockets();
 
-// linear mapping function
+// linear mapping function --> https://www.arduino.cc/reference/en/language/functions/math/map/
 
 #define MAP(x, in_min, in_max, out_min, out_max) (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
