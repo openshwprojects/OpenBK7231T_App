695bee41c24ef84c27de821486f13f99716e4af9
Indu Prakash <leolite1@gmail.com>
2022-10-23 10:29:18 -0500

    Formatted

--- src/driver/drv_public.h
+++ src/driver/drv_public.h
@@ -10,31 +10,31 @@ enum {
 };
 
 enum {
-    OBK_CONSUMPTION_TOTAL = OBK_NUM_MEASUREMENTS,
-    OBK_CONSUMPTION_LAST_HOUR,
-    OBK_CONSUMPTION_STATS,
-    OBK_NUM_EMUNS_MAX
+	OBK_CONSUMPTION_TOTAL = OBK_NUM_MEASUREMENTS,
+	OBK_CONSUMPTION_LAST_HOUR,
+	OBK_CONSUMPTION_STATS,
+	OBK_NUM_EMUNS_MAX
 };
 
 #define OBK_NUM_COUNTERS            (OBK_NUM_EMUNS_MAX-OBK_NUM_MEASUREMENTS)
 #define OBK_NUM_SENSOR_COUNT         OBK_NUM_EMUNS_MAX
 
 // MQTT names of sensors (voltage, current, power)
-extern const char *sensor_mqttNames[];
-extern const char *counter_mqttNames[];
-extern const char *counter_devClasses[];
+extern const char* sensor_mqttNames[];
+extern const char* counter_mqttNames[];
+extern const char* counter_devClasses[];
 
 void DRV_Generic_Init();
-void DRV_AppendInformationToHTTPIndexPage(http_request_t *request);
+void DRV_AppendInformationToHTTPIndexPage(http_request_t* request);
 void DRV_OnEverySecond();
 void DRV_RunQuickTick();
-void DRV_StartDriver(const char *name);
-void DRV_StopDriver(const char *name);
-bool DRV_IsRunning(const char *name);
-void DRV_OnChannelChanged(int channel,int iVal);
-void SM2135_Write(byte *rgbcw);
-void BP5758D_Write(byte *rgbcw);
-void BP1658CJ_Write(byte *rgbcw);
+void DRV_StartDriver(const char* name);
+void DRV_StopDriver(const char* name);
+bool DRV_IsRunning(const char* name);
+void DRV_OnChannelChanged(int channel, int iVal);
+void SM2135_Write(byte* rgbcw);
+void BP5758D_Write(byte* rgbcw);
+void BP1658CJ_Write(byte* rgbcw);
 void DRV_DGR_OnLedDimmerChange(int iVal);
 void DRV_DGR_OnLedEnableAllChange(int iVal);
 
