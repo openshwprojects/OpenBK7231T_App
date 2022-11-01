af9adec1427744453522c082b31138142f4357ea
btsimonh <simon@yellaumbrella.tv>
2022-11-01 21:16:39 +0000

    keep a list of obk SSDP devices, and make available on /obkdevicelist as a JSON array.

--- src/driver/drv_ssdp.c
+++ src/driver/drv_ssdp.c
@@ -50,6 +50,68 @@ static char *http_message = NULL;
 int http_message_len = 0;
 
 
+#define MAX_OBK_DEVICES 40
+#define OBK_DEVICE_TIMEOUT 60
+
+typedef struct OBK_DEVICE_tag{
+    uint32_t ip;
+    int timeout; // seconds
+} OBK_DEVICE;
+
+OBK_DEVICE obkDevices[MAX_OBK_DEVICES];
+
+static void obkDeviceTick(uint32_t ip){
+    int i;
+    for (i = 0; i < MAX_OBK_DEVICES; i++){
+        if (obkDevices[i].ip == ip){
+            obkDevices[i].timeout = OBK_DEVICE_TIMEOUT;
+            addLogAdv(LOG_EXTRADEBUG, LOG_FEATURE_HTTP,"SSDP obk device still present 0x%08x",ip);
+            break;
+        }
+    }
+
+    if (i == MAX_OBK_DEVICES){
+        for (i = 0; i < MAX_OBK_DEVICES; i++){
+            if (obkDevices[i].ip == 0){
+                obkDevices[i].ip = ip;
+                obkDevices[i].timeout = OBK_DEVICE_TIMEOUT;
+                addLogAdv(LOG_EXTRADEBUG, LOG_FEATURE_HTTP,"SSDP new obk device 0x%08x",ip);
+                break;
+            }
+        }
+    }
+}
+
+static void obkDeviceList(){
+    for (int i = 0; i < MAX_OBK_DEVICES; i++){
+        if (obkDevices[i].ip != 0){
+            addLogAdv(LOG_EXTRADEBUG, LOG_FEATURE_HTTP,"obk device 0x%08x", obkDevices[i].ip);
+        }
+    }
+}
+
+
+static int http_rest_get_devicelist(http_request_t* request) {
+	http_setup(request, httpMimeTypeJson);
+	hprintf255(request, "[");
+    int count = 0;
+    for (int i = 0; i < MAX_OBK_DEVICES; i++){
+        if (obkDevices[i].ip != 0){
+            if (count) hprintf255(request,",");
+            hprintf255(request,"{\"ip\":\"%d.%d.%d.%d\"}", 
+                obkDevices[i].ip & 0xff,
+                (obkDevices[i].ip & 0xff00)>>8,
+                (obkDevices[i].ip & 0xff0000) >> 16,
+                (obkDevices[i].ip & 0xff000000) >> 24
+                );
+            count++;
+        }
+    }
+	hprintf255(request, "]\n");
+	poststr(request, NULL);
+	return 0;
+}
+
 ///////////////////////////////
 // private functions, only used by the public functions...
 
@@ -200,9 +262,11 @@ static void DRV_SSDP_Send_Advert_To(struct sockaddr_in *addr) {
 
 
 
-
+// we could add a SERVER: entry so we could recognise our Devices
+// without making the HTTP call....
 static const char notify_template[] = 
 "NOTIFY * HTTP/1.1\r\n" \
+"SERVER: OpenBk\r\n" \
 "HOST: 239.255.255.250:1900\r\n" \
 "CACHE-CONTROL: max-age=1800\r\n" \
 "LOCATION: http://%s:80/ssdp.xml'\r\n" \
@@ -211,6 +275,7 @@ static const char notify_template[] =
 "USN: uuid:%s::upnp:rootdevice\r\n" \
 "\r\n\r\n" \
 ;
+
 
 
 
@@ -321,12 +386,19 @@ static int DRV_SSDP_Service_Http(http_request_t* request){
 // end private functions
 ///////////////////////////////
 
+static int Cmd_obkDeviceList(const void *context, const char *cmd, const char *args, int cmdFlags){
+    obkDeviceList();
+    return 1;
+}
 
 ///////////////////////////////////////////////
 // public functions, only used in drv_main
 
+
 void DRV_SSDP_Init()
 {
+    memset(obkDevices, 0, sizeof(obkDevices));
+
     addLogAdv(LOG_INFO, LOG_FEATURE_HTTP,"DRV_SSDP_Init");
     // like "e427ce1a-3e80-43d0-ad6f-89ec42e46363";
     sprintf(g_ssdp_uuid, "%08x-%04x-%04x-%04x-%04x%08x",
@@ -340,10 +412,13 @@ void DRV_SSDP_Init()
 
 	DRV_SSDP_CreateSocket_Receive();
     HTTP_RegisterCallback("/ssdp.xml", HTTP_GET, DRV_SSDP_Service_Http);
-    //CMD_RegisterCommand("SSDPNotify", "", CMD_SSDP_Notify, "qqq", NULL);
+    CMD_RegisterCommand("obkDeviceList", "", Cmd_obkDeviceList, "qqq", NULL);
+
+    HTTP_RegisterCallback("/obkdevicelist", HTTP_GET, http_rest_get_devicelist);
 
     DRV_SSDP_Active = 1;
 }
+
 
 void DRV_SSDP_RunEverySecond() {
     ssdp_timercount++;
@@ -351,6 +426,15 @@ void DRV_SSDP_RunEverySecond() {
         // multicast a notify
         DRV_SSDP_Send_Notify();
         ssdp_timercount = 0;
+    }
+
+    for (int i = 0; i < MAX_OBK_DEVICES; i++){
+        if (obkDevices[i].timeout){
+            obkDevices[i].timeout --;
+            if (!obkDevices[i].timeout){
+                obkDevices[i].ip = 0;
+            }
+        }
     }
 }
 
@@ -405,6 +489,25 @@ void DRV_SSDP_RunQuickTick() {
         addLogAdv(LOG_EXTRADEBUG, LOG_FEATURE_HTTP,"Is MSEARCH - responding");
         DRV_SSDP_Send_Advert_To(&addr);
     }
+
+    // our NOTIFTY like:
+    //"NOTIFY * HTTP/1.1\r\n" 
+    //"SERVER: OpenBk\r\n" 
+    if (!strncmp(udp_msgbuf, "NOTIFY", 6)){
+        const char *p = udp_msgbuf;
+        while (*p && *p != '\n'){
+            p++;
+        }
+        if (*p == '\n'){
+            p++;
+            if (!strncmp(p, "SERVER: OpenBk", 14)){
+                addLogAdv(LOG_EXTRADEBUG, LOG_FEATURE_HTTP,"NOTIFY from a peer device");
+                // add the device to the device list, or set timeout to 0
+                obkDeviceTick(*(uint32_t *)(&((struct sockaddr_in *)&addr)->sin_addr));
+            }
+        }
+    }
+
 }
 
 
