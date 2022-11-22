bb76bfbacf744986a939f775dc469a35be08e601
btsimonh <simon@yellaumbrella.tv>
2022-11-22 08:50:21 +0000

    Change mqtt_processing to be driven by xTimerPendFunction if available. Make it available by setting #define INCLUDE_xTimerPendFunctionCall 1 in FreeRTOSConfig.h

--- src/hal/bk7231/hal_main_bk7231.c
+++ src/hal/bk7231/hal_main_bk7231.c
@@ -8,8 +8,8 @@
 
 // main timer tick every 1s
 beken_timer_t g_main_timer_1s;
-beken2_timer_t g_mqtt_timer_oneshot;
-
+// from rtos_pub.c
+extern uint32_t  ms_to_tick_ratio;
 
 #if PLATFORM_BK7231T
 
@@ -27,11 +27,66 @@ void MQTT_process_received_timer(void *a, void*b){
   MQTT_process_received();
 }
 
+//#undef INCLUDE_xTimerPendFunctionCall
+
+#if ( INCLUDE_xTimerPendFunctionCall == 1 )
+// we use this if FreeRTOS is configured for xTimerPendFunctionCall
+// with #define INCLUDE_xTimerPendFunctionCall 1 in FreeRTOSConfig.h
+
+
+// our timer thread callback - extend for multiple options,
+// e.g. pins called back from GPIO level interrupt.
+#define MQTT_PROCESS 1
+
+void OBK_timer_cb(void *a, uint32_t cmd){
+  switch (cmd){
+    case MQTT_PROCESS:
+      MQTT_process_received();
+      break;
+  }
+}
+
+// note, if called from an ISR, delay is ignored
+OSStatus OBK_rtos_callback_in_timer_thread( PendedFunction_t xFunctionToPend, void *pvParameter1, uint32_t ulParameter2, uint32_t delay_ms)
+{
+  signed portBASE_TYPE result;
+
+  if ( platform_is_in_interrupt_context() == RTOS_SUCCESS ) {
+    signed portBASE_TYPE xHigherPriorityTaskWoken = pdFALSE;
+    result = xTimerPendFunctionCallFromISR( xFunctionToPend, pvParameter1, ulParameter2, &xHigherPriorityTaskWoken );
+    portEND_SWITCHING_ISR( xHigherPriorityTaskWoken );
+  } 
+	else
+	{
+    result = xTimerPendFunctionCall( xFunctionToPend, pvParameter1, ulParameter2, ( delay_ms / ms_to_tick_ratio ));
+	}
+
+  if ( result != pdPASS )
+  {
+      return kGeneralErr;
+  }
+
+  return kNoErr;
+}
+
+
+void MQTT_TriggerRead(){
+  OSStatus err;
+  err = OBK_rtos_callback_in_timer_thread( OBK_timer_cb, NULL, MQTT_PROCESS, 1);
+}
+	#warning xTimerPendFunctionCall() function is available.
+#else
+	#warning xTimerPendFunctionCall() function not available.
+
+
+// we use this only if FreeRTOS is not configured for xTimerPendFunctionCall
+// with #define INCLUDE_xTimerPendFunctionCall 1 in FreeRTOSConfig.h
+beken2_timer_t g_mqtt_timer_oneshot;
 void MQTT_TriggerRead(){
   OSStatus err;
   err = rtos_start_oneshot_timer(&g_mqtt_timer_oneshot);
 }
-
+#endif
 
 void user_main(void)
 {
@@ -49,12 +104,16 @@ void user_main(void)
   ASSERT(kNoErr == err);
 	ADDLOGF_DEBUG("started timer\r\n");
 
+#if ( INCLUDE_xTimerPendFunctionCall == 1 )
+  // timer not required
+#else
   // initialise a one-shot timer, triggered by MQTT_TriggerRead()
   err = rtos_init_oneshot_timer(&g_mqtt_timer_oneshot,
                         1,
                         MQTT_process_received_timer,
                         (void *)0,
                         (void *)0);
+#endif
 }
 
 #if PLATFORM_BK7231N
