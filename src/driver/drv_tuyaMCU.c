676fa0a3eefcefa9a300d84cafad6e980a0a212d
openshwprojects <openshwprojects@gmail.com>
2022-12-14 18:57:27 +0100

    TuyaMCU will now reply to 0x24 packet with current date - this is needed for TH06 humidity & temperature calendar clock with LCD

--- src/driver/drv_tuyaMCU.c
+++ src/driver/drv_tuyaMCU.c
@@ -1130,16 +1130,11 @@ void TuyaMCU_ProcessIncoming(const byte *data, int len) {
 
 			}
 			break;
-		case 0x06:
-			// See: https://www.elektroda.com/rtvforum/viewtopic.php?p=20319441#20319441
-			// UPDATE: not needed, it is send from us to device
-			//if (version == 0) {
-			//	// This packet includes NO DateTime, ONLY RealTimeDataStorage
-			//	TuyaMCU_V0_ParseRealTimeWithRecordStorage(data + 6, len - 6, false);
-			//}
-			//else {
-
-			//}
+		case 0x24:
+			// This is send by TH06
+			// Info:TuyaMCU:TUYAMCU received: 55 AA 03 24 00 00 26     
+			addLogAdv(LOG_INFO, LOG_FEATURE_TUYAMCU,"TuyaMCU_ProcessIncoming: (test for TH06 calendar) received 0x24, so sending back time");
+			TuyaMCU_Send_SetTime(TuyaMCU_Get_NTP_Time());
 			break;
         default:
             addLogAdv(LOG_INFO, LOG_FEATURE_TUYAMCU,"TuyaMCU_ProcessIncoming: unhandled type %i",cmd);
