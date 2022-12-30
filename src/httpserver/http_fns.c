cd6229887d610dd4bfdae666cc6124f9f7927ee2
openshwprojects <openshwprojects@gmail.com>
2022-12-30 15:49:27 +0100

    warning fix

--- src/httpserver/http_fns.c
+++ src/httpserver/http_fns.c
@@ -2142,6 +2142,7 @@ int http_tasmota_json_status_STS(http_request_t* request) {
 	hprintf255(request, "\"Downtime\":\"0T06:13:34\"");
 	hprintf255(request, "}"); // close WiFi
 	hprintf255(request, "}");
+	return 0;
 }
 /*
 {"Status":{"Module":0,"DeviceName":"Tasmota","FriendlyName":["Tasmota"],"Topic":"tasmota_48E7F3","ButtonTopic":"0","Power":1,"PowerOnState":3,"LedState":1,"LedMask":"FFFF","SaveData":1,"SaveState":1,"SwitchTopic":"0","SwitchMode":[0,0,0,0,0,0,0,0],"ButtonRetain":0,"SwitchRetain":0,"SensorRetain":0,"PowerRetain":0,"InfoRetain":0,"StateRetain":0}}
