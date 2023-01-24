8dcd795d440fd26a8420016f58d31338b155563a
openshwprojects <openshwprojects@gmail.com>
2023-01-24 02:19:25 +0100

    third script loop self test

--- src/selftest/selftest_script.c
+++ src/selftest/selftest_script.c
@@ -19,6 +19,15 @@ const char *demo_loop_2 =
 "    if $CH20>0 then goto again\r\n"
 "    setChannel 21 123\r\n";
 
+const char *demo_loop_3 =
+"setChannel 20 15\r\n"
+"setChannel 0 1\r\n"
+"again:\r\n"
+"    addChannel 20 -1\r\n"
+"    delay_s 1\r\n"
+"    if $CH20>0 then goto again\r\n"
+"    setChannel 0 0\r\n";
+
 void Test_Scripting_Loop1() {
 	char buffer[64];
 
@@ -66,9 +75,43 @@ void Test_Scripting_Loop2() {
 	SELFTEST_ASSERT_CHANNEL(22, 3);
 	//system("pause");
 }
+void Test_Scripting_Loop3() {
+	char buffer[64];
+
+	// reset whole device
+	SIM_ClearOBK();
+	CMD_ExecuteCommand("lfsformat", 0);
+
+	// put file in LittleFS
+	Test_FakeHTTPClientPacket_POST("api/lfs/demo_loop_3.txt", demo_loop_3);
+	// get this file 
+	Test_FakeHTTPClientPacket_GET("api/lfs/demo_loop_3.txt");
+	SELFTEST_ASSERT_HTML_REPLY(demo_loop_3);
+
+	CMD_ExecuteCommand("startScript demo_loop_3.txt", 0);
+	SELFTEST_ASSERT_INTEGER(CMD_GetCountActiveScriptThreads(), 1);
+	// should execute and quit within few frames
+	// (we have a limit of scripting executed per frame)
+	for (int i = 0; i < 14; i++) {
+		Sim_RunSeconds(1, false);
+		SELFTEST_ASSERT_INTEGER(CMD_GetCountActiveScriptThreads(), 1);
+		SELFTEST_ASSERT_CHANNEL(0, 1);
+	}
+	for (int i = 0; i < 2; i++) {
+		Sim_RunSeconds(1, false);
+	}
+	for (int i = 0; i < 3; i++) {
+		Sim_RunSeconds(1, false);
+		SELFTEST_ASSERT_INTEGER(CMD_GetCountActiveScriptThreads(), 0);
+	}
+	SELFTEST_ASSERT_CHANNEL(0, 0);
+	SELFTEST_ASSERT_CHANNEL(20, 0);
+	//system("pause");
+}
 void Test_Scripting() {
 	Test_Scripting_Loop1();
 	Test_Scripting_Loop2();
+	Test_Scripting_Loop3();
 }
 
 #endif
