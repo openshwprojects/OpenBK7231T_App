9458ce595648ee336199b16953a0ee3967954c2e
openshwprojects <openshwprojects@gmail.com>
2023-01-24 04:32:18 +0100

    warning fix

--- src/new_common.h
+++ src/new_common.h
@@ -429,6 +429,7 @@ extern int g_startPingWatchDogAfter;
 typedef int(*jsonCb_t)(void *userData, const char *fmt, ...);
 int JSON_ProcessCommandReply(const char *cmd, const char *args, void *request, jsonCb_t printer, int flags);
 void ScheduleDriverStart(const char *name, int delay);
+bool isWhiteSpace(char ch);
 
 
 #endif /* __NEW_COMMON_H__ */
