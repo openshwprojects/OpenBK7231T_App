1ec8c81c86ce7d16eab1ee5ac2814098fc2c7954
openshwprojects <openshwprojects@gmail.com>
2023-02-02 09:56:53 +0100

    lfs commands for numbers, format fix, error print on lfs write fail, add debouncer stub, int lfs write self test

---
+++ src/driver/drv_debouncer.c
@@ -0,0 +1,17 @@
+#include "../new_common.h"
+#include "../new_pins.h"
+#include "../new_cfg.h"
+// Commands register, execution API and cmd tokenizer
+#include "../cmnds/cmd_public.h"
+#include "../mqtt/new_mqtt.h"
+#include "../logging/logging.h"
+#include "../hal/hal_pins.h"
+#include "../hal/hal_wifi.h"
+#include "drv_public.h"
+#include "drv_local.h"
+#include "drv_ssdp.h"
+#include "../httpserver/new_http.h"
+
+int sourceChannel;
+int currentValue;
+int margin;
