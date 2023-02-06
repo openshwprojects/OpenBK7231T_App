fdb3afd7052c4192ed9f37bcf6bf5cc064d386c9
Rémi Serriere <Remi.Serriere@getyooz.com>
2023-02-06 17:41:41 +0100

    Add a flagged option to apply channel type multiplier (if any) when publishing MQTT value

--- src/mqtt/new_mqtt.c
+++ src/mqtt/new_mqtt.c
@@ -1210,19 +1210,63 @@ OBK_Publish_Result MQTT_PublishMain_StringString(const char* sChannel, const cha
 	return MQTT_PublishMain(mqtt_client, sChannel, valueStr, flags, true);
 
 }
+float MQTT_MultiplierConfiguredOnChannel(int channel, int iVal) {
+	// Init float variable
+	float fVal = 0;
+
+	// Checking if flag is set
+	if (CFG_HasFlag(OBK_FLAG_PUBLISH_MULTIPLIED_VALUES)) {
+		switch (CHANNEL_GetType(channel))
+		{
+		case ChType_Humidity_div10:
+		case ChType_Temperature_div10:
+		case ChType_Voltage_div10:
+			fVal = (float)iVal / 10;
+			break;
+		case ChType_Frequency_div100:
+		case ChType_Current_div100:
+		case ChType_EnergyTotal_kWh_div100:
+			fVal = (float)iVal / 100;
+			break;
+		case ChType_PowerFactor_div1000:
+		case ChType_EnergyTotal_kWh_div1000:
+		case ChType_EnergyExport_kWh_div1000:
+		case ChType_EnergyToday_kWh_div1000:
+		case ChType_Current_div1000:
+			fVal = (float)iVal / 1000;
+			break;
+		default:
+			break;
+		}
+	}
+
+	// Returning float value. If it is zero then we dod nothing, or the value is indeed zero so it doesn't matter if we send a float or int.
+	return fVal;
+}
 OBK_Publish_Result MQTT_ChannelChangeCallback(int channel, int iVal)
 {
 	char channelNameStr[8];
 	char valueStr[16];
 	int flags;
 
-	flags = 0;
-	addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "Channel has changed! Publishing %i to channel %i \n", iVal, channel);
+	// Getting float value if any, and converting if required
+	float fVal = MQTT_MultiplierConfiguredOnChannel(channel, iVal);
+	if (fVal == 0) {
+		// Integer value
+		addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "Channel has changed! Publishing %i to channel %i \n", iVal, channel);
+		sprintf(valueStr, "%i", iVal);
+	}
+	else {
+		// Float value
+		addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "Channel has changed! Publishing %f to channel %i \n", fVal, channel);
+		sprintf(valueStr, "%f", fVal);
+	}
 
+	flags = 0;
 	MQTT_BroadcastTasmotaTeleSTATE();
 
+	// String from channel number
 	sprintf(channelNameStr, "%i", channel);
-	sprintf(valueStr, "%i", iVal);
 
 	// This will set RETAIN flag for all channels that are used for RELAY
 	if (CFG_HasFlag(OBK_FLAG_MQTT_RETAIN_POWER_CHANNELS)) {
@@ -1241,10 +1285,21 @@ OBK_Publish_Result MQTT_ChannelPublish(int channel, int flags)
 
 	iValue = CHANNEL_Get(channel);
 
-	addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "Forced channel publish! Publishing val %i to %i", iValue, channel);
+	// Getting float value if any, and converting if required
+	float fVal = MQTT_MultiplierConfiguredOnChannel(channel, iValue);
+	if (fVal == 0) {
+		// Integer value
+		addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "Forced channel publish! Publishing val %i to %i", iValue, channel);
+		sprintf(valueStr, "%i", iValue);
+	}
+	else {
+		// Float value
+		addLogAdv(LOG_INFO, LOG_FEATURE_MQTT, "Forced channel publish! Publishing val %f to %i", fVal, channel);
+		sprintf(valueStr, "%f", fVal);
+	}
 
+	// String from channel number
 	sprintf(channelNameStr, "%i", channel);
-	sprintf(valueStr, "%i", iValue);
 
 	// This will set RETAIN flag for all channels that are used for RELAY
 	if (CFG_HasFlag(OBK_FLAG_MQTT_RETAIN_POWER_CHANNELS)) {
