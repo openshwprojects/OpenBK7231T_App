86744107391bd4e852010cc8ff6bc3776369e36e
dheenhasty <dheenhasty@gmail.com>
2023-02-08 00:05:22 +0100

    Error on Hal reference

--- src/cmnds/cmd_main.c
+++ src/cmnds/cmd_main.c
@@ -127,7 +127,7 @@ static commandResult_t CMD_BATT_Meas(const void* context, const char* cmd, const
 		channel_rel = g_cfg.pins.channels[g_pin_rel];
 	}
 	extern void HAL_ADC_Init(int g_pin_adc);
-	extern void HAL_ADC_Read(int g_pin_adc);
+	extern int HAL_ADC_Read(int g_pin_adc);
 	HAL_ADC_Init(g_pin_adc);
 	batt_value = HAL_ADC_Read(g_pin_adc);
 	if (batt_value < 1024) {
