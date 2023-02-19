2ffea9d478d36f2ca8dd3983677e33dfcce79343
openshwprojects <openshwprojects@gmail.com>
2023-02-19 09:42:15 +0100

    bl602 fix

--- src/cmnds/cmd_newLEDDriver.c
+++ src/cmnds/cmd_newLEDDriver.c
@@ -590,21 +590,6 @@ OBK_Publish_Result sendColorChange() {
 
 	return MQTT_PublishMain_StringString_DeDuped(DEDUP_LED_BASECOLOR_RGB,DEDUP_EXPIRE_TIME,"led_basecolor_rgb",s, 0);
 }
-void LED_SetBaseColorByIndex(int i, float f, bool bApply) {
-	if (i < 0 || i >= 5)
-		return;
-	baseColors[i] = f;
-	if (bApply) {
-		if (CFG_HasFlag(OBK_FLAG_LED_AUTOENABLE_ON_ANY_ACTION)) {
-			LED_SetEnableAll(true);
-		}
-
-		// set g_lightMode
-		SET_LightMode(Light_RGB);
-		sendColorChange();
-		apply_smart_light();
-	}
-}
 void LED_GetBaseColorString(char * s) {
 	byte c[3];
 
@@ -662,6 +647,21 @@ void SET_LightMode(int newMode) {
 			GetLightModeStr(g_lightMode),
 			GetLightModeStr(newMode));
 		g_lightMode = newMode;
+	}
+}
+void LED_SetBaseColorByIndex(int i, float f, bool bApply) {
+	if (i < 0 || i >= 5)
+		return;
+	baseColors[i] = f;
+	if (bApply) {
+		if (CFG_HasFlag(OBK_FLAG_LED_AUTOENABLE_ON_ANY_ACTION)) {
+			LED_SetEnableAll(true);
+		}
+
+		// set g_lightMode
+		SET_LightMode(Light_RGB);
+		sendColorChange();
+		apply_smart_light();
 	}
 }
 OBK_Publish_Result LED_SendCurrentLightMode() {
