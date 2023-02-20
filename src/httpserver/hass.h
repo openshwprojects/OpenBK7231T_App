d7a99e2d18cbadcd7ef9c719633b8a2e1fbb4eae
dheenhasty <dheenhasty@gmail.com>
2023-02-20 18:35:18 +0100

    Adding Battery driver discovery Adding Flag for sensor avaibility publish

--- src/httpserver/hass.h
+++ src/httpserver/hass.h
@@ -33,6 +33,12 @@ typedef enum {
 	TEMPERATURE_SENSOR,
 	/// @brief Humidity sensor
 	HUMIDITY_SENSOR
+
+	/// @brief Battery level sensor in perc
+	BATTERY_SENSOR,
+	/// @brief Battery votage sensor in mV
+	BATTERY_VOLTAGE_SENSOR
+
 } ENTITY_TYPE;
 
 //unique_id is defined in hass_populate_unique_id and is based on CFG_GetDeviceName() whose size is CGF_DEVICE_NAME_SIZE.
