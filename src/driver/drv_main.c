941201f382104540e678f0da090b11ef2f793e1a
openshwprojects <openshwprojects@gmail.com>
2023-03-11 17:29:42 +0100

    power measuring /battery fix from PR?

--- src/driver/drv_main.c
+++ src/driver/drv_main.c
@@ -462,18 +462,12 @@ void DRV_AppendInformationToHTTPIndexPage(http_request_t* request) {
 
 bool DRV_IsMeasuringPower() {
 #ifndef OBK_DISABLE_ALL_DRIVERS
-	return DRV_IsRunning("BL0937") || DRV_IsRunning("BL0942") || DRV_IsRunning("CSE7766") || DRV_IsRunning("TESTPOWER") || DRV_IsRunning("Battery");
+	return DRV_IsRunning("BL0937") || DRV_IsRunning("BL0942") || DRV_IsRunning("CSE7766") || DRV_IsRunning("TESTPOWER");
 #else
 	return false;
 #endif
 }
-bool DRV_IsMeasuring() {
-#ifndef OBK_DISABLE_ALL_DRIVERS
-	return DRV_IsRunning("BL0937") || DRV_IsRunning("BL0942") || DRV_IsRunning("CSE7766") || DRV_IsRunning("TESTPOWER") || DRV_IsRunning("Battery");
-#else
-	return false;
-#endif
-}
+
 bool DRV_IsSensor() {
 #ifndef OBK_DISABLE_ALL_DRIVERS
 	return DRV_IsRunning("SHT3X") || DRV_IsRunning("CHT8305");
