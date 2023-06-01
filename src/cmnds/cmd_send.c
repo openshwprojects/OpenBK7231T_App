c92c0693d21b6755a470fd8c0b593a52f029d022
Tom <tomthegeek@gmail.com>
2023-06-01 22:41:54 +0000

    Removed extra comments on SendPost added by docs script.

--- src/cmnds/cmd_send.c
+++ src/cmnds/cmd_send.c
@@ -7,8 +7,8 @@
 // SendGet http://192.168.0.112/cm?cmnd=Power0%20Toggle
 // addRepeatingEvent 5 SendGet http://192.168.0.112/cm?cmnd=Power0%20Toggle
 // addEventHandler OnClick 8 SendGet http://192.168.0.112/cm?cmnd=Power0%20Toggle
-static commandResult_t CMD_SendGET(const void *context, const char *cmd, const char *args, int cmdFlags){
-	ADDLOG_INFO(LOG_FEATURE_CMD, " CMD_SendGET received with args %s",args);
+static commandResult_t CMD_SendGET(const void* context, const char* cmd, const char* args, int cmdFlags) {
+	ADDLOG_INFO(LOG_FEATURE_CMD, " CMD_SendGET received with args %s", args);
 
 #if defined(PLATFORM_BEKEN) || defined(WINDOWS)
 	HTTPClient_Async_SendGet(args);
@@ -19,14 +19,14 @@ static commandResult_t CMD_SendGET(const void *context, const char *cmd, const c
 	return CMD_RES_OK;
 }
 // SendPOST http://localhost:3000/ 3000 "application/json" "{ \"a\":123, \"b\":77 }"
-static commandResult_t CMD_SendPOST(const void *context, const char *cmd, const char *args, int cmdFlags) {
+static commandResult_t CMD_SendPOST(const void* context, const char* cmd, const char* args, int cmdFlags) {
 	ADDLOG_INFO(LOG_FEATURE_CMD, " CMD_SendPOST received with args %s", args);
 
 	Tokenizer_TokenizeString(args, TOKENIZER_ALLOW_QUOTES | TOKENIZER_ALLOW_ESCAPING_QUOTATIONS);
 
 #if defined(PLATFORM_BEKEN) || defined(WINDOWS)
 	HTTPClient_Async_SendPost(Tokenizer_GetArg(0),
-		Tokenizer_GetArgIntegerDefault(1,80),
+		Tokenizer_GetArgIntegerDefault(1, 80),
 		Tokenizer_GetArg(2),
 		Tokenizer_GetArg(3),
 		Tokenizer_GetArg(4));
@@ -36,7 +36,7 @@ static commandResult_t CMD_SendPOST(const void *context, const char *cmd, const 
 #endif
 	return CMD_RES_OK;
 }
-static commandResult_t CMD_TestPOST(const void *context, const char *cmd, const char *args, int cmdFlags) {
+static commandResult_t CMD_TestPOST(const void* context, const char* cmd, const char* args, int cmdFlags) {
 
 #if defined(PLATFORM_BEKEN) || defined(WINDOWS)
 	HTTPClient_Async_SendPost("http://localhost:3000/",
@@ -52,36 +52,19 @@ static commandResult_t CMD_TestPOST(const void *context, const char *cmd, const 
 }
 
 
-int CMD_InitSendCommands(){
+int CMD_InitSendCommands() {
 	//cmddetail:{"name":"sendGet","args":"[TargetURL]",
 	//cmddetail:"descr":"Sends a HTTP GET request to target URL. May include GET arguments. Can be used to control devices by Tasmota HTTP protocol. Command supports argument expansion, so $CH11 changes to value of channel 11, etc, etc.",
 	//cmddetail:"fn":"CMD_SendGET","file":"cmnds/cmd_send.c","requires":"",
 	//cmddetail:"examples":""}
-    CMD_RegisterCommand("sendGet", CMD_SendGET, NULL);
+	CMD_RegisterCommand("sendGet", CMD_SendGET, NULL);
 
-	//cmddetail:{"name":"sendPost","args":"[TargetURL]",
-	//cmddetail:"descr":"Sends a HTTP POST request to target URL. TODO",
+	//cmddetail:{"name":"sendPost","args":"[TargetURL] [HTTP Port] [Content Type] [Post Content]",
+	//cmddetail:"descr":"Sends a HTTP POST request to target URL",
 	//cmddetail:"fn":"CMD_SendPOST","file":"cmnds/cmd_send.c","requires":"",
-	//cmddetail:"examples":""}
-	//cmddetail:{"name":"sendPOST","args":"CMD_SendPOST",
-	//cmddetail:"descr":"",
-	//cmddetail:"fn":"NULL);","file":"cmnds/cmd_send.c","requires":"",
-	//cmddetail:"examples":""}
-	//cmddetail:{"name":"sendPOST","args":"CMD_SendPOST",
-	//cmddetail:"descr":"",
-	//cmddetail:"fn":"NULL);","file":"cmnds/cmd_send.c","requires":"",
-	//cmddetail:"examples":""}
-	//cmddetail:{"name":"sendPOST","args":"CMD_SendPOST",
-	//cmddetail:"descr":"",
-	//cmddetail:"fn":"NULL);","file":"cmnds/cmd_send.c","requires":"",
-	//cmddetail:"examples":""}
-	//cmddetail:{"name":"sendPOST","args":"CMD_SendPOST",
-	//cmddetail:"descr":"",
-	//cmddetail:"fn":"NULL);","file":"cmnds/cmd_send.c","requires":"",
-	//cmddetail:"examples":""}
+	//cmddetail:"examples":"SendPost ""http://192.168.0.1/write?db=energy"" 8086 ""application/octet-stream"" ""current,clientid=A0:92:08:CB:41:E1 value=$current"""}
 	CMD_RegisterCommand("sendPOST", CMD_SendPOST, NULL);
 
-
 	//CMD_RegisterCommand("testPost", CMD_TestPOST, NULL);
-    return 0;
+	return 0;
 }
