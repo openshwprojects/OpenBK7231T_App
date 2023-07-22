135382fef78fc33e14d840a1607e1e5f50e92d3e
Tester23 <openshwprojects@gmail.com>
2023-07-22 20:40:32 +0200

    test zero kp

--- src/driver/drv_kp18058.c
+++ src/driver/drv_kp18058.c
@@ -15,26 +15,40 @@ static softI2C_t g_softI2C;
 
 
 void KP18058_Write(float *rgbcw) {
-
+	bool bAllZero = false;
 	int i;
 
 
-	Soft_I2C_Start(&g_softI2C, 0xE1);
-	Soft_I2C_WriteByte(&g_softI2C, 0x00);
-	Soft_I2C_WriteByte(&g_softI2C, 0x03);
-	Soft_I2C_WriteByte(&g_softI2C, 0x7D);
 	for (int i = 0; i < 5; i++) {
-		unsigned short cur_col_12 = MAP(rgbcw[g_cfg.ledRemap.ar[i]], 0, 255.0f, 0, 4095.0f);
-		byte a, b;
-		a = cur_col_12 & 0x3F;
-		b = (cur_col_12 >> 6) & 0x3F;
-		Soft_I2C_WriteByte(&g_softI2C, a);
-		Soft_I2C_WriteByte(&g_softI2C, b);
+		if (rgbcw[i] > 0.01f) {
+			bAllZero = false;
+		}
+	}
+
+	Soft_I2C_Start(&g_softI2C, 0xE1);
+	if (bAllZero) {
+		for (int i = 0; i < 13; i++) {
+			Soft_I2C_WriteByte(&g_softI2C, 0x00);
+		}
+	}
+	else {
+		Soft_I2C_WriteByte(&g_softI2C, 0x00);
+		Soft_I2C_WriteByte(&g_softI2C, 0x03);
+		Soft_I2C_WriteByte(&g_softI2C, 0x7D);
+		for (int i = 0; i < 5; i++) {
+			unsigned short cur_col_12 = MAP(rgbcw[g_cfg.ledRemap.ar[i]], 0, 255.0f, 0, 4095.0f);
+			byte a, b;
+			a = cur_col_12 & 0x3F;
+			b = (cur_col_12 >> 6) & 0x3F;
+			Soft_I2C_WriteByte(&g_softI2C, a);
+			Soft_I2C_WriteByte(&g_softI2C, b);
+		}
 	}
 	Soft_I2C_Stop(&g_softI2C);
 }
 
 // startDriver KP18058
+// KP18058_Map 0 1 2 3 4
 // KP18058_RGBCW FF00000000
 void KP18058_Init() {
 
