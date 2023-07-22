eb535c08cee86f86c2c3c7db6f8610cd9a17b568
Tester23 <openshwprojects@gmail.com>
2023-07-22 20:54:08 +0200

    test

--- src/driver/drv_kp18058.c
+++ src/driver/drv_kp18058.c
@@ -25,13 +25,14 @@ void KP18058_Write(float *rgbcw) {
 		}
 	}
 
-	Soft_I2C_Start(&g_softI2C, 0xE1);
 	if (bAllZero) {
+		Soft_I2C_Start(&g_softI2C, 0x00);
 		for (int i = 0; i < 13; i++) {
 			Soft_I2C_WriteByte(&g_softI2C, 0x00);
 		}
 	}
 	else {
+		Soft_I2C_Start(&g_softI2C, 0xE1);
 		Soft_I2C_WriteByte(&g_softI2C, 0x00);
 		Soft_I2C_WriteByte(&g_softI2C, 0x03);
 		Soft_I2C_WriteByte(&g_softI2C, 0x7D);
