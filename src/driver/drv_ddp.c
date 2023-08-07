c03218edbdf2b8de100d3296eff9c7c659b4191a
openshwprojects <85486843+openshwprojects@users.noreply.github.com>
2023-08-07 09:26:38 +0200

    Update drv_ddp.c

--- src/driver/drv_ddp.c
+++ src/driver/drv_ddp.c
@@ -113,7 +113,7 @@ void DRV_DDP_CreateSocket_Receive() {
 	addLogAdv(LOG_INFO, LOG_FEATURE_DDP,"Waiting for packets\n");
 }
 void DDP_Parse(byte *data, int len) {
-	if(len > 13) {
+	if(len > 12) {
 		byte r, g, b, w, type;
 		type = data[2];
 		r = data[10];
@@ -121,8 +121,13 @@ void DDP_Parse(byte *data, int len) {
 		b = data[12];
 		w = data[13];
 
-	        addLogAdv(LOG_INFO, LOG_FEATURE_DDP, "DDP data: type=%u r=%u g=%u b=%u w=%u", type, r, g, b, w);
-		LED_SetFinalRGB(r,g,b);
+	        //addLogAdv(LOG_INFO, LOG_FEATURE_DDP, "DDP data: type=%u r=%u g=%u b=%u w=%u", type, r, g, b, w);
+		if(type == 0x1B) {
+		    // TODO: support W
+		    LED_SetFinalRGB(r,g,b);
+		} else {
+		    LED_SetFinalRGB(r,g,b);
+		}
 	}
 }
 void DRV_DDP_RunFrame() {
