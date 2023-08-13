d8e1d55de0d7b05df93117d070c3a4e2e99da9e9
Tester23 <openshwprojects@gmail.com>
2023-08-13 11:03:12 +0200

    better charging limit that keeps values on screen, etc

--- src/driver/drv_chargingLimit.c
+++ src/driver/drv_chargingLimit.c
@@ -12,8 +12,10 @@ static int chl_targetChannel = -1;
 static int chl_targetChannelStartValue = -1;
 static int chl_targetChannelMaxDelta = -1;
 static int chl_timeLeftSeconds = -1;
+static int chl_timeMax = -1;
 static char *chl_commandToRun = 0;
 static int chl_lastDelteForDisplay = -1;
+static bool chl_running = false;
 
 // startDriver ChargingLimit
 // chSetupLimit [limitChannelIndex] [maxAllowedLimitChannelDelta] [timeoutOr-1] [commandToRun]
@@ -35,7 +37,7 @@ commandResult_t ChargingLimit_SetupCommand(const void* context, const char* cmd,
 	chl_targetChannel = Tokenizer_GetArgInteger(0);
 	chl_targetChannelStartValue = CHANNEL_Get(chl_targetChannel);
 	chl_targetChannelMaxDelta = Tokenizer_GetArgInteger(1);
-	chl_timeLeftSeconds = Tokenizer_GetArgInteger(2);
+	chl_timeMax = chl_timeLeftSeconds = Tokenizer_GetArgInteger(2);
 	newCmd = Tokenizer_GetArg(3);
 	if (chl_commandToRun != 0 && !strcmp(chl_commandToRun, newCmd)) {
 		// no change, avoid free + malloc
@@ -45,7 +47,7 @@ commandResult_t ChargingLimit_SetupCommand(const void* context, const char* cmd,
 			free(chl_commandToRun);
 		chl_commandToRun = strdup(newCmd);
 	}
-	
+	chl_running = true;
 	return CMD_RES_OK;
 }
 void ChargingLimit_Init() {
@@ -56,17 +58,14 @@ void ChargingLimit_OnLimitReached(bool bTimedOut) {
 	// cast event
 	CMD_ExecuteCommand(chl_commandToRun, 0);
 	// reset variables
-	chl_targetChannel = -1;
-	chl_targetChannelStartValue = -1;
-	chl_targetChannelMaxDelta = -1;
-	chl_timeLeftSeconds = -1;
+	chl_running = false;
 	// do not reset command because we may want to reuse the strdup allocation
 }
 void ChargingLimit_OnEverySecond() {
 	int newValue;
 
 	// if off
-	if (chl_targetChannel == -1)
+	if (chl_running == false)
 		return;
 	// tick time
 	if (chl_timeLeftSeconds != -1) {
@@ -90,14 +89,38 @@ void ChargingLimit_OnEverySecond() {
 
 void ChargingLimit_AppendInformationToHTTPIndexPage(http_request_t* request)
 {
+	const char *rStr;
+	int type;
+
 	if (chl_targetChannel == -1) {
-		hprintf255(request, "<h2>Charging limit reached</h2>");
+		hprintf255(request, "<h2>Charging limit not started yet</h2>");
+		return;
+	}
+	if (chl_running)
+		rStr = "Running";
+	else
+		rStr = "Finished";
+
+	type = CHANNEL_GetType(chl_targetChannel);
+
+	hprintf255(request, "<h2>Charging limit on channel %i: Status %s, Time: - %i of %i seconds",
+			chl_targetChannel, rStr,
+		chl_timeLeftSeconds, chl_timeMax);
+
+	if (type == ChType_EnergyTotal_kWh_div100) {
+		hprintf255(request, ", Power %.2f of %.2f kWh</h2>",
+			chl_lastDelteForDisplay*0.01f, chl_targetChannelMaxDelta*0.01f);
+	}
+	else if (type == ChType_EnergyTotal_kWh_div1000) {
+		hprintf255(request, ", Power %.2f of %.2f kWh</h2>",
+			chl_lastDelteForDisplay*0.001f, chl_targetChannelMaxDelta*0.001f);
 	}
 	else {
-		hprintf255(request, "<h2>Charging limit on for channel %i - %i/%i, time left %i s</h2>",
-			chl_targetChannel, 
-			chl_lastDelteForDisplay, chl_targetChannelMaxDelta, chl_timeLeftSeconds);
+		hprintf255(request, ", Power %i of %i </h2>",
+			chl_lastDelteForDisplay, chl_targetChannelMaxDelta);
 	}
+
+
 }
 
 
