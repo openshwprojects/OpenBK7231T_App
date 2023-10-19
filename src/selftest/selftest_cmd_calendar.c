25186b82fdf720be400336623581bf44a5dec74b
Tester23 <openshwprojects@gmail.com>
2023-10-19 16:53:55 +0200

    a bit more time self tests

--- src/selftest/selftest_cmd_calendar.c
+++ src/selftest/selftest_cmd_calendar.c
@@ -10,6 +10,31 @@ void Test_Commands_Calendar() {
 	// set 2022, 06, 10, 11:27:34, Friday
 	NTP_SetSimulatedTime(1654853254);
 
+	SELFTEST_ASSERT_EXPRESSION("$minute",27);
+	SELFTEST_ASSERT_EXPRESSION("$hour", 11);
+	SELFTEST_ASSERT_EXPRESSION("$second", 34);
+	SELFTEST_ASSERT_EXPRESSION("$day", 5);
+	//SELFTEST_ASSERT_EXPRESSION("$month", 5);
+	//SELFTEST_ASSERT_EXPRESSION("$year", 2022);
+
+//	CMD_ExecuteCommand("setChannel 5 $year", 0);
+//	SELFTEST_ASSERT_EXPRESSION("$CH5", 2022);
+
+//	CMD_ExecuteCommand("setChannel 5 $month", 0);
+//	SELFTEST_ASSERT_EXPRESSION("$CH5", 5);
+
+	CMD_ExecuteCommand("setChannel 5 $second", 0);
+	SELFTEST_ASSERT_EXPRESSION("$CH5", 34);
+
+	CMD_ExecuteCommand("setChannel 5 $minute", 0);
+	SELFTEST_ASSERT_EXPRESSION("$CH5", 27);
+
+	CMD_ExecuteCommand("setChannel 5 $hour", 0);
+	SELFTEST_ASSERT_EXPRESSION("$CH5", 11);
+
+	CMD_ExecuteCommand("setChannel 5 $hour+$minute", 0);
+	SELFTEST_ASSERT_EXPRESSION("$CH5", 11+27);
+
 	SELFTEST_ASSERT_CHANNEL(1, 0);
 
 	// every day
