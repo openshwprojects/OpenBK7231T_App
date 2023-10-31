c62a46ce2cd48d544c0078d251da2f51f2bf2e8d
Tester23 <openshwprojects@gmail.com>
2023-10-31 01:11:04 +0100

    Update new_pins.h

--- src/new_pins.h
+++ src/new_pins.h
@@ -1175,7 +1175,7 @@ typedef struct mainConfig_s {
 	char ping_host[64];
 	// ofs 0x000005E0 (dec 1504)
 	//char initCommandLine[512];
-#if PLATFORM_W600
+#if PLATFORM_W600 || PLATFORM_W800
 #define ALLOW_SSID2 0
 	char initCommandLine[512];
 #else
