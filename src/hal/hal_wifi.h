6df0f6df4311d482afe31402139fa192344a19d9
Tester23 <openshwprojects@gmail.com>
2023-11-02 08:41:33 +0100

    prit more ip info

--- src/hal/hal_wifi.h
+++ src/hal/hal_wifi.h
@@ -28,6 +28,9 @@ void HAL_WiFi_SetupStatusCallback(void (*cb)(int code));
 // This must return correct IP for both SOFT_AP and STATION modes,
 // because, for example, javascript control panel requires it
 const char* HAL_GetMyIPString();
+const char* HAL_GetMyGatewayString();
+const char* HAL_GetMyDNSString();
+const char* HAL_GetMyMaskString();
 const char* HAL_GetMACStr(char* macstr);
 void WiFI_GetMacAddress(char* mac);
 int WiFI_SetMacAddress(char* mac);
