206920946ef0d61077df6355bae7599634d82404
Tester23 <openshwprojects@gmail.com>
2023-12-11 21:16:03 +0100

    silence warning

--- src/cmnds/cmd_newLEDDriver.c
+++ src/cmnds/cmd_newLEDDriver.c
@@ -105,6 +105,30 @@ void LED_ResetGlobalVariablesToDefaults() {
 	led_temperature_current = HASS_TEMPERATURE_MIN;
 }
 
+// The color order is RGBCW.
+// some people set RED to channel 0, and some of them set RED to channel 1
+// Let's detect if there is a PWM on channel 0
+int LED_GetFirstChannelIndex() {
+#if 0
+	int firstChannelIndex;
+	if (CHANNEL_HasChannelPinWithRoleOrRole(0, IOR_PWM, IOR_PWM_n)) {
+		firstChannelIndex = 0;
+	}
+	else {
+		firstChannelIndex = 1;
+	}
+	return firstChannelIndex;
+#else
+	int i;
+	for (i = 0; i < 5; i++) {
+		if (CHANNEL_HasChannelPinWithRoleOrRole(i, IOR_PWM, IOR_PWM_n)) {
+			return i;
+		}
+	}
+	return 0;
+#endif
+}
+
 bool LED_IsLedDriverChipRunning()
 {
 #ifndef PLATFORM_W600
@@ -416,29 +440,6 @@ float led_gamma_correction (int color, float iVal) { // apply LED gamma and RGB 
 
 void LED_SaveStateToFlashVarsNow() {
 	HAL_FlashVars_SaveLED(g_lightMode, g_brightness0to100, led_temperature_current, baseColors[0], baseColors[1], baseColors[2], g_lightEnableAll);
-}
-// The color order is RGBCW.
-// some people set RED to channel 0, and some of them set RED to channel 1
-// Let's detect if there is a PWM on channel 0
-int LED_GetFirstChannelIndex() {
-#if 0
-	int firstChannelIndex;
-	if (CHANNEL_HasChannelPinWithRoleOrRole(0, IOR_PWM, IOR_PWM_n)) {
-		firstChannelIndex = 0;
-	}
-	else {
-		firstChannelIndex = 1;
-	}
-	return firstChannelIndex;
-#else
-	int i;
-	for (i = 0; i < 5; i++) {
-		if (CHANNEL_HasChannelPinWithRoleOrRole(i, IOR_PWM, IOR_PWM_n)) {
-			return i;
-		}
-	}
-	return 0;
-#endif
 }
 void apply_smart_light() {
 	int i;
