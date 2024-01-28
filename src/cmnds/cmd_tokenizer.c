df91eea498f18f3226bbeef77793ba5563fa3a3d
imre <ilengyel@gmail.com>
2024-01-28 20:31:24 +1100

    Revert a function delete

--- src/cmnds/cmd_tokenizer.c
+++ src/cmnds/cmd_tokenizer.c
@@ -57,15 +57,83 @@ bool Tokenizer_IsArgInteger(int i) {
 	}
 	return strIsInteger(g_args[i]);
 }
+const char *Tokenizer_GetArgExpanding(int i) {
+	const char *s;
+	char tokLine[sizeof(g_argsExpanded[i])];
+	char Templine[sizeof(g_argsExpanded[i])];
+	char convert[10];
 
+	if (i >= g_numArgs)
+		return 0;
+
+	s = g_args[i];
+
+	//séparators for strtok to detect constants
+	const char * separators = "${}";
+	//pointer for strstr function
+	char *ptrConst;
+
+	//copy input string before manipulations
+	strcpy_safe(g_argsExpanded[i], s, sizeof(g_argsExpanded[i]));
+	strcpy_safe(tokLine, s, sizeof(tokLine));
+
+	//start strtok
+	char *strToken = strtok(tokLine, separators);
+	while (strToken != NULL) {
+		//build tconst with ${<token>} and try find it
+		char tconst[20] = "${";
+		strcat(tconst, strToken);
+		strcat(tconst, "}");
+		ptrConst = strstr(g_argsExpanded[i], tconst);
+		if (ptrConst == NULL) {
+			// we didn't find ${<token>} so we try with $<token>
+			strcpy(tconst, "$");
+			strcat(tconst, strToken);
+			ptrConst = strstr(g_argsExpanded[i], tconst);
+		}
+		// if we found ${<token>} or $<token> it means we found a constant
+		if (ptrConst != NULL) {
+			//put 0 on the start of the constant to copy the left part of the input string
+			ptrConst[0] = 0;
+			strcpy_safe(Templine, g_argsExpanded[i], sizeof(Templine));
+			//analyse the constant found to replace it with it's value/string and concat it with the left part of the input string
+			if (!strcmp(tconst, "${IP}") || !strcmp(tconst, "$IP")) {
+				strcat_safe(Templine, HAL_GetMyIPString(), sizeof(Templine));
+			}
+			else if (!strcmp(tconst, "${ShortName}") || !strcmp(tconst, "$ShortName")) {
+				strcat_safe(Templine, CFG_GetShortDeviceName(), sizeof(Templine));
+			}
+			else if (!strcmp(tconst, "${Name}") || !strcmp(tconst, "$Name")) {
+				strcat_safe(Templine, CFG_GetDeviceName(), sizeof(Templine));
+			}
+			else {
+				float f;
+				int iValue;
+				CMD_ExpandConstant(tconst, 0, &f);
+				iValue = f;
+				sprintf(convert, "%i", iValue);
+				strcat_safe(Templine, convert, sizeof(Templine));
+			}
+			//concat with the right part, after the constant
+			strcat_safe(Templine, ptrConst + strlen(tconst), sizeof(Templine));
+			//update the input string with the replaced constant
+			strcpy_safe(g_argsExpanded[i], Templine, sizeof(g_argsExpanded[i]));
+		}
+		//look for next token
+		strToken = strtok(NULL, separators);
+
+	}
+
+	return g_argsExpanded[i];
+
+}
 const char *Tokenizer_GetArg(int i) {
 	const char *s;
 
 	if (i >= g_numArgs)
 		return 0;
 
-	if (g_argsExpanded[i][0] != 0)
-	{
+	if (g_argsExpanded[i][0] != 0) {
 		return g_argsExpanded[i];
 	}
 
