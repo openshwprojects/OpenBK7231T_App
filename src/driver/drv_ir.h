6065b9201451021bb237c3c640b535b8cfd6569c
T Aviss <dev@error-gap.ca>
2024-02-22 14:53:16 -0800

    Updates to drv_ir.h and drv_ir.cpp * Migrate headers for SpoofIrSender to drv_ir.h (avoid issues between SpoofIrSender and IRSend object instantiation) * Update code for SpoofIrSender to incorporate a pointer to myIRsend object which can be attached to SpoofIrSender instance * Push functions through currently attached myIRsend instance for previously blank functions within SpoofIrSender

--- src/driver/drv_ir.h
+++ src/driver/drv_ir.h
@@ -1,5 +1,6 @@
 #ifndef __DRV_IR_H__
 #define __DRV_IR_H__
+#define SEND_MAXBITS 128
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,6 +11,27 @@ void DRV_IR_RunFrame();
 
 #ifdef __cplusplus
 }
-#endif
+
+
+//Fwd declaration(s)
+struct PulsePauseWidthProtocolConstants;
+class myIRsend;
+
+//#include "../libraries/Arduino-IRremote-mod/src/IRProtocol.h"
+class SpoofIrSender {
+    public:
+        void enableIROut(uint_fast8_t);
+        void mark(unsigned int);
+        void space(unsigned int);
+        void sendPulseDistanceWidthFromArray(uint_fast8_t, unsigned int,unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, uint32_t*, unsigned int, bool,bool, unsigned int, int_fast8_t);
+        void sendPulseDistanceWidthFromArray(PulsePauseWidthProtocolConstants *aProtocolConstants, uint32_t *aDecodedRawDataArray,
+        unsigned int aNumberOfBits, int_fast8_t aNumberOfRepeats);
+		void SetParent(myIRsend *IRParent);
+	private:
+		myIRsend * Parent;
+
+};
 
 #endif
+#endif
+
