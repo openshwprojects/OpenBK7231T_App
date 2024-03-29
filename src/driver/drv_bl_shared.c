bc7dbd038f0040b14badf5040f1291093690ecfb
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-03-29 00:54:34 +0000

    Update drv_bl_shared.c

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -113,8 +113,8 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
 					(i == OBK_CONSUMPTION_TOTAL ? 0.001 : 1) * sensors[i].lastReading, //always display OBK_CONSUMPTION_TOTAL in kwh
 					i == OBK_CONSUMPTION_TOTAL ? "kWh": sensors[i].names.units);
 
-			//hprintf255(request,"<h2>Periodic Statistics</h2><h5>Consumption (during this period): ");
-        		//hprintf255(request,"%1.*f Wh<br>", sensors[OBK_CONSUMPTION_LAST_HOUR].rounding_decimals, DRV_GetReading(OBK_CONSUMPTION_LAST_HOUR));
+			hprintf255(request,"<h2>Periodic Statistics</h2><h5>Consumption (during this period): ");
+        		hprintf255(request,"%1.*f Wh<br>", sensors[OBK_CONSUMPTION_LAST_HOUR].rounding_decimals, DRV_GetReading(OBK_CONSUMPTION_LAST_HOUR));
 		}
 	};
 
@@ -532,8 +532,8 @@ void BL_ProcessUpdate(float voltage, float current, float power,
     sensors[OBK_CONSUMPTION_TOTAL].lastReading += (double)energy;
     sensors[OBK_CONSUMPTION_TODAY].lastReading += (double)generation;
     //
-        hprintf255(request,"<h2>Periodic Statistics</h2><h5>Consumption (during this period): ");
-        hprintf255(request,"%1.*f Wh<br>", sensors[OBK_CONSUMPTION_TODAY].lastReading += (double)generation;
+      //  hprintf255(request,"<h2>Periodic Statistics</h2><h5>Consumption (during this period): ");
+       // hprintf255(request,"%1.*f Wh<br>", sensors[OBK_CONSUMPTION_TODAY].lastReading += (double)generation;
 
 	
     //
