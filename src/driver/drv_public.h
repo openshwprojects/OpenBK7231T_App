b0408c76583dee442f05c1bf6403fb6d525c43f3
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-03-29 15:16:48 +0000

    Update drv_public.h

--- src/driver/drv_public.h
+++ src/driver/drv_public.h
@@ -12,6 +12,7 @@ typedef enum energySensor_e {
 	OBK_POWER_REACTIVE,
 	OBK_POWER_FACTOR,
 	OBK_CONSUMPTION_TOTAL,
+	OBK_GENERATION_TOTAL,
 	OBK__NUM_MEASUREMENTS = OBK_CONSUMPTION_TOTAL,
 
 	// TODO OBK_CONSUMPTION_LAST_HOUR is actally "sum of consumption stats recording period"
