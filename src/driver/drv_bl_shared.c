0b3ed32be084b9342d36176218289247c75547ab
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-03-29 22:18:22 +0000

    Update drv_bl_shared.c

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -121,7 +121,7 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
 	hprintf255(request,"<h2>Periodic Statistics</h2><h5>Consumption (during this period): ");
         hprintf255(request,"%1.*f Wh<br>", sensors[OBK_CONSUMPTION_LAST_HOUR].rounding_decimals, DRV_GetReading(OBK_CONSUMPTION_LAST_HOUR));
 	poststr(request, "<tr><td><b>Total Generation</b></td><td style='text-align: right;'>");
-        hprintf255(request, "%.3f</td><td>KWh</td>", ([OBK_GENERATION_TOTAL].lastReading * 0.001)); //always display OBK_GNERATION_TOTAL in kwh
+        hprintf255(request, "%.3f</td><td>KWh</td>", (sensors[OBK_GENERATION_TOTAL].lastReading * 0.001)); //always display OBK_GNERATION_TOTAL in kwh
 		
 		//sensors[OBK_CONSUMPTION_TODAY].lastReading );
 
