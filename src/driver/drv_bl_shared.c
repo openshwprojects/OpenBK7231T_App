9806fbea73847d1e84aacfeaf11a32e20e494d9a
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-03-30 01:24:55 +0000

    Update drv_bl_shared.c

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -132,7 +132,7 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
    	 }
   }
 	// Print out periodic statistics and Total Generation at the bottom of the page.
-	hprintf255(request,"<h2>Periodic Statistics</h2><h5>NetMetering (Last 15min): ");
+	hprintf255(request,"<h2>Periodic Statistics</h2><h5>"NetMetering (Last 15min): ");
         hprintf255(request,"%1.*f Wh<br>", sensors[OBK_CONSUMPTION_LAST_HOUR].rounding_decimals, DRV_GetReading(OBK_CONSUMPTION_LAST_HOUR));
 	poststr(request, "<tr><td><b>Total Generation</b></td><td style='text-align: right;'>");
         hprintf255(request, "%.3f</td><td>KWh</td>", (starting_net_metering_value + (sensors[OBK_CONSUMPTION_TOTAL].lastReading-sensors[OBK_GENERATION_TOTAL].lastReading) * 0.001)); //always display OBK_GNERATION_TOTAL in kwh
