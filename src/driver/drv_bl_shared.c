22aec257263add0574d790efef8202767574a639
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-04-01 13:12:25 +0100

    Update drv_bl_shared.c

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -116,7 +116,7 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
         hprintf255(request, "%.2f</td><td>Hz</td>", lastReadingFrequency);
     }
 
-	for (int i = (1+OBK__FIRST); i <= (OBK_CONSUMPTION__DAILY_LAST); i++) {
+	for (int i = (OBK__FIRST); i <= (OBK_CONSUMPTION__DAILY_LAST); i++) {
 	//for (int i = 0; i <= (15); i++) {
 		if (i <= OBK__NUM_MEASUREMENTS || NTP_IsTimeSynced()) {
 			poststr(request, "<tr><td><b>");
