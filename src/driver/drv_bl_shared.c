4d0e50a727f87cccc3d5f2e256d85e076d46dcd4
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-04-03 01:53:56 +0100

    Update drv_bl_shared.c

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -166,13 +166,13 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
    			lastsync = check_time;
 			//CMD_ExecuteCommand("SendGet http://192.168.8.164/cm?cmnd=Power%20TOGGLE", 0);
 			// Are we exporting enough? If so, turn the relay on
-			if ((min_production>(sensors[OBK_POWER].lastReading))&&(net_energy>dump_load_on))
+			if ((min_production>(sensors[OBK_POWER].lastReading)&&(net_energy>dump_load_off))||(net_energy>dump_load_on))
 			{
 				dump_load_relay = 1;
 				CMD_ExecuteCommand("SendGet http://192.168.8.164/cm?cmnd=Power%20on", 0);
 			}
 			// Are we close to zero export? Turn the relay off.
-			else if (net_energy<dump_load_off)
+			else 
 			{
 				dump_load_relay = 0;
 				CMD_ExecuteCommand("SendGet http://192.168.8.164/cm?cmnd=Power%20off", 0);
