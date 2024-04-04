fa0fc8a35703a8f0ecd87afe243e988b57446b87
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-04-04 20:35:17 +0100

    Update drv_bl_shared.c

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -174,7 +174,7 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
 			{
 				dump_load_relay = 1;
 				CMD_ExecuteCommand("SendGet http://192.168.8.164/cm?cmnd=Power%20on", 0);
-				digitalWrite(9, 0);
+				CMD_ExecuteCommand(setChannel 1 1, 0);
 			}
 			// Are we close to zero export? Turn the relay off.
 			else 
@@ -183,7 +183,7 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
 				{
 				dump_load_relay = 0;
 				CMD_ExecuteCommand("SendGet http://192.168.8.164/cm?cmnd=Power%20off", 0);
-				digitalWrite(9, 1);
+				CMD_ExecuteCommand(setChannel 1 0, 0);
 				}
 			}
 
