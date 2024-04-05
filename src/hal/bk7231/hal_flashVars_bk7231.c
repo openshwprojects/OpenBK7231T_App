587c4a3bf07a36541da3decdc39d3f3ba9384e9b
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-04-05 22:24:42 +0100

    Update hal_flashVars_bk7231.c

--- src/hal/bk7231/hal_flashVars_bk7231.c
+++ src/hal/bk7231/hal_flashVars_bk7231.c
@@ -684,5 +684,25 @@ void HAL_FlashVars_SaveTotalConsumption(float total_consumption)
 #endif
 }
 
+
+
+#endif
+
+void HAL_FlashVars_SaveEnergyExport(float f)
+{
+	// use two last retain channels as float,
+	// I don't think it will cause a clash, power metering devices dont use that many channels anyway
+	memcpy(&flash_vars.savedValues[MAX_RETAIN_CHANNELS - 2], &f, sizeof(float));
+}
+
+#endif
+
+void HAL_FlashVars_SaveEnergySold(float f)
+{
+	// use two last retain channels as float,
+	// I don't think it will cause a clash, power metering devices dont use that many channels anyway
+	memcpy(&flash_vars.savedValues[MAX_RETAIN_CHANNELS - 4], &f, sizeof(float));
+}
+
 #endif
 
