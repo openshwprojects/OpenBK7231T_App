6b25c9a5e82469fd6ac0db34943189fd882535f2
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-04-06 02:42:59 +0100

    Update drv_bl_shared.c

--- src/driver/drv_bl_shared.c
+++ src/driver/drv_bl_shared.c
@@ -138,6 +138,7 @@ void BL09XX_AppendInformationToHTTPIndexPage(http_request_t *request)
 				poststr(request, "<br><hr> **** TOTALS **** ");
 				poststr(request, "<hr><table style='width:100%'>");
 				poststr(request, "</b></td><td style='text-align: right;'>");
+				poststr(request, sensors[i].names.name_friendly);
 				i++;
 			}
 			if ((i >= OBK_CONSUMPTION_TOTAL) /*|| (i == OBK_GENERATION_TOTAL) || (i == OBK_GENERATION_SOLD_TOTAL)*/)
