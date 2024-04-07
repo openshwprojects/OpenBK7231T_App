108384fcd2d203bd578c037033955fb66a8f0581
giedriuslt <123905703+giedriuslt@users.noreply.github.com>
2024-04-07 09:12:45 +0300

    Fix RSSI for W600 (#1172)
    Looks like rrsi is inverted for W600, otherwise looks similar to beken in similar places

--- src/hal/w800/hal_wifi_w800.c
+++ src/hal/w800/hal_wifi_w800.c
@@ -105,7 +105,7 @@ int HAL_GetWifiStrength()
 {
 	struct tls_curr_bss_t bss;
 	tls_wifi_get_current_bss(&bss);
-	return bss.rssi;
+	return (signed char)(0x100-bss.rssi); //this is adjusted according to example
 }
 
 static void apsta_net_status(u8 status)
