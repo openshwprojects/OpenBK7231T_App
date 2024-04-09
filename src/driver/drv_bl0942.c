2a6357855cb5054c80407a2eab301c222f86bf9e
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-04-09 17:32:26 +0100

    Update drv_bl0942.c

--- src/driver/drv_bl0942.c
+++ src/driver/drv_bl0942.c
@@ -85,11 +85,6 @@ static void ScaleAndUpdate(bl0942_data_t *data) {
             fabsf(PwrCal_ScalePowerOnly(diff)) * 1638.4f * 256.0f / 3600.0f;
     }
     PrevCfCnt = data->cf_cnt;
-	 else
-		 {
-		 // If energy is positive
-		 energyWh = fabsf(PwrCal_ScalePowerOnly(data->cf_cnt)) * 1638.4f * 256.0f / 3600.0f;
-	 	 }
     BL_ProcessUpdate(voltage, current, power, frequency, energyWh);
 }
 
