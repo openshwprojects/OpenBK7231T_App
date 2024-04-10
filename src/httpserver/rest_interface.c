89fc09a921cf7b3a9431b30c09d598ec1c6f7d76
Tester23 <openshwprojects@gmail.com>
2024-04-10 15:44:59 +0200

    fix Web App API for long startup commands > 255 chars

--- src/httpserver/rest_interface.c
+++ src/httpserver/rest_interface.c
@@ -875,8 +875,10 @@ static int http_rest_get_info(http_request_t* request) {
 	hprintf255(request, "\"chipset\":\"%s\",", PLATFORM_MCU_NAME);
 	hprintf255(request, "\"webapp\":\"%s\",", CFG_GetWebappRoot());
 	hprintf255(request, "\"shortName\":\"%s\",", CFG_GetShortDeviceName());
-	
-	hprintf255(request, "\"startcmd\":\"%s\",", CFG_GetShortStartupCommand());
+	poststr(request, "\"startcmd\":\"");
+	// This can be longer than 255
+	poststr(request, CFG_GetShortStartupCommand());
+	poststr(request, "\",");
 #ifndef OBK_DISABLE_ALL_DRIVERS
 	hprintf255(request, "\"supportsSSDP\":%d,", DRV_IsRunning("SSDP") ? 1 : 0);
 #else
