cf102e0ed21bc52cbe96419fadb7e2e0074e48f0
giedriuslt <123905703+giedriuslt@users.noreply.github.com>
2024-04-14 19:40:38 +0300

    Use ISO time everywhere

--- src/win_main_scriptOnly.c
+++ src/win_main_scriptOnly.c
@@ -89,9 +89,9 @@ int __cdecl main(void)
 	  addLogAdv(1,1, "%sTime %i, idle %i/s, free %d, MQTT %i(%i), bWifi %i, secondsWithNoPing %i, socks %i/%i\n",
 		  "[TEST]", 123, 555,95000,1, 6, 
             1, -1, 3, 64);
-    printf("Consumption Reset Time: %04d/%02d/%02d %02d:%02d:%02d",
+    printf("Consumption Reset Time: %04d-%02d-%02d %02d:%02d:%02d",
                        ltm->tm_year+1900, ltm->tm_mon+1, ltm->tm_mday, ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
-    addLogAdv(1,1, "Consumption Reset Time: %04d/%02d/%02d %02d:%02d:%02d",
+    addLogAdv(1,1, "Consumption Reset Time: %04d-%02d-%02d %02d:%02d:%02d",
                        ltm->tm_year+1900, ltm->tm_mon+1, ltm->tm_mday, ltm->tm_hour, ltm->tm_min, ltm->tm_sec);
 
             addLogAdv(1,1, "Today: %1.1f Wh DailyStats: [", 3.145f);
