a14abd26abb75dff84b459e8071ad2adfb317d87
2Fblob <164585100+2Fblob@users.noreply.github.com>
2024-04-19 09:31:43 +0200

    Update drv_bl0942.c

--- src/driver/drv_bl0942.c
+++ src/driver/drv_bl0942.c
@@ -280,7 +280,7 @@ void BL0942_SPI_RunEverySecond(void) {
     SPI_ReadReg(BL0942_REG_I_RMS, &data.i_rms);
     SPI_ReadReg(BL0942_REG_V_RMS, &data.v_rms);
     SPI_ReadReg(BL0942_REG_WATT, (uint32_t *)&data.watt);
-    data.watt = Int24ToInt32(data.watt);}
+    data.watt = Int24ToInt32(data.watt);
     if (CFG_HasFlag(OBK_FLAG_MQTT_ENERGY_IN_KWH)){
 		data.watt = (-1*data.watt);}
     SPI_ReadReg(BL0942_REG_CF_CNT, &data.cf_cnt);
