f2480fd2327ebccc41cb25b03916faed736f9f8b
openshwprojects <85486843+openshwprojects@users.noreply.github.com>
2024-05-02 17:52:31 +0200

    rename pwm freq command (#1204)
    * pwm freqq
    
    * rename

--- src/cmnds/cmd_main.c
+++ src/cmnds/cmd_main.c
@@ -643,8 +643,7 @@ commandResult_t CMD_FindPattern(const void *context, const char *cmd, const char
 
 int g_pwmFrequency = PWM_FREQUENCY_DEFAULT;
 
-commandResult_t CMD_PWMHz(const void* context, const char* cmd, const char* args, int cmdFlags) {
-
+commandResult_t CMD_PWMFrequency(const void* context, const char* cmd, const char* args, int cmdFlags) {
 	Tokenizer_TokenizeString(args, TOKENIZER_ALLOW_QUOTES | TOKENIZER_DONT_EXPAND);
 	// following check must be done after 'Tokenizer_TokenizeString',
 	// so we know arguments count in Tokenizer. 'cmd' argument is
@@ -815,7 +814,8 @@ void CMD_Init_Early() {
 
 	CMD_RegisterCommand("TimeSize", CMD_TimeSize, NULL);
 
-	CMD_RegisterCommand("PWMHz", CMD_PWMHz, NULL);
+	CMD_RegisterCommand("PWMFrequency", CMD_PWMFrequency, NULL);
+
 	
 #if (defined WINDOWS) || (defined PLATFORM_BEKEN) || (defined PLATFORM_BL602)
 	CMD_InitScripting();
