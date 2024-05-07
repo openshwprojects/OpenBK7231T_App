c65a87066353abc45b4148cbb8b26420190e2970
divadiow <divadiow@hotmail.com>
2024-05-07 11:13:36 +0100

    Revert "Update BMP280.h test"
    This reverts commit 7cb4fd0ed3c5177f5398fc2bba754c56de7330f7.

--- src/driver/BMP280.h
+++ src/driver/BMP280.h
@@ -16,7 +16,7 @@
 
 #include <stdint.h>
 
-#define BMP280_CHIP_ID        0x58
+#define BMP280_CHIP_ID        0x77
 #define BME280_CHIP_ID        0x60
 
 #define BMP280_REG_DIG_T1     0x88
@@ -41,46 +41,48 @@
 #define BMP280_REG_CONFIG     0xF5
 #define BMP280_REG_PRESS_MSB  0xF7
 
-#define BMP280_I2C_ADDR       0x77  // I2C Address
-
 int32_t adc_T, adc_P, t_fine;
 
+// BMP280 sensor modes, register ctrl_meas mode[1:0]
 typedef enum
 {
-  MODE_SLEEP  = 0x00,
-  MODE_FORCED = 0x01,
-  MODE_NORMAL = 0x03
+  MODE_SLEEP  = 0x00,  // sleep mode
+  MODE_FORCED = 0x01,  // forced mode
+  MODE_NORMAL = 0x03   // normal mode
 } BMP280_mode;
 
+// oversampling setting. osrs_t[2:0], osrs_p[2:0]
 typedef enum
 {
-  SAMPLING_SKIPPED = 0x00,
-  SAMPLING_X1      = 0x01,
-  SAMPLING_X2      = 0x02,
-  SAMPLING_X4      = 0x03,
-  SAMPLING_X8      = 0x04,
-  SAMPLING_X16     = 0x05
+  SAMPLING_SKIPPED = 0x00,  //skipped, output set to 0x80000
+  SAMPLING_X1      = 0x01,  // oversampling x1
+  SAMPLING_X2      = 0x02,  // oversampling x2
+  SAMPLING_X4      = 0x03,  // oversampling x4
+  SAMPLING_X8      = 0x04,  // oversampling x8
+  SAMPLING_X16     = 0x05   // oversampling x16
 } BMP280_sampling;
 
+// filter setting filter[2:0]
 typedef enum
 {
-  FILTER_OFF = 0x00,
-  FILTER_2   = 0x01,
-  FILTER_4   = 0x02,
-  FILTER_8   = 0x03,
-  FILTER_16  = 0x04
+  FILTER_OFF = 0x00,  // filter off
+  FILTER_2   = 0x01,  // filter coefficient = 2
+  FILTER_4   = 0x02,  // filter coefficient = 4
+  FILTER_8   = 0x03,  // filter coefficient = 8
+  FILTER_16  = 0x04   // filter coefficient = 16
 } BMP280_filter;
 
+// standby (inactive) time in ms (used in normal mode), t_sb[2:0]
 typedef enum
 {
-  STANDBY_0_5   =  0x00,
-  STANDBY_62_5  =  0x01,
-  STANDBY_125   =  0x02,
-  STANDBY_250   =  0x03,
-  STANDBY_500   =  0x04,
-  STANDBY_1000  =  0x05,
-  STANDBY_2000  =  0x06,
-  STANDBY_4000  =  0x07
+  STANDBY_0_5   =  0x00,  // standby time = 0.5 ms
+  STANDBY_62_5  =  0x01,  // standby time = 62.5 ms
+  STANDBY_125   =  0x02,  // standby time = 125 ms
+  STANDBY_250   =  0x03,  // standby time = 250 ms
+  STANDBY_500   =  0x04,  // standby time = 500 ms
+  STANDBY_1000  =  0x05,  // standby time = 1000 ms
+  STANDBY_2000  =  0x06,  // standby time = 2000 ms
+  STANDBY_4000  =  0x07   // standby time = 4000 ms
 } standby_time;
 
 struct
@@ -100,31 +102,34 @@ struct
   int16_t  dig_P9;
 } BMP280_calib;
 
+// writes 1 byte '_data' to register 'reg_addr'
 void BMP280_Write8(uint8_t reg_addr, uint8_t _data)
 {
   BMP280_Start();
-  BMP280_Write(BMP280_I2C_ADDR);
+  BMP280_Write(g_softI2C.address8bit);
   BMP280_Write(reg_addr);
   BMP280_Write(_data);
   BMP280_Stop();
 }
 
+// reads 8 bits from register 'reg_addr'
 uint8_t BMP280_Read8(uint8_t reg_addr)
 {
   uint8_t ret;
 
   BMP280_Start();
-  BMP280_Write(BMP280_I2C_ADDR);
+  BMP280_Write(g_softI2C.address8bit);
   BMP280_Write(reg_addr);
-  Soft_I2C_Stop();
+  Soft_I2C_Stop(&g_softI2C);
   BMP280_Start();
-  BMP280_Write(BMP280_I2C_ADDR | 1);
+  BMP280_Write(g_softI2C.address8bit | 1);
   ret = BMP280_Read(0);
   BMP280_Stop();
 
   return ret;
 }
 
+// reads 16 bits from register 'reg_addr'
 uint16_t BMP280_Read16(uint8_t reg_addr)
 {
   union
@@ -134,45 +139,55 @@ uint16_t BMP280_Read16(uint8_t reg_addr)
   } ret;
 
   BMP280_Start();
-  BMP280_Write(BMP280_I2C_ADDR);
+  BMP280_Write(g_softI2C.address8bit);
   BMP280_Write(reg_addr);
-  Soft_I2C_Stop();
+  Soft_I2C_Stop(&g_softI2C);
   BMP280_Start();
-  BMP280_Write(BMP280_I2C_ADDR | 1);
+  BMP280_Write(g_softI2C.address8bit | 1);
   ret.b[0] = BMP280_Read(1);
   ret.b[1] = BMP280_Read(0);
   BMP280_Stop();
 
-  return ret.w;
+  return(ret.w);
 }
 
-void BMP280_Configure(BMP280_mode mode, BMP280_sampling T_sampling, BMP280_sampling P_sampling, BMP280_filter filter, standby_time standby)
+// BMP280 sensor configuration function
+void BMP280_Configure(BMP280_mode mode, BMP280_sampling T_sampling,
+                      BMP280_sampling P_sampling, BMP280_filter filter, standby_time standby)
 {
   uint8_t  _ctrl_meas, _config;
 
   _config = ((standby << 5) | (filter << 2)) & 0xFC;
   _ctrl_meas = (T_sampling << 5) | (P_sampling << 2) | mode;
 
-  BMP280_Write8(BMP280_REG_CONFIG, _config);
+  BMP280_Write8(BMP280_REG_CONFIG,  _config);
   BMP280_Write8(BMP280_REG_CONTROL, _ctrl_meas);
 }
 
-uint8_t BMP280_begin(BMP280_mode mode, BMP280_sampling T_sampling, BMP280_sampling P_sampling, BMP280_filter filter, standby_time standby)
+// initializes the BMP280 sensor, returns 1 if OK and 0 if error
+uint8_t BMP280_begin(BMP280_mode mode,
+                  BMP280_sampling T_sampling,
+                  BMP280_sampling P_sampling,
+                  BMP280_filter filter,
+                  standby_time  standby)
 {
-  int id = BMP280_Read8(BMP280_REG_CHIPID);
-  if (id == BMP280_CHIP_ID) {
-    addLogAdv(LOG_INFO, LOG_FEATURE_SENSOR, "BMP280 detected!");
-  } else if (id == BME280_CHIP_ID) {
-    addLogAdv(LOG_INFO, LOG_FEATURE_SENSOR, "BME280 detected!");
-  } else {
-    addLogAdv(LOG_INFO, LOG_FEATURE_SENSOR, "BMx280 wrong ID!");
-    return 0;
-  }
+	int id = BMP280_Read8(BMP280_REG_CHIPID);
+	if (id == BMP280_CHIP_ID) {
+		addLogAdv(LOG_INFO, LOG_FEATURE_SENSOR, "BMP280 detected!");
+	} else if (id == BME280_CHIP_ID) {
+		addLogAdv(LOG_INFO, LOG_FEATURE_SENSOR, "BME280 detected!");
+	}
+	else {
+		addLogAdv(LOG_INFO, LOG_FEATURE_SENSOR, "BMx280 wrong ID!");
+		return 0;
+	}
 
+  // reset the BMP280 with soft reset
   BMP280_Write8(BMP280_REG_SOFTRESET, 0xB6);
   delay_ms(100);
 
-  while ((BMP280_Read8(BMP280_REG_STATUS) & 0x01) == 0x01)
+  // if NVM data are being copied to image registers, wait 100 ms
+  while ( (BMP280_Read8(BMP280_REG_STATUS) & 0x01) == 0x01 )
     delay_ms(100);
 
   BMP280_calib.dig_T1 = BMP280_Read16(BMP280_REG_DIG_T1);
@@ -194,20 +209,25 @@ uint8_t BMP280_begin(BMP280_mode mode, BMP280_sampling T_sampling, BMP280_sampli
   return 1;
 }
 
+// Takes a new measurement, for forced mode only!
+// Returns 1 if ok and 0 if error (sensor is not in sleep mode)
 uint8_t BMP280_ForcedMeasurement()
 {
   uint8_t ctrl_meas_reg = BMP280_Read8(BMP280_REG_CONTROL);
 
-  if ((ctrl_meas_reg & 0x03) != 0x00)
-    return 0;  // sensor is not in sleep mode
+  if ( (ctrl_meas_reg & 0x03) != 0x00 )
+    return 0;   // sensor is not in sleep mode
 
+  // set sensor to forced mode
   BMP280_Write8(BMP280_REG_CONTROL, ctrl_meas_reg | 1);
+  // wait for conversion complete
   while (BMP280_Read8(BMP280_REG_STATUS) & 0x08)
     delay_ms(1);
 
   return 1;
 }
 
+// read (updates) adc_P, adc_T and adc_H from BMP280 sensor
 void BMP280_Update()
 {
   union
@@ -218,16 +238,16 @@ void BMP280_Update()
   ret.b[3] = 0x00;
 
   BMP280_Start();
-  BMP280_Write(BMP280_I2C_ADDR);
+  BMP280_Write(g_softI2C.address8bit);
   BMP280_Write(BMP280_REG_PRESS_MSB);
-  Soft_I2C_Stop();
+  Soft_I2C_Stop(&g_softI2C);
   BMP280_Start();
-  BMP280_Write(BMP280_I2C_ADDR | 1);
+  BMP280_Write(g_softI2C.address8bit | 1);
   ret.b[2] = BMP280_Read(1);
   ret.b[1] = BMP280_Read(1);
   ret.b[0] = BMP280_Read(1);
 
-  adc_P = (ret.dw >> 4) & 0xFFFFF;
+  adc_P = (ret.dw >> 4) & 0xFFFFF;  
 
   ret.b[2] = BMP280_Read(1);
   ret.b[1] = BMP280_Read(1);
@@ -237,12 +257,16 @@ void BMP280_Update()
   adc_T = (ret.dw >> 4) & 0xFFFFF;
 }
 
+// Reads temperature from BMP280 sensor.
+// Temperature is stored in hundredths C (output value of "5123" equals 51.23 DegC).
+// Temperature value is saved to *temp, returns 1 if OK and 0 if error.
 uint8_t BMP280_readTemperature(int32_t *temp)
 {
   int32_t var1, var2;
 
   BMP280_Update();
 
+  // calculate temperature
   var1 = ((((adc_T / 8) - ((int32_t)BMP280_calib.dig_T1 * 2))) *
          ((int32_t)BMP280_calib.dig_T2)) / 2048;
 
@@ -257,39 +281,45 @@ uint8_t BMP280_readTemperature(int32_t *temp)
   return 1;
 }
 
+// Reads pressure from BMP280 sensor.
+// Pressure is stored in Pa (output value of "96386" equals 96386 Pa = 963.86 hPa).
+// Pressure value is saved to *pres, returns 1 if OK and 0 if error.
 uint8_t BMP280_readPressure(uint32_t *pres)
 {
   int32_t var1, var2;
   uint32_t p;
 
+  // calculate pressure
   var1 = (((int32_t)t_fine) / 2) - (int32_t)64000;
-  var2 = (((var1 / 4) * (var1 / 4)) / 2048) * ((int32_t)BMP280_calib.dig_P6);
-  var2 = var2 + ((var1 * ((int32_t)BMP280_calib.dig_P5)) * 2);
-  var2 = (var2 / 4) + (((int32_t)BMP280_calib.dig_P4) * 65536);
-  var1 = ((((int32_t)BMP280_calib.dig_P3 * (((var1 / 4) * (var1 / 4)) / 8192)) / 8) +
-         ((((int32_t)BMP280_calib.dig_P2) * var1) / 2)) / 262144;
-  var1 = ((((32768 + var1)) * ((int32_t)BMP280_calib.dig_P1)) / 32768);
+  var2 = (((var1/4) * (var1/4)) / 2048 ) * ((int32_t)BMP280_calib.dig_P6);
 
-  if (var1 == 0) {
+  var2 = var2 + ((var1 * ((int32_t)BMP280_calib.dig_P5)) * 2);
+  var2 = (var2/4) + (((int32_t)BMP280_calib.dig_P4) * 65536);
+
+  var1 = ((((int32_t)BMP280_calib.dig_P3 * (((var1/4) * (var1/4)) / 8192 )) / 8) +
+         ((((int32_t)BMP280_calib.dig_P2) * var1)/2)) / 262144;
+
+  var1 =((((32768 + var1)) * ((int32_t)BMP280_calib.dig_P1)) / 32768);
+
+  if (var1 == 0)
     return 0; // avoid exception caused by division by zero
-  }
 
   p = (((uint32_t)(((int32_t)1048576) - adc_P) - (var2 / 4096))) * 3125;
-  if (p < 0x80000000) {
-    p = (p * 2) / ((uint32_t)var1);
-  } else {
-    p = (p / (uint32_t)var1) * 2;
-  }
 
-  var1 = (((int32_t)BMP280_calib.dig_P9) * ((int32_t)(((p / 8) * (p / 8)) / 8192))) / 4096;
-  var2 = (((int32_t)(p / 4)) * ((int32_t)BMP280_calib.dig_P8)) / 8192;
+  if (p < 0x80000000)
+    p = (p * 2) / ((uint32_t)var1);
+
+  else
+    p = (p / (uint32_t)var1) * 2;
+
+  var1 = (((int32_t)BMP280_calib.dig_P9) * ((int32_t)(((p/8) * (p/8)) / 8192))) / 4096;
+  var2 = (((int32_t)(p/4)) * ((int32_t)BMP280_calib.dig_P8)) / 8192;
+
   p = (uint32_t)((int32_t)p + ((var1 + var2 + (int32_t)BMP280_calib.dig_P7) / 16));
 
   *pres = p;
 
   return 1;
-}
-
 }
 
 // end of driver code.
