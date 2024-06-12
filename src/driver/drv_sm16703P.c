59fcf19bbdee45dee56f23e74ac3b8e1cacca5da
openshwprojects <85486843+openshwprojects@users.noreply.github.com>
2024-06-12 10:54:36 +0200

    SM15155E support
    SM15155E support

--- src/driver/drv_sm16703P.c
+++ src/driver/drv_sm16703P.c
@@ -11,35 +11,24 @@
 #include "../logging/logging.h"
 #include "../mqtt/new_mqtt.h"
 
-#include "drv_spidma.h"
+#include "drv_spiLED.h"
 
-#if WINDOWS
-
-void SPIDMA_Init(struct spi_message *spi_msg) {
-
-}
-void SPIDMA_StartTX(struct spi_message *spi_msg) {
-
-}
-void SPIDMA_StopTX(void) {
-
-}
-
-#endif
-
-static uint8_t data_translate[4] = {0b10001000, 0b10001110, 0b11101000, 0b11101110};
-
-UINT8 *send_buf;
-struct spi_message *spi_msg;
-BOOLEAN initialized = false;
 // Number of pixels that can be addressed
-uint32_t pixel_count = 0;
-// Number of empty bytes to send before pixel data on each frame
-// Likely not needed as the data line should be LOW (reset) between frames anyway
-uint32_t pixel_offset = 0;
-// Number of empty bytes to send after pixel data on each frame
-// Workaround to stuff the SPI buffer with empty bytes after a transmission (s.a. #1055)
-uint32_t pixel_padding = 64;
+uint32_t pixel_count;
+
+void SM16703P_GetPixel(uint32_t pixel, byte *dst) {
+	int i;
+	uint8_t *input;
+
+	if (spiLED.msg == 0)
+		return;
+	
+	input = spiLED.buf + spiLED.ofs + (pixel * 3 * 4);
+
+	for (i = 0; i < 3; i++) {
+		*dst++ = reverse_translate_byte(input + i * 4);
+	}
+}
 
 #define SM16703P_COLOR_ORDER_RGB         0x00
 #define SM16703P_COLOR_ORDER_RBG         0x01
@@ -49,78 +38,6 @@ uint32_t pixel_padding = 64;
 #define SM16703P_COLOR_ORDER_GBR         0x05
 int color_order = SM16703P_COLOR_ORDER_RGB; // default to RGB
 
-static uint8_t translate_2bit(uint8_t input) {
-	//ADDLOG_INFO(LOG_FEATURE_CMD, "Translate 0x%02x to 0x%02x", (input & 0b00000011), data_translate[(input & 0b00000011)]);
-	return data_translate[(input & 0b00000011)];
-}
-
-static uint8_t reverse_translate_2bit(uint8_t input) {
-	uint8_t i;
-	for (i = 0; i < 4; ++i) {
-		if (data_translate[i] == input) {
-			return i;
-		}
-	}
-	return 0;
-}
-
-static byte reverse_translate_byte(uint8_t *input) {
-	byte dst;
-	dst = (reverse_translate_2bit(*input++) << 6);
-	dst |= (reverse_translate_2bit(*input++) << 4);
-	dst |= (reverse_translate_2bit(*input++) << 2);
-	dst |= (reverse_translate_2bit(*input++) << 0);
-	return dst;
-}
-static void translate_byte(uint8_t input, uint8_t *dst) {
-	// return 0x00000000 |
-	// 	   translate_2bit((input >> 6)) |
-	// 	   translate_2bit((input >> 4)) |
-	// 	   translate_2bit((input >> 2)) |
-	// 	   translate_2bit(input);
-	*dst++ = translate_2bit((input >> 6));
-	*dst++ = translate_2bit((input >> 4));
-	*dst++ = translate_2bit((input >> 2));
-	*dst++ = translate_2bit(input);
-
-#if WINDOWS
-	byte test = reverse_translate_byte(dst - 4);
-	if (input != test) {
-		printf("reverse_translate_byte is brken");
-	}
-#endif
-}
-bool SM16703P_GetPixel(uint32_t pixel, byte *dst) {
-	int i;
-	uint8_t *input;
-
-	if (spi_msg == 0)
-		return;
-	
-	input = spi_msg->send_buf + pixel_offset + (pixel * 3 * 4);
-
-	for (i = 0; i < 3; i++) {
-		*dst++ = reverse_translate_byte(input + i * 4);
-	}
-}
-void SM16703P_setRaw(int start_offset, const char *s, int push) {
-	// start offset is in bytes, and we do 2 bits per dst byte, so *4
-	uint8_t *dst = spi_msg->send_buf + pixel_offset + start_offset*4;
-
-	// parse hex string like FFAABB0011 byte by byte
-	while (s[0] && s[1]) {
-		byte b;
-		b = hexbyte(s);
-		*dst++ = translate_2bit((b >> 6));
-		*dst++ = translate_2bit((b >> 4));
-		*dst++ = translate_2bit((b >> 2));
-		*dst++ = translate_2bit(b);
-		s += 2;
-	}
-	if (push) {
-		SPIDMA_StartTX(spi_msg);
-	}
-}
 
 bool SM16703P_VerifyPixel(uint32_t pixel, byte r, byte g, byte b) {
 	byte real[3];
@@ -138,7 +55,7 @@ bool SM16703P_VerifyPixel(uint32_t pixel, byte r, byte g, byte b) {
 void SM16703P_setMultiplePixel(uint32_t pixel, uint8_t *data, bool push) {
 
 	// Return if driver is not loaded
-	if (!initialized)
+	if (!spiLED.ready)
 		return;
 
 	// Check max pixel
@@ -146,7 +63,7 @@ void SM16703P_setMultiplePixel(uint32_t pixel, uint8_t *data, bool push) {
 		pixel = pixel_count;
 
 	// Iterate over pixel
-	uint8_t *dst = spi_msg->send_buf + pixel_offset;
+	uint8_t *dst = spiLED.buf + spiLED.ofs;
 	for (uint32_t i = 0; i < pixel; i++) {
 		uint8_t r, g, b;
 		if (color_order == SM16703P_COLOR_ORDER_RGB) {
@@ -194,11 +111,11 @@ void SM16703P_setMultiplePixel(uint32_t pixel, uint8_t *data, bool push) {
 		*dst++ = translate_2bit(b);
 	}
 	if (push) {
-		SPIDMA_StartTX(spi_msg);
+		SPIDMA_StartTX(spiLED.msg);
 	}
 }
 void SM16703P_setPixel(int pixel, int r, int g, int b) {
-	if (!initialized)
+	if (!spiLED.ready)
 		return;
 	// Load data in correct format
 	int b0, b1, b2;
@@ -232,9 +149,9 @@ void SM16703P_setPixel(int pixel, int r, int g, int b) {
 		b1 = b;
 		b2 = r;
 	}
-	translate_byte(b0, spi_msg->send_buf + (pixel_offset + 0 + (pixel * 3 * 4)));
-	translate_byte(b1, spi_msg->send_buf + (pixel_offset + 4 + (pixel * 3 * 4)));
-	translate_byte(b2, spi_msg->send_buf + (pixel_offset + 8 + (pixel * 3 * 4)));
+	translate_byte(b0, spiLED.buf + (spiLED.ofs + 0 + (pixel * 3 * 4)));
+	translate_byte(b1, spiLED.buf + (spiLED.ofs + 4 + (pixel * 3 * 4)));
+	translate_byte(b2, spiLED.buf + (spiLED.ofs + 8 + (pixel * 3 * 4)));
 }
 extern float g_brightness0to100;//TODO
 void SM16703P_setPixelWithBrig(int pixel, int r, int g, int b) {
@@ -253,7 +170,7 @@ void SM16703P_scaleAllPixels(int scale) {
 
 	for (pixel = 0; pixel < pixel_count; pixel++) {
 		for (ofs = 0; ofs < 3; ofs++) {
-			data = spi_msg->send_buf + (pixel_offset + ofs * 4 + (pixel * 3 * 4));
+			data = spiLED.buf + (spiLED.ofs + ofs * 4 + (pixel * 3 * 4));
 			b = reverse_translate_byte(data);
 			b = SCALE8_PIXEL(b, scale);
 			translate_byte(b, data);
@@ -262,7 +179,7 @@ void SM16703P_scaleAllPixels(int scale) {
 }
 void SM16703P_setAllPixels(int r, int g, int b) {
 	int pixel;
-	if (!initialized)
+	if (!spiLED.ready)
 		return;
 	// Load data in correct format
 	int b0, b1, b2;
@@ -297,9 +214,9 @@ void SM16703P_setAllPixels(int r, int g, int b) {
 		b2 = r;
 	}
 	for (pixel = 0; pixel < pixel_count; pixel++) {
-		translate_byte(b0, spi_msg->send_buf + (pixel_offset + 0 + (pixel * 3 * 4)));
-		translate_byte(b1, spi_msg->send_buf + (pixel_offset + 4 + (pixel * 3 * 4)));
-		translate_byte(b2, spi_msg->send_buf + (pixel_offset + 8 + (pixel * 3 * 4)));
+		translate_byte(b0, spiLED.buf + (spiLED.ofs + 0 + (pixel * 3 * 4)));
+		translate_byte(b1, spiLED.buf + (spiLED.ofs + 4 + (pixel * 3 * 4)));
+		translate_byte(b2, spiLED.buf + (spiLED.ofs + 8 + (pixel * 3 * 4)));
 	}
 }
 
@@ -311,7 +228,7 @@ commandResult_t SM16703P_CMD_setRaw(const void *context, const char *cmd, const 
 	Tokenizer_TokenizeString(args, 0);
 	bPush = Tokenizer_GetArgInteger(0);
 	ofs = Tokenizer_GetArgInteger(1);
-	SM16703P_setRaw(ofs, Tokenizer_GetArg(2), bPush);
+	SPILED_SetRawHexString(ofs, Tokenizer_GetArg(2), bPush);
 	return CMD_RES_OK;
 }
 commandResult_t SM16703P_CMD_setPixel(const void *context, const char *cmd, const char *args, int flags) {
@@ -348,18 +265,18 @@ commandResult_t SM16703P_CMD_setPixel(const void *context, const char *cmd, cons
 		SM16703P_setPixel(pixel, r, g, b);
 
 		ADDLOG_INFO(LOG_FEATURE_CMD, "Raw Data 0x%02x 0x%02x 0x%02x 0x%02x - 0x%02x 0x%02x 0x%02x 0x%02x - 0x%02x 0x%02x 0x%02x 0x%02x",
-			spi_msg->send_buf[pixel_offset + 0 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 1 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 2 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 3 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 4 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 5 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 6 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 7 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 8 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 9 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 10 + (pixel * 3 * 4)],
-			spi_msg->send_buf[pixel_offset + 11 + (pixel * 3 * 4)]);
+			spiLED.buf[spiLED.ofs + 0 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 1 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 2 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 3 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 4 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 5 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 6 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 7 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 8 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 9 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 10 + (pixel * 3 * 4)],
+			spiLED.buf[spiLED.ofs + 11 + (pixel * 3 * 4)]);
 	}
 
 
@@ -397,98 +314,61 @@ commandResult_t SM16703P_InitForLEDCount(const void *context, const char *cmd, c
 		}
 		else if (!stricmp(format, "GBR")) {
 			color_order = SM16703P_COLOR_ORDER_GBR;
-		} else {
+		}
+		else {
 			ADDLOG_INFO(LOG_FEATURE_CMD, "Invalid color format, should be combination of R,G,B", format);
 			return CMD_RES_ERROR;
 		}
 	}
-	// Third arg (optional, default "0"): pixel_offset to prepend to each transmission
+	// Third arg (optional, default "0"): spiLED.ofs to prepend to each transmission
 	if (Tokenizer_GetArgsCount() > 2) {
-		pixel_offset = Tokenizer_GetArgIntegerRange(2, 0, 255);
+		spiLED.ofs = Tokenizer_GetArgIntegerRange(2, 0, 255);
 	}
-	// Fourth arg (optional, default "64"): pixel_padding to append to each transmission
-	if (Tokenizer_GetArgsCount() > 3) {
-		pixel_padding = Tokenizer_GetArgIntegerRange(3, 0, 255);
-	}
+	// Fourth arg (optional, default "64"): spiLED.padding to append to each transmission
+	//spiLED.padding = 64;
+	//if (Tokenizer_GetArgsCount() > 3) {
+	//	spiLED.padding = Tokenizer_GetArgIntegerRange(3, 0, 255);
+	//}
 
 	ADDLOG_INFO(LOG_FEATURE_CMD, "Register driver with %i LEDs", pixel_count);
-
-	// Prepare buffer
-	uint32_t buffer_size = pixel_offset + (pixel_count * 3 * 4) + pixel_padding; //Add `pixel_offset` bytes for "Reset"
-	send_buf = (UINT8 *)os_malloc(sizeof(UINT8) * (buffer_size)); //18LEDs x RGB x 4Bytes
-	int i;
-
-	// Fill `pixel_offset` slice of the buffer with zero
-	for (i = 0; i < pixel_offset; i++) {
-		send_buf[i] = 0;
-	}
-	// Fill data slice of the buffer with data bits that decode to black color
-	for (i = pixel_offset; i < buffer_size - pixel_padding; i++) {
-		send_buf[i] = 0b10001000;
-	}
-	// Fill `pixel_padding` slice of the buffer with zero
-	for (i = buffer_size - pixel_padding; i < buffer_size; i++) {
-		send_buf[i] = 0;
-	}
-
-	spi_msg = os_malloc(sizeof(struct spi_message));
-	spi_msg->send_buf = send_buf;
-	spi_msg->send_len = buffer_size;
-
-	SPIDMA_Init(spi_msg);
-
-	initialized = true;
+	
+	// each pixel is RGB, so 3 bytes per pixel
+	SPILED_InitDMA(pixel_count * 3);
 
 	return CMD_RES_OK;
 }
 
 void SM16703P_Show() {
-	SPIDMA_StartTX(spi_msg);
+	SPIDMA_StartTX(spiLED.msg);
 }
 static commandResult_t SM16703P_StartTX(const void *context, const char *cmd, const char *args, int flags) {
-	if (!initialized)
+	if (!spiLED.ready)
 		return CMD_RES_ERROR;
 	SM16703P_Show();
 	return CMD_RES_OK;
 }
-static commandResult_t SM16703P_CMD_sendBytes(const void *context, const char *cmd, const char *args, int flags) {
-	if (!initialized)
-		return CMD_RES_ERROR;
-	const char *s = args;
-	int i = 0;
-	while (*s && s[1]) {
-		*(spi_msg->send_buf + (pixel_offset + i)) = hexbyte(s);
-		s += 2;
-		i++;
-	}
-	SPIDMA_StartTX(spi_msg);
-	return CMD_RES_OK;
-}
+//static commandResult_t SM16703P_CMD_sendBytes(const void *context, const char *cmd, const char *args, int flags) {
+//	if (!spiLED.ready)
+//		return CMD_RES_ERROR;
+//	const char *s = args;
+//	int i = spiLED.ofs;
+//	while (*s && s[1]) {
+//		*(spiLED.buf + (i)) = hexbyte(s);
+//		s += 2;
+//		i++;
+//	}
+//	while (i < spiLED.msg->send_len) {
+//		*(spiLED.buf + (i)) = 0;
+//	}
+//	SPIDMA_StartTX(spiLED.msg);
+//	return CMD_RES_OK;
+//}
 
 // startDriver SM16703P
 // backlog startDriver SM16703P; SM16703P_Test
 void SM16703P_Init() {
+	SPILED_Init();
 
-	initialized = false;
-
-	uint32_t val;
-#if PLATFORM_BK7231N
-	val = GFUNC_MODE_SPI_USE_GPIO_14;
-	sddev_control(GPIO_DEV_NAME, CMD_GPIO_ENABLE_SECOND, &val);
-
-	val = GFUNC_MODE_SPI_USE_GPIO_16_17;
-	sddev_control(GPIO_DEV_NAME, CMD_GPIO_ENABLE_SECOND, &val);
-
-	UINT32 param;
-	param = PCLK_POSI_SPI;
-	sddev_control(ICU_DEV_NAME, CMD_CONF_PCLK_26M, &param);
-
-	param = PWD_SPI_CLK_BIT;
-	sddev_control(ICU_DEV_NAME, CMD_CLK_PWR_UP, &param);
-#else
-
-
-#endif
 	//cmddetail:{"name":"SM16703P_Init","args":"[NumberOfLEDs][ColorOrder]",
 	//cmddetail:"descr":"This will setup LED driver for a strip with given number of LEDs. Please note that it also works for WS2812B and similiar LEDs. You can optionally set the color order with either RGB, RBG, BRG, BGB, GRB or GBR (default RGB). See [tutorial](https://www.elektroda.com/rtvforum/topic4036716.html).",
 	//cmddetail:"fn":"NULL);","file":"driver/drv_sm16703P.c","requires":"",
@@ -510,6 +390,6 @@ void SM16703P_Init() {
 	//cmddetail:"examples":""}
 	CMD_RegisterCommand("SM16703P_SetRaw", SM16703P_CMD_setRaw, NULL);
 
-	CMD_RegisterCommand("SM16703P_SendBytes", SM16703P_CMD_sendBytes, NULL);
+	//CMD_RegisterCommand("SM16703P_SendBytes", SM16703P_CMD_sendBytes, NULL);
 }
 #endif
