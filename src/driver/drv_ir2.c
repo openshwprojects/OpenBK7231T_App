8097c71fa520a3ff4bf4baf8094a0b6fffc9ffa5
Tester23 <openshwprojects@gmail.com>
2024-06-28 16:48:16 +0200

    Update drv_ir2.c

--- src/driver/drv_ir2.c
+++ src/driver/drv_ir2.c
@@ -4,7 +4,6 @@
 
 #if PLATFORM_BEKEN
 
-extern "C" {
 
 #include "include.h"
 #include "arm_arch.h"
@@ -26,10 +25,15 @@ extern "C" {
 #include "../../beken378/driver/i2c/i2c1.h"
 #include "../../beken378/driver/gpio/gpio.h"
 
+
 #endif
 
-void IR2_Init() {
-	int txpin = 26;
+static UINT32 ir_chan = BKTIMER0;
+static UINT32 ir_div = 1;
+static UINT32 ir_periodus = 50;
+int txpin = 26;
+
+static commandResult_t CMD_IR2_Test1(const void* context, const char* cmd, const char* args, int cmdFlags) {
 	int pwmIndex = PIN_GetPWMIndexForPinIndex(txpin);
 	// is this pin capable of PWM?
 	if (pwmIndex != -1) {
@@ -44,6 +48,60 @@ void IR2_Init() {
 #endif
 		bk_pwm_start((bk_pwm_t)pwmIndex);
 	}
+
+	return CMD_RES_OK;
+}
+int state = 0;
+void Test2_ISR(UINT8 t) {
+	state = !state;
+	bk_gpio_output(txpin, state);
+}
+static commandResult_t CMD_IR2_Test2(const void* context, const char* cmd, const char* args, int cmdFlags) {
+	bk_gpio_config_output(txpin);
+
+	timer_param_t params = {
+	 (unsigned char)ir_chan,
+	 (unsigned char)ir_div, // div
+	 ir_periodus, // us
+	 Test2_ISR
+	};
+	//GLOBAL_INT_DECLARATION();
+
+
+	UINT32 res;
+	// test what error we get with an invalid command
+	res = sddev_control((char *)TIMER_DEV_NAME, -1, nullptr);
+
+	if (res == 1) {
+		ADDLOG_INFO(LOG_FEATURE_IR, (char *)"bk_timer already initialised");
+	}
+	else {
+		ADDLOG_ERROR(LOG_FEATURE_IR, (char *)"bk_timer driver not initialised?");
+		if ((int)res == -5) {
+			ADDLOG_INFO(LOG_FEATURE_IR, (char *)"bk_timer sddev not found - not initialised?");
+			return;
+		}
+		return;
+	}
+
+
+	//ADDLOG_INFO(LOG_FEATURE_IR, (char *)"ir timer init");
+	// do not need to do this
+	//bk_timer_init();
+	//ADDLOG_INFO(LOG_FEATURE_IR, (char *)"ir timer init done");
+	ADDLOG_INFO(LOG_FEATURE_IR, (char *)"will ir timer setup %u", res);
+	res = sddev_control((char *)TIMER_DEV_NAME, CMD_TIMER_INIT_PARAM_US, &params);
+	ADDLOG_INFO(LOG_FEATURE_IR, (char *)"ir timer setup %u", res);
+	res = sddev_control((char *)TIMER_DEV_NAME, CMD_TIMER_UNIT_ENABLE, &ir_chan);
+	ADDLOG_INFO(LOG_FEATURE_IR, (char *)"ir timer enabled %u", res);
+	return CMD_RES_OK;
+}
+void IR2_Init() {
+	CMD_RegisterCommand("Test1", CMD_IR2_Test1, NULL);
+	CMD_RegisterCommand("Test2", CMD_IR2_Test2, NULL);
+
+
+
 }
 
 
