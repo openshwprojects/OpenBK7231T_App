2de3c94ab09ea676559b18c2a7383f2c3b761815
Tester23 <openshwprojects@gmail.com>
2024-06-29 17:19:53 +0200

    fx

--- src/driver/drv_ir2.c
+++ src/driver/drv_ir2.c
@@ -273,7 +273,7 @@ static commandResult_t CMD_IR2_StartTimer(const void* context, const char* cmd, 
 	// is this pin capable of PWM?
 	if (pwmIndex != -1) {
 		group = get_set_group(pwmIndex);
-		channel = get_set_channel(pwmIndex);10
+		channel = get_set_channel(pwmIndex);
 		uint32_t pwmfrequency = 38000;
 		period = (26000000 / pwmfrequency);
 		duty_on = period * duty_on_frac;
