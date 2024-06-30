917042ba2bf4b367eeddd03c284fccb7b6f66a23
Bjoern3003 <bjoern3003@gmx.de>
2024-06-30 18:53:30 +0200

    Update new_pins.c

--- src/new_pins.c
+++ src/new_pins.c
@@ -1161,6 +1161,8 @@ const char *ChannelType_GetUnit(int type) {
 		return "vAr";
 	case ChType_Illuminance:
 		return "Lux";
+	case ChType_Ph:
+		return "Ph";
 	}
 	return "";
 }
@@ -1208,6 +1210,8 @@ const char *ChannelType_GetTitle(int type) {
 		return "ReactivePower";
 	case ChType_Illuminance:
 		return "Illuminance";
+	case ChType_Ph:
+		return "Ph Water Quality";
 	case ChType_ReadOnly:
 	case ChType_ReadOnly_div10:
 	case ChType_ReadOnly_div100:
