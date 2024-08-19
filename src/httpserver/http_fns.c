1b2fc53f7dd067a4a737b557fa6e1dc6d8582b4f
divadiow <62958974+divadiow@users.noreply.github.com>
2024-08-19 16:10:37 +0100

    grammar and missing colon (#1318)

--- src/httpserver/http_fns.c
+++ src/httpserver/http_fns.c
@@ -765,7 +765,7 @@ int http_fn_index(http_request_t* request) {
 		}
 		hprintf255(request, "</h5>");
 	}
-	hprintf255(request, "<h5>Cfg size: %i, change counter: %i, ota counter: %i, boot incompletes %i (might change to 0 if you wait to 30 sec)!</h5>",
+	hprintf255(request, "<h5>Cfg size: %i, change counter: %i, ota counter: %i, incomplete boots: %i (might change to 0 if you wait to 30 sec)!</h5>",
 		sizeof(g_cfg), g_cfg.changeCounter, g_cfg.otaCounter, g_bootFailures);
 
   // display temperature - thanks to giedriuslt
