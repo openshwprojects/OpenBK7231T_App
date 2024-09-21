20d21d0a537e95f537398f88fbd3908d8e3875e0
openshwprojects <85486843+openshwprojects@users.noreply.github.com>
2024-09-21 07:21:19 +0200

    testing (#1354)
    * run
    
    * print socketg error on change
    
    * typo
    
    * resa
    
    * fix??
    
    * try
    
    * try error
    
    * fix error, add more info
    
    * some tests
    
    * more self tests
    
    * tests

--- src/selftest/selftest_main.c
+++ src/selftest/selftest_main.c
@@ -6,9 +6,11 @@ static int g_selfTestErrors = 0;
 int g_selfTestsMode = 0;
 
 void SelfTest_Failed(const char *file, const char *function, int line, const char *exp) {
+	g_selfTestErrors++;
+
 	printf("ERROR: SelfTest failed for %s\n", exp);
 	printf("Check %s - %s - line %i\n", file, function, line);
-	g_selfTestErrors++;
+	printf("Total SelfTest errors so far: %i\n", g_selfTestErrors);
 
 	system("pause");
 }
