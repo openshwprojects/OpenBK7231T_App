6cb99cbd72274b7a2137d3770511267915c214fd
Tester23 <openshwprojects@gmail.com>
2024-10-26 20:40:18 +0200

    defines

--- src/driver/drv_main.c
+++ src/driver/drv_main.c
@@ -175,12 +175,12 @@ static driver_t g_drivers[] = {
 	//drvdetail:"requires":""}
 	{ "MAX6675",	MAX6675_Init,		MAX6675_RunEverySecond,			NULL, NULL, NULL, NULL, false },
 #endif
-#if 1
-	//drvdetail:{"name":"MAX318555",
+#if ENABLE_DRIVER_MAX31855
+	//drvdetail:{"name":"MAX31855",
 	//drvdetail:"title":"TODO",
 	//drvdetail:"descr":"T",
 	//drvdetail:"requires":""}
-	{ "MAX318555",	MAX31855_Init,		MAX31855_RunEverySecond,			NULL, NULL, NULL, NULL, false },
+	{ "MAX31855",	MAX31855_Init,		MAX31855_RunEverySecond,			NULL, NULL, NULL, NULL, false },
 #endif
 #if ENABLE_DRIVER_PT6523
 	//drvdetail:{"name":"PT6523",
