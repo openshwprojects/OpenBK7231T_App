a74fbc98a75dcf8e259a6b1d973247c59ee1d57f
MaxineMuster <146550015+MaxineMuster@users.noreply.github.com>
2024-11-13 19:19:08 +0100

    Update http_fns.c

--- src/httpserver/http_fns.c
+++ src/httpserver/http_fns.c
@@ -2508,6 +2508,8 @@ int http_fn_cfg(http_request_t* request) {
 			<input type=\"submit\" value=\"Set device clock to browser time\">\
 			</form>");
 #endif
+
+
 #if 0
 #if PLATFORM_BK7231T | PLATFORM_BK7231N
 	{
