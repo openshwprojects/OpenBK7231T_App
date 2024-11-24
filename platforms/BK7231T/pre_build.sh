6f1f2f2d6d22de62a196329d5733b492a7c63926
divadiow <divadiow@hotmail.com>
2024-11-24 17:28:47 +0000

    done

--- platforms/BK7231T/pre_build.sh
+++ platforms/BK7231T/pre_build.sh
@@ -6,5 +6,6 @@ for X in $(find platforms/BK7231T/override/ -type f);do
 S=${X};
 D=${X#platforms/BK7231T/override/};
 [ -e $D ] && echo "PREBUILD: replacing file\n\t$D\n\twith file\n\t$S" || echo "PREBUILD: adding file\n\t$S\n\tas\n\t$D"
-cp $S $D;
+#	cp $S $D;
+done
 IFS=$OFS
