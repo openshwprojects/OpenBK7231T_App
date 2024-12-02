64718b1cf5df4a8c2e990b73ade2baca61a48b30
Tester23 <openshwprojects@gmail.com>
2024-12-02 10:45:57 +0100

    TEST1

--- src/driver/drv_openWeatherMap.c
+++ src/driver/drv_openWeatherMap.c
@@ -9,8 +9,28 @@
 #include "../httpserver/new_http.h"
 #include "drv_ntp.h"
 
+xTaskHandle g_weather_thread = NULL;
+
+static void weather_thread(beken_thread_arg_t arg) {
+
+}
+void startWeatherThread() {
+	OSStatus err = kNoErr;
+
+	err = rtos_create_thread(&g_weather_thread, BEKEN_APPLICATION_PRIORITY,
+		"OWM",
+		(beken_thread_function_t)weather_thread,
+		0x800,
+		(beken_thread_arg_t)0);
+	if (err != kNoErr)
+	{
+		ADDLOG_ERROR(LOG_FEATURE_HTTP, "create \"OWM\" thread failed with %i!\r\n", err);
+	}
+}
 static commandResult_t CMD_OWM_Request(const void *context, const char *cmd, const char *args, int flags) {
 	Tokenizer_TokenizeString(args, TOKENIZER_ALLOW_QUOTES);
+
+	startWeatherThread();
 
 	return CMD_RES_OK;
 }
