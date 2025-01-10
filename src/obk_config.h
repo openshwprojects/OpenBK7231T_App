776a510d6d7864d56c0a7b0cf97ea94c0ec5c911
Tester23 <85486843+openshwprojects@users.noreply.github.com>
2025-01-11 00:03:15 +0100

    Update obk_config.h

--- src/obk_config.h
+++ src/obk_config.h
@@ -30,7 +30,8 @@
 
 #elif PLATFORM_W600
 
-// Some limited drivers are supported on W600, OBK_DISABLE_ALL_DRIVERS is not defined
+// parse things like $CH1 or $hour etc
+#define ENABLE_EXPAND_CONSTANT	1
 #define	ENABLE_HA_DISCOVERY		1
 #define ENABLE_MQTT 1
 #define ENABLE_TASMOTADEVICEGROUPS 1
@@ -41,6 +42,7 @@
 #define ENABLE_TASMOTA_JSON		1
 #define ENABLE_DRIVER_DS1820		1
 #define OBK_OTA_EXTENSION 		".img"
+#define ENABLE_OBK_SCRIPTING			1
 
 #elif PLATFORM_W800
 
