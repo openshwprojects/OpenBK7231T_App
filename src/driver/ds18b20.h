d4828cd9d6f7664b14fa1da29d2c28cb40584d5e
MaxineMuster <146550015+MaxineMuster@users.noreply.github.com>
2025-02-07 15:51:26 +0100

    fix ds18b20.h

--- src/driver/ds18b20.h
+++ src/driver/ds18b20.h
@@ -12,7 +12,7 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+#pragma once
 #ifndef DS18B20_H_  
 #define DS18B20_H_
 
