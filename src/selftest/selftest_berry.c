7d6bfc7dcf35d3347dff8dd59ab296207d5aa7a4
Tester23 <85486843+openshwprojects@users.noreply.github.com>
2025-03-30 19:08:34 +0200

    fx

--- src/selftest/selftest_berry.c
+++ src/selftest/selftest_berry.c
@@ -149,7 +149,6 @@ void Test_Berry_Fibonacci() {
 		"tester = module('tester')\n"
 		"\n"
 		"tester.fib = def(n)\n"
-		"def fib(n)\n"
 		"	if n <= 1 return n end\n"
 		"	return fib(n - 1) + fib(n - 2)\n"
 		"end\n"
