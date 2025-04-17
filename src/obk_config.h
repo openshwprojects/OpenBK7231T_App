56648d8da06e69449a94ebf710c0f1127255aeb0
Tester23 <85486843+openshwprojects@users.noreply.github.com>
2025-04-17 22:39:10 +0200

    Update obk_config.h

--- src/obk_config.h
+++ src/obk_config.h
@@ -235,50 +235,6 @@
 //#define ENABLE_I2C_MCP23017		1
 //#define ENABLE_I2C_LCD_PCF8574	1
 
-#if defined(VARIANT) && VARIANT == berry
-#define ENABLE_OBK_BERRY		1
-
-#elif defined(VARIANT) && VARIANT == tuyamcu
-//
-// ==== TuyaMCU mode ====
-//
-// Allow Berry - it can be used to parse DataPoints
-#define ENABLE_OBK_BERRY		1
-// Disable power metering chips - they are never used with TuyaMCU
-#undef ENABLE_DRIVER_BL0937
-#undef ENABLE_DRIVER_BL0942
-#undef ENABLE_DRIVER_BL0942SPI
-#undef ENABLE_DRIVER_CSE7766
-// Disable I2C-like LED drivers
-#undef ENABLE_DRIVER_LED
-#undef ENABLE_DRIVER_KP18058
-// Disable ADC smoother
-#undef ENABLE_DRIVER_ADCSMOOTHER
-// Disable brridge on pins
-#undef ENABLE_DRIVER_BRIDGE
-// disable WS2812
-#undef ENABLE_DRIVER_SM16703P
-#undef ENABLE_DRIVER_PIXELANIM
-#undef ENABLE_DRIVER_SM15155E
-
-#elif defined(VARIANT) && VARIANT == energy
-//
-// ==== Energy meter plug with BL0942 or BL0937 etc ====
-//
-// Allow graphs
-#define ENABLE_DRIVER_CHARTS		1
-// Disable I2C-like LED drivers
-#undef ENABLE_DRIVER_LED
-#undef ENABLE_DRIVER_KP18058
-// disable TuyaMCU 
-#undef ENABLE_DRIVER_TUYAMCU
-// disable WS2812
-#undef ENABLE_DRIVER_SM16703P
-#undef ENABLE_DRIVER_PIXELANIM
-#undef ENABLE_DRIVER_SM15155E
-
-#endif
-
 #elif PLATFORM_LN882H
 
 
