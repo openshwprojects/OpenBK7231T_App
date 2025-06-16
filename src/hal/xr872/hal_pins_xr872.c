f283f7d0d65c920a28d245e7338387e657a27e99
divadiow <divadiow@hotmail.com>
2025-06-16 17:14:32 +0100

    hmm. what does this do

--- src/hal/xr872/hal_pins_xr872.c
+++ src/hal/xr872/hal_pins_xr872.c
@@ -1,8 +1,7 @@
-#ifdef PLATFORM_XR809
+#ifdef PLATFORM_XR872
 
 #include "../../new_common.h"
 #include "../../logging/logging.h"
-
 #include "driver/chip/hal_gpio.h"
 
 typedef struct xr809pin_s {
@@ -11,9 +10,8 @@ typedef struct xr809pin_s {
 	int pin;
 } xr809pin_t;
 
-// https://developer.tuya.com/en/docs/iot/xr3-datasheet?id=K98s9168qi49g
-
 static xr809pin_t g_xrPins[] = {
+	// Original known pins
 	{ "PA19", GPIO_PORT_A, GPIO_PIN_19 },
 	{ "PB03", GPIO_PORT_B, GPIO_PIN_3 },
 	{ "PA12", GPIO_PORT_A, GPIO_PIN_12 },
@@ -27,7 +25,46 @@ static xr809pin_t g_xrPins[] = {
 	{ "PA22", GPIO_PORT_A, GPIO_PIN_22 },
 	{ "PB02", GPIO_PORT_B, GPIO_PIN_2 },
 	{ "PA08", GPIO_PORT_A, GPIO_PIN_8 },
+
+	// XR872-specific additions
+	{ "PA00", GPIO_PORT_A, GPIO_PIN_0 },
+	{ "PA01", GPIO_PORT_A, GPIO_PIN_1 },
+	{ "PA02", GPIO_PORT_A, GPIO_PIN_2 },
+	{ "PA03", GPIO_PORT_A, GPIO_PIN_3 },
+	{ "PA04", GPIO_PORT_A, GPIO_PIN_4 },
+	{ "PA05", GPIO_PORT_A, GPIO_PIN_5 },
+	{ "PA09", GPIO_PORT_A, GPIO_PIN_9 },
+	{ "PA10", GPIO_PORT_A, GPIO_PIN_10 },
+	{ "PA11", GPIO_PORT_A, GPIO_PIN_11 },
+	{ "PA13", GPIO_PORT_A, GPIO_PIN_13 },
+	{ "PA17", GPIO_PORT_A, GPIO_PIN_17 },
+	{ "PA18", GPIO_PORT_A, GPIO_PIN_18 },
+
+	{ "PB00", GPIO_PORT_B, GPIO_PIN_0 },
+	{ "PB01", GPIO_PORT_B, GPIO_PIN_1 },
+	{ "PB04", GPIO_PORT_B, GPIO_PIN_4 },
+	{ "PB05", GPIO_PORT_B, GPIO_PIN_5 },
+	{ "PB06", GPIO_PORT_B, GPIO_PIN_6 },
+	{ "PB07", GPIO_PORT_B, GPIO_PIN_7 },
+	{ "PB08", GPIO_PORT_B, GPIO_PIN_8 },
+	{ "PB09", GPIO_PORT_B, GPIO_PIN_9 },
+	{ "PB10", GPIO_PORT_B, GPIO_PIN_10 },
+	{ "PB11", GPIO_PORT_B, GPIO_PIN_11 },
+	{ "PB12", GPIO_PORT_B, GPIO_PIN_12 },
+	{ "PB13", GPIO_PORT_B, GPIO_PIN_13 },
+	{ "PB14", GPIO_PORT_B, GPIO_PIN_14 },
+	{ "PB15", GPIO_PORT_B, GPIO_PIN_15 },
+
+	{ "PC00", GPIO_PORT_C, GPIO_PIN_0 },
+	{ "PC01", GPIO_PORT_C, GPIO_PIN_1 },
+	{ "PC02", GPIO_PORT_C, GPIO_PIN_2 },
+	{ "PC03", GPIO_PORT_C, GPIO_PIN_3 },
+	{ "PC04", GPIO_PORT_C, GPIO_PIN_4 },
+	{ "PC05", GPIO_PORT_C, GPIO_PIN_5 },
+	{ "PC06", GPIO_PORT_C, GPIO_PIN_6 },
+	{ "PC07", GPIO_PORT_C, GPIO_PIN_7 },
 };
+
 int g_numXRPins = sizeof(g_xrPins) / sizeof(g_xrPins[0]);
 
 static void PIN_XR809_GetPortPinForIndex(int index, int *xr_port, int *xr_pin) {
@@ -47,37 +84,32 @@ const char *HAL_PIN_GetPinNameAlias(int index) {
 	return g_xrPins[index].name;
 }
 
-
-
-// XR809 - PWM todo
 int HAL_PIN_CanThisPinBePWM(int index) {
+	// You can improve this later to return 1 for PWM-capable pins
 	return 0;
 }
+
 void HAL_PIN_SetOutputValue(int index, int iVal) {
-	int xr_port; // eg GPIO_PORT_A
-	int xr_pin; // eg. GPIO_PIN_20
-
+	int xr_port;
+	int xr_pin;
 	PIN_XR809_GetPortPinForIndex(index, &xr_port, &xr_pin);
-
 	HAL_GPIO_WritePin(xr_port, xr_pin, iVal);
 }
 
 int HAL_PIN_ReadDigitalInput(int index) {
-	int xr_port; // eg GPIO_PORT_A
-	int xr_pin; // eg. GPIO_PIN_20
-
+	int xr_port;
+	int xr_pin;
 	PIN_XR809_GetPortPinForIndex(index, &xr_port, &xr_pin);
-
-	if (HAL_GPIO_ReadPin(xr_port, xr_pin) == GPIO_PIN_LOW)
-		return 0;
-	return 1;
+	return (HAL_GPIO_ReadPin(xr_port, xr_pin) == GPIO_PIN_LOW) ? 0 : 1;
 }
+
 void HAL_PIN_Setup_Input_Pulldown(int index) {
-
+	// Not implemented — can be added later
 }
+
 void HAL_PIN_Setup_Input_Pullup(int index) {
-	int xr_port; // eg GPIO_PORT_A
-	int xr_pin; // eg. GPIO_PIN_20
+	int xr_port;
+	int xr_pin;
 	GPIO_InitParam param;
 
 	PIN_XR809_GetPortPinForIndex(index, &xr_port, &xr_pin);
@@ -87,9 +119,10 @@ void HAL_PIN_Setup_Input_Pullup(int index) {
 	param.pull = GPIO_PULL_UP;
 	HAL_GPIO_Init(xr_port, xr_pin, &param);
 }
+
 void HAL_PIN_Setup_Input(int index) {
-	int xr_port; // eg GPIO_PORT_A
-	int xr_pin; // eg. GPIO_PIN_20
+	int xr_port;
+	int xr_pin;
 	GPIO_InitParam param;
 
 	PIN_XR809_GetPortPinForIndex(index, &xr_port, &xr_pin);
@@ -101,32 +134,29 @@ void HAL_PIN_Setup_Input(int index) {
 }
 
 void HAL_PIN_Setup_Output(int index) {
+	int xr_port;
+	int xr_pin;
 	GPIO_InitParam param;
-	int xr_port; // eg GPIO_PORT_A
-	int xr_pin; // eg. GPIO_PIN_20
 
 	PIN_XR809_GetPortPinForIndex(index, &xr_port, &xr_pin);
 
-	/*set pin driver capability*/
 	param.driving = GPIO_DRIVING_LEVEL_1;
 	param.mode = GPIOx_Pn_F1_OUTPUT;
 	param.pull = GPIO_PULL_NONE;
 	HAL_GPIO_Init(xr_port, xr_pin, &param);
 }
 
-
 void HAL_PIN_PWM_Stop(int pinIndex) {
+	// Not yet implemented
 }
 
 void HAL_PIN_PWM_Start(int index, int freq) {
-
+	// Not yet implemented
 }
-void HAL_PIN_PWM_Update(int index, float value) {
 
-	if(value<0)
-		value = 0;
-	if(value>100)
-		value = 100;
+void HAL_PIN_PWM_Update(int index, float value) {
+	if (value < 0) value = 0;
+	if (value > 100) value = 100;
 }
 
 unsigned int HAL_GetGPIOPin(int index) {
@@ -137,4 +167,3 @@ unsigned int HAL_GetGPIOPin(int index) {
 }
 
 #endif
-
