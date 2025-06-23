ed8a1071b26bc78eddcd4085f638ec85aa9c1edd
Tester23 <85486843+openshwprojects@users.noreply.github.com>
2025-06-23 13:25:04 +0200

    Update drv_spi_flash.c

--- src/driver/drv_spi_flash.c
+++ src/driver/drv_spi_flash.c
@@ -20,16 +20,9 @@
 
 #define SPI_USLEEP(x)
 
-//#ifdef PLATFORM_BEKEN
-//#define OBK_DISABLE_INTERRUPTS \
-//	GLOBAL_INT_DECLARATION(); \
-//	GLOBAL_INT_DISABLE();
-//#define OBK_ENABLE_INTERRUPTS \
-//	GLOBAL_INT_RESTORE();
-//#else
+
 #define OBK_ENABLE_INTERRUPTS 
 #define OBK_DISABLE_INTERRUPTS
-//#endif
 
 void spi_flash_read_id(softSPI_t* spi, byte* jedec_id) {
 	OBK_DISABLE_INTERRUPTS;
