c5ef0d3e1dbcd0387a76f09a9376e1b510d43084
Tester23 <85486843+openshwprojects@users.noreply.github.com>
2025-07-05 20:14:37 +0200

    test2

--- src/driver/drv_spi_flash.c
+++ src/driver/drv_spi_flash.c
@@ -31,7 +31,8 @@
 // http://192.168.0.166/api/lfs/
 // To get file, use GET:
 // http://192.168.0.166/api/lfs/
-#if (PLATFORM_BK7231T || PLATFORM_BK7231N) && !PLATFORM_BEKEN_NEW
+#if 0
+//(PLATFORM_BK7231T || PLATFORM_BK7231N) && !PLATFORM_BEKEN_NEW
 
 #include "include.h"
 #include "arm_arch.h"
@@ -144,54 +145,54 @@ byte FastSPI_Read(softSPI_t *spi) {
 #endif
 
 void spi_flash_read_id(softSPI_t* spi, byte* jedec_id) {
-	//SPI_Begin(spi);
-	//SPI_Send(spi, READ_ID_FLASH_CMD);
-	//jedec_id[0] = SPI_Read(spi);
-	//jedec_id[1] = SPI_Read(spi);
-	//jedec_id[2] = SPI_Read(spi);
-	//SPI_End(spi);
+	SPI_Begin(spi);
+	SPI_Send(spi, READ_ID_FLASH_CMD);
+	jedec_id[0] = SPI_Read(spi);
+	jedec_id[1] = SPI_Read(spi);
+	jedec_id[2] = SPI_Read(spi);
+	SPI_End(spi);
 }
 
 
 
 static void flash_wait_for(softSPI_t* spi, byte mask) {
-	//byte status;
-	//int loops = 0;
-	//while (1) {
-	//	SPI_Begin(spi);
-	//	SPI_Send(spi, READ_STATUS_REG_CMD);
-	//	status = SPI_Read(spi);
-	//	SPI_End(spi);
-	//	if (!(status & mask)) {
-	//		break;
-	//	}
-	//	loops++;
-	//	rtos_delay_milliseconds(loops);
-	//}
+	byte status;
+	int loops = 0;
+	while (1) {
+		SPI_Begin(spi);
+		SPI_Send(spi, READ_STATUS_REG_CMD);
+		status = SPI_Read(spi);
+		SPI_End(spi);
+		if (!(status & mask)) {
+			break;
+		}
+		loops++;
+		rtos_delay_milliseconds(loops);
+	}
 
-	////ADDLOG_INFO(LOG_FEATURE_CMD, "flash_wait_for: done %i loops\n", loops);
+	//ADDLOG_INFO(LOG_FEATURE_CMD, "flash_wait_for: done %i loops\n", loops);
 
 }
 
 static void flash_wait_while_busy(softSPI_t* spi) {
-	//flash_wait_for(spi, STATUS_BUSY_MASK);
+	flash_wait_for(spi, STATUS_BUSY_MASK);
 }
 void spi_flash_read(softSPI_t* spi, int adr, byte* out, int cnt) {
-	//int i;
+	int i;
 
-	//OBK_DISABLE_INTERRUPTS;
+	OBK_DISABLE_INTERRUPTS;
 
-	//SPI_Begin(spi);
-	//SPI_Send(spi, READ_FLASH_CMD);
-	//SPI_Send(spi, (adr >> 16) & 0xFF);
-	//SPI_Send(spi, (adr >> 8) & 0xFF);
-	//SPI_Send(spi, adr & 0xFF);
-	//for (i = 0; i < cnt; i++) {
-	//	out[i] = SPI_Read(spi);
-	//}
-	//SPI_End(spi);
+	SPI_Begin(spi);
+	SPI_Send(spi, READ_FLASH_CMD);
+	SPI_Send(spi, (adr >> 16) & 0xFF);
+	SPI_Send(spi, (adr >> 8) & 0xFF);
+	SPI_Send(spi, adr & 0xFF);
+	for (i = 0; i < cnt; i++) {
+		out[i] = SPI_Read(spi);
+	}
+	SPI_End(spi);
 
-	//OBK_ENABLE_INTERRUPTS;
+	OBK_ENABLE_INTERRUPTS;
 }
 
 //static void flash_wait_for_wel(softSPI_t* spi) {
@@ -199,63 +200,63 @@ void spi_flash_read(softSPI_t* spi, int adr, byte* out, int cnt) {
 //}
 
 void spi_flash_erase(softSPI_t* spi) {
-	//OBK_DISABLE_INTERRUPTS;
+	OBK_DISABLE_INTERRUPTS;
 
-	//SPI_Begin(spi);
-	//SPI_Send(spi, WRITEENABLE_FLASH_CMD);
-	//SPI_End(spi);
+	SPI_Begin(spi);
+	SPI_Send(spi, WRITEENABLE_FLASH_CMD);
+	SPI_End(spi);
 
-	//SPI_Begin(spi);
-	//SPI_Send(spi, ERASE_WHOLE_FLASH_CMD);
-	//SPI_End(spi);
+	SPI_Begin(spi);
+	SPI_Send(spi, ERASE_WHOLE_FLASH_CMD);
+	SPI_End(spi);
 
-	//flash_wait_while_busy(spi);
+	flash_wait_while_busy(spi);
 
-	//OBK_ENABLE_INTERRUPTS;
+	OBK_ENABLE_INTERRUPTS;
 }
 
 void spi_flash_write2(softSPI_t* spi, int adr, const byte* data, int cnt) {
-	//int remaining = cnt;
-	//int offset = 0;
+	int remaining = cnt;
+	int offset = 0;
 
-	//OBK_DISABLE_INTERRUPTS;
+	OBK_DISABLE_INTERRUPTS;
 
-	////ADDLOG_INFO(LOG_FEATURE_CMD, "spi_flash_write2 %i at %i\n", cnt, adr);
+	//ADDLOG_INFO(LOG_FEATURE_CMD, "spi_flash_write2 %i at %i\n", cnt, adr);
 
-	//while (remaining > 0) {
-	//	int page_offset = adr % 256;
-	//	int write_len = 256 - page_offset;
-	//	if (write_len > remaining)
-	//		write_len = remaining;
+	while (remaining > 0) {
+		int page_offset = adr % 256;
+		int write_len = 256 - page_offset;
+		if (write_len > remaining)
+			write_len = remaining;
 
 
-	//	SPI_Begin(spi);
-	//	SPI_Send(spi, WRITEENABLE_FLASH_CMD);
-	//	SPI_End(spi);
+		SPI_Begin(spi);
+		SPI_Send(spi, WRITEENABLE_FLASH_CMD);
+		SPI_End(spi);
 
-	//	//ADDLOG_INFO(LOG_FEATURE_CMD, "Write fragmnent %i at %i\n", write_len, adr);
-	//	SPI_Begin(spi);
-	//	SPI_Send(spi, WRITE_FLASH_CMD);
-	//	SPI_Send(spi, (adr >> 16) & 0xFF);
-	//	SPI_Send(spi, (adr >> 8) & 0xFF);
-	//	SPI_Send(spi, adr & 0xFF);
-	//	for (int i = 0; i < write_len; i++) {
-	//		SPI_Send(spi, data[offset + i]);
-	//	}
-	//	SPI_End(spi);
+		//ADDLOG_INFO(LOG_FEATURE_CMD, "Write fragmnent %i at %i\n", write_len, adr);
+		SPI_Begin(spi);
+		SPI_Send(spi, WRITE_FLASH_CMD);
+		SPI_Send(spi, (adr >> 16) & 0xFF);
+		SPI_Send(spi, (adr >> 8) & 0xFF);
+		SPI_Send(spi, adr & 0xFF);
+		for (int i = 0; i < write_len; i++) {
+			SPI_Send(spi, data[offset + i]);
+		}
+		SPI_End(spi);
 
-	//	flash_wait_while_busy(spi);
+		flash_wait_while_busy(spi);
 
-	//	adr += write_len;
-	//	offset += write_len;
-	//	remaining -= write_len;
-	//}
+		adr += write_len;
+		offset += write_len;
+		remaining -= write_len;
+	}
 
-	//OBK_ENABLE_INTERRUPTS;
+	OBK_ENABLE_INTERRUPTS;
 }
 
 void spi_flash_erase_sector(softSPI_t* spi, int addr) {
-	/*OBK_DISABLE_INTERRUPTS;
+	OBK_DISABLE_INTERRUPTS;
 
 	SPI_Begin(spi);
 	SPI_Send(spi, WRITEENABLE_FLASH_CMD);
@@ -270,72 +271,72 @@ void spi_flash_erase_sector(softSPI_t* spi, int addr) {
 
 	flash_wait_while_busy(spi);
 
-	OBK_ENABLE_INTERRUPTS;*/
+	OBK_ENABLE_INTERRUPTS;
 }
 
 void flash_test_pages(softSPI_t* spi, int baseAddr, int length, byte pattern) {
-	//int i, err = 0;
-	//byte *writeBuf = malloc(length);
-	//byte *readBuf = malloc(length);
+	int i, err = 0;
+	byte *writeBuf = malloc(length);
+	byte *readBuf = malloc(length);
 
-	//for (i = 0; i < length; i++)
-	//	writeBuf[i] = pattern;
+	for (i = 0; i < length; i++)
+		writeBuf[i] = pattern;
 
-	//if (0) {
-	//	spi_flash_erase(spi);
-	//}
-	//else {
-	//	int startAddr = baseAddr & ~(0xFFF); // align down to 4KB
-	//	int endAddr = (baseAddr + length - 1) & ~(0xFFF); // align down last used address
+	if (0) {
+		spi_flash_erase(spi);
+	}
+	else {
+		int startAddr = baseAddr & ~(0xFFF); // align down to 4KB
+		int endAddr = (baseAddr + length - 1) & ~(0xFFF); // align down last used address
 
-	//	for (int addr = startAddr; addr <= endAddr; addr += 4096) {
-	//		spi_flash_erase_sector(spi, addr);
-	//	}
-	//}
-	//ADDLOG_INFO(LOG_FEATURE_CMD, "Erased flash.");
+		for (int addr = startAddr; addr <= endAddr; addr += 4096) {
+			spi_flash_erase_sector(spi, addr);
+		}
+	}
+	ADDLOG_INFO(LOG_FEATURE_CMD, "Erased flash.");
 
-	//spi_flash_read(spi, baseAddr, readBuf, length);
-	//for (i = 0; i < length; i++) {
-	//	if (readBuf[i] != 0xFF)
-	//		err++;
-	//}
-	//ADDLOG_INFO(LOG_FEATURE_CMD, "Flash erased test: errors = %d/%d", err, length);
+	spi_flash_read(spi, baseAddr, readBuf, length);
+	for (i = 0; i < length; i++) {
+		if (readBuf[i] != 0xFF)
+			err++;
+	}
+	ADDLOG_INFO(LOG_FEATURE_CMD, "Flash erased test: errors = %d/%d", err, length);
 
-	//err = 0;
-	//spi_flash_write2(spi, baseAddr, writeBuf, length);
-	//ADDLOG_INFO(LOG_FEATURE_CMD, "Wrote pattern %02X to flash.", pattern);
+	err = 0;
+	spi_flash_write2(spi, baseAddr, writeBuf, length);
+	ADDLOG_INFO(LOG_FEATURE_CMD, "Wrote pattern %02X to flash.", pattern);
 
-	//spi_flash_read(spi, baseAddr, readBuf, length);
-	//for (i = 0; i < length; i++) {
-	//	if (readBuf[i] != pattern)
-	//		err++;
-	//}
+	spi_flash_read(spi, baseAddr, readBuf, length);
+	for (i = 0; i < length; i++) {
+		if (readBuf[i] != pattern)
+			err++;
+	}
 
-	//ADDLOG_INFO(LOG_FEATURE_CMD, "Flash write-read test: errors = %d/%d", err, length);
+	ADDLOG_INFO(LOG_FEATURE_CMD, "Flash write-read test: errors = %d/%d", err, length);
 
-	//free(writeBuf);
-	//free(readBuf);
+	free(writeBuf);
+	free(readBuf);
 }
 
 softSPI_t g_lfs_spi;
 int g_lfs_spi_ready = 0;
 void LFS_SPI_Init() {
-	/*g_lfs_spi.miso = MISO_PIN;
+	g_lfs_spi.miso = MISO_PIN;
 	g_lfs_spi.mosi = MOSI_PIN;
 	g_lfs_spi.ss = SS_PIN;
 	g_lfs_spi.sck = SCK_PIN;
 
 	SPI_Setup(&g_lfs_spi);
-	g_lfs_spi_ready = 1;*/
+	g_lfs_spi_ready = 1;
 }
 void LFS_SPI_Flash_EraseSector(int addr) {
-	//if (g_lfs_spi_ready == 0) {
-	//	LFS_SPI_Init();
-	//}
-	//spi_flash_erase_sector(&g_lfs_spi, addr);
+	if (g_lfs_spi_ready == 0) {
+		LFS_SPI_Init();
+	}
+	spi_flash_erase_sector(&g_lfs_spi, addr);
 }
 void spi_test() {
-	/*softSPI_t spi;
+	softSPI_t spi;
 
 	spi.miso = MISO_PIN;
 	spi.mosi = MOSI_PIN;
@@ -347,22 +348,22 @@ void spi_test() {
 
 	spi_flash_read_id(&spi, jedec_id);
 
-	ADDLOG_INFO(LOG_FEATURE_CMD, "ID %02X %02X %02X", jedec_id[0], jedec_id[1], jedec_id[2]);*/
+	ADDLOG_INFO(LOG_FEATURE_CMD, "ID %02X %02X %02X", jedec_id[0], jedec_id[1], jedec_id[2]);
 }
 void LFS_SPI_Flash_Read(int adr, int cnt, byte *data) {
-	//if (g_lfs_spi_ready == 0) {
-	//	LFS_SPI_Init();
-	//}
-	//spi_flash_read(&g_lfs_spi, adr, data, cnt);
+	if (g_lfs_spi_ready == 0) {
+		LFS_SPI_Init();
+	}
+	spi_flash_read(&g_lfs_spi, adr, data, cnt);
 }
 void LFS_SPI_Flash_Write(int adr, const byte *data, int cnt) {
-	//if (g_lfs_spi_ready == 0) {
-	//	LFS_SPI_Init();
-	//}
-	//spi_flash_write2(&g_lfs_spi, adr, data, cnt);
+	if (g_lfs_spi_ready == 0) {
+		LFS_SPI_Init();
+	}
+	spi_flash_write2(&g_lfs_spi, adr, data, cnt);
 }
 void spi_test_read_and_print(int adr, int cnt) {
-	/*byte *data;
+	byte *data;
 	int i;
 
 	data = malloc(cnt);
@@ -381,11 +382,11 @@ void spi_test_read_and_print(int adr, int cnt) {
 		}
 		ADDLOG_INFO(LOG_FEATURE_CMD, "Ofs %i - %s", i, tmp);
 	}
-	free(data);*/
+	free(data);
 }
 
 void spi_test_erase() {
-	/*softSPI_t spi;
+	softSPI_t spi;
 
 	spi.miso = MISO_PIN;
 	spi.mosi = MOSI_PIN;
@@ -394,7 +395,7 @@ void spi_test_erase() {
 
 	SPI_Setup(&spi);
 
-	spi_flash_erase(&spi);*/
+	spi_flash_erase(&spi);
 }
 
 
