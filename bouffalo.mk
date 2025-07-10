ad96552f9885e1c16ca8318c23f131c8d3fbdd1f
maxinemuster <max.muster@kaffeeschluerfer.com>
2025-07-10 20:11:03 +0200

    try fixing makefiles part 1

--- bouffalo.mk
+++ bouffalo.mk
@@ -1,11 +1,12 @@
 # Component Makefile
 #
 ## These include paths would be exported to project level
-COMPONENT_ADD_INCLUDEDIRS += src/ src/httpserver/ src/cmnds/ src/logging/ src/hal/bl602/ src/mqtt/ src/cJSON src/base64 src/driver src/devicegroups src/bitmessage src/littlefs libraries/berry/src src/libraries/obktime/ include/
+COMPONENT_ADD_INCLUDEDIRS += src/ src/httpserver/ src/httpclient/ src/cmnds/ src/logging/ src/hal/bl602/ src/mqtt/ src/cJSON src/base64 src/driver src/devicegroups src/bitmessage src/littlefs libraries/berry/src src/libraries/obktime/ include/
 
 ## not be exported to project level
 COMPONENT_PRIV_INCLUDEDIRS :=
 
+CFLAGS += -DOBK_VARIANT=${OBK_VARIANT}
 
 ## This component's src 
 COMPONENT_SRCS := 
@@ -13,4 +14,6 @@ COMPONENT_SRCS :=
 COMPONENT_OBJS := $(patsubst %.c,%.o, $(COMPONENT_SRCS))
 COMPONENT_OBJS := $(patsubst %.S,%.o, $(COMPONENT_OBJS))
 
-COMPONENT_SRCDIRS := src/ src/jsmn src/httpserver/ src/cmnds/ src/logging/ src/hal/bl602/ src/mqtt/ src/i2c/ src/cJSON src/base64 src/driver src/devicegroups src/bitmessage src/littlefs src/hal/generic src/libraries/obktime/
+COMPONENT_SRCDIRS := src/ src/jsmn src/httpserver/ src/httpclient/ src/cmnds/ src/logging/ src/hal/bl602/ src/mqtt/ src/i2c/ src/cJSON src/base64 src/driver src/devicegroups src/bitmessage src/littlefs src/hal/generic libraries/berry/src src/berry src/berry/modules src/libraries/obktime/
+
+COMPONENT_ADD_LDFLAGS = -Wl,--whole-archive -l$(COMPONENT_NAME) -Wl,--no-whole-archive
