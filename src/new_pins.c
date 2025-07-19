1c7cdd4881a95dc76ed70c0971b7a95ccdaf6d24
MaxineMuster <146550015+MaxineMuster@users.noreply.github.com>
2025-07-19 08:03:46 +0200

    Ds1820 simple+full as separate drivers (#1705)
    * Seperate simple and full driver, introduce "common" for shared code
    
    * Fix command prefixes
    
    * Generate command as multiline if requested
    
    * Fix JS linebreak
    
    * Only use simple dirver - check sizes of firmware images
    
    * Update obk_config.h - enable Full driver for all platforms to allow testing
    
    * Change parsing sttring to device address to allow compact form
    Change obk_config.h: remove extra entry for W800, add "FULL" driver to windows build
    
    * fix windows code for "fake" DS18B20
    
    * fix missing files in openBeken_win32_mvsc2017.vcxproj
    
    * fix handle missing task.h for windows
    
    * change Windows emulation to MAX/2 fake sensors
    
    * try fix BL602
    
    * change "fake temperature" of sensors in windows
    
    * change windows sensor temperature once more
    
    * shorten commands by alias
    
    * fix escaping linebreak
    
    * Fix broken JS code ...
    
    * Fix ESP32 is crashing if interrupts are disabled during OWReset for > 480us
    
    * Change output for unused channels
    Test if DS1820 channels are in use
    
    * fix warning/error for channel (<=64) printed into a char[5] value
    
    * removeunuses quotes for address
    
    * Fix language
    
    * Capial "S" in Menu entry "Sensors"
    
    * restore obk_config - #define ENABLE_DRIVER_DS1820_FULLonly for WINDOWS and SENSORS variant

--- src/new_pins.c
+++ src/new_pins.c
@@ -1735,6 +1735,10 @@ bool CHANNEL_IsInUse(int ch) {
 			}
 		}
 	}
+#if (ENABLE_DRIVER_DS1820_FULL)
+#include "driver/drv_ds1820_full.h"
+	return ds18b20_used_channel(ch);
+#endif
 	return false;
 }
 
