c7d48e427cd5de89701a7bfdd1675787785043aa
Steffen <14131163+slo617@users.noreply.github.com>
2025-07-28 13:29:58 +0200

    SM16703P: learn to handle RGBCW, e.g. more than 3 channels per pixel. (#1736)

--- src/driver/drv_main.c
+++ src/driver/drv_main.c
@@ -258,7 +258,7 @@ static driver_t g_drivers[] = {
 	//drvdetail:"title":"TODO",
 	//drvdetail:"descr":"SM16703P is an individually addressable LEDs controller like WS2812B. Currently SM16703P LEDs are supported through hardware SPI, LEDs data should be connected to P16 (MOSI), [here you can read](https://www.elektroda.com/rtvforum/topic4005865.html) how to break it out on CB2S.",
 	//drvdetail:"requires":""}
-	{ "SM16703P",	SM16703P_Init,		NULL,						NULL, NULL, NULL, NULL, NULL, false },
+	{ "SM16703P",	SM16703P_Init,		NULL,						NULL, NULL, SM16703P_Shutdown, NULL, NULL, false },
 #endif
 #if ENABLE_DRIVER_SM15155E
 	//drvdetail:{"name":"SM15155E",
