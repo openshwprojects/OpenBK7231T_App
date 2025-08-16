121455cab2c6648b664f1c1b99f48d8959ca91a5
zdenekkla-blip <zdenek.kla@gmail.com>
2025-08-16 17:44:16 +0200

    Update obk_config.h

--- src/obk_config.h
+++ src/obk_config.h
@@ -58,7 +58,7 @@
 #if PLATFORM_XR806
 #define ENABLE_DRIVER_TUYAMCU					1
 #define ENABLE_DRIVER_DS1820					1
-#define ENABLE_DRIVER_DHT						1
+#define ENABLE_DRIVER_DHT						0
 #define ENABLE_DRIVER_AHT2X						1
 #define ENABLE_DRIVER_SSDP						1
 #define ENABLE_OBK_BERRY						1
