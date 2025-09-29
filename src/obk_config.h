7ddf1c2b652550a2a6f9d81799c10d671a8ccb3a
maxinemuster <max.muster@kaffeeschluerfer.com>
2025-09-29 17:14:00 +0200

    fix define for Beken

--- src/obk_config.h
+++ src/obk_config.h
@@ -240,7 +240,7 @@
 #define ENABLE_LITTLEFS							1
 #define ENABLE_NTP								1
 // #define ENABLE_CLOCK_DST						1
-#define ENABLE_NTP_SUNRISE_SUNSET				1
+#define ENABLE_CLOCK_SUNRISE_SUNSET				1
 #define ENABLE_DRIVER_LED						1
 #define ENABLE_DRIVER_BL0937					1
 #define ENABLE_DRIVER_BL0942					1
