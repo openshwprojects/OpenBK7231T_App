f60d242fbb45694455bede4fc380f87428f039e6
divadiow <62958974+divadiow@users.noreply.github.com>
2025-10-03 13:50:09 +0100

    Flag13 - extend pin state display to PLATFORM_GPIO_MAX (#1823)
    * Update http_fns.c
    
    * Update http_fns.c
    
    * Update http_fns.c
    
    * Revert "Update http_fns.c"
    
    This reverts commit c899cb45b0f982cbe65febf64c6f0b14acbc4f4f.
    
    * Revert "Update http_fns.c"
    
    This reverts commit 012c49c32dd752e16ed0c69d3fe0e04c44db885e.
    
    * Update http_fns.c

--- src/httpserver/http_fns.c
+++ src/httpserver/http_fns.c
@@ -1000,7 +1000,7 @@ typedef enum {
 	/* Format current PINS input state for all unused pins */
 	if (CFG_HasFlag(OBK_FLAG_HTTP_PINMONITOR))
 	{
-		for (i = 0; i < 29; i++)
+		for (i = 0; i < PLATFORM_GPIO_MAX; i++)
 		{
 			if ((PIN_GetPinRoleForPinIndex(i) == IOR_None) && (i != 0) && (i != 1))
 			{
@@ -1009,7 +1009,7 @@ typedef enum {
 		}
 
 		hprintf255(request, "<h5> PIN States<br>");
-		for (i = 0; i < 29; i++)
+		for (i = 0; i < PLATFORM_GPIO_MAX; i++)
 		{
 			if ((PIN_GetPinRoleForPinIndex(i) != IOR_None) || (i == 0) || (i == 1))
 			{
