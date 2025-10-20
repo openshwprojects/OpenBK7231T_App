e8a5c6885f5b878a5e3bef56a4de6eb8f13b9bd7
NonPIayerCharacter <18557343+NonPIayerCharacter@users.noreply.github.com>
2025-10-20 18:14:35 +0300

    rda5981 ota

--- src/hal/rda5981/hal_main_rda5981.cpp
+++ src/hal/rda5981/hal_main_rda5981.cpp
@@ -9,13 +9,16 @@
 extern "C" {
 	extern void Main_Init();
 	extern void Main_OnEverySecond();
+	extern uint32_t OTA_Offset;
 }
 
 extern WiFiStackInterface wifi;
 
 __attribute__((used)) int main()
 {
-	//rda5981_set_user_data_addr(0x180FB000, 0x180FC000, 3584);
+	char* temp_buf = (char*)malloc(256);
+	rda5981_read_flash(OTA_Offset, temp_buf, 256);
+	if(temp_buf[0] == 0xAE && temp_buf[1] == 0xAE) rda5981_erase_flash(OTA_Offset, 1024);
 	wifi.init();
 	Main_Init();
 	while(true)
--- src/hal/rda5981/hal_ota_rda5981.c
+++ src/hal/rda5981/hal_ota_rda5981.c
@@ -9,6 +9,80 @@
 
 #include "rda5981_ota.h"
 
+uint32_t OTA_Offset = 0x18095000;
+
+static int flash_write(unsigned int addr, char* buf, unsigned int len)
+{
+	if(len == 0) return 0;
+	int ret;
+	int left;
+	unsigned int addr_t, len_t;
+	char* temp_buf = NULL;
+	addr_t = addr & 0xffffff00;
+	len_t = addr - addr_t + len;
+	if(len_t % 256)
+		len_t += 256 - len_t % 256;
+	//printf("addr %x addr_t %x\r\n", addr, addr_t);
+	//printf("len %d len_t %d\r\n", len, len_t);
+	temp_buf = (char*)malloc(256);
+	if(temp_buf == NULL)
+		return -1;
+
+	ret = rda5981_read_flash(addr_t, temp_buf, 256);
+	if(ret)
+	{
+		free(temp_buf);
+		return -1;
+	}
+	left = 256 - (addr - addr_t);
+	if(len < left)
+		memcpy(temp_buf + addr - addr_t, buf, len);//256-(addr-addr_t));
+	else
+		memcpy(temp_buf + addr - addr_t, buf, left);
+	ret = rda5981_write_flash(addr_t, temp_buf, 256);
+	if(ret)
+	{
+		free(temp_buf);
+		return -1;
+	}
+
+	len_t -= 256;
+	buf += 256 - (addr - addr_t);
+	len -= 256 - (addr - addr_t);
+	addr_t += 256;
+
+	while(len_t != 0)
+	{
+		//printf("len_t %d buf %x len %d addr_t %x\r\n", len_t, buf, len, addr_t);
+		if(len >= 256)
+		{
+			memcpy(temp_buf, buf, 256);
+		}
+		else
+		{
+			ret = rda5981_read_flash(addr_t, temp_buf, 256);
+			if(ret)
+			{
+				free(temp_buf);
+				return -1;
+			}
+			memcpy(temp_buf, buf, len);
+		}
+		ret = rda5981_write_flash(addr_t, temp_buf, 256);
+		if(ret)
+		{
+			free(temp_buf);
+			return -1;
+		}
+		len_t -= 256;
+		buf += 256;
+		len -= 256;
+		addr_t += 256;
+	}
+	free(temp_buf);
+	return 0;
+}
+
 int http_rest_post_flash(http_request_t* request, int startaddr, int maxaddr)
 {
 	int total = 0;
@@ -26,32 +100,34 @@ int http_rest_post_flash(http_request_t* request, int startaddr, int maxaddr)
 		ADDLOG_ERROR(LOG_FEATURE_OTA, "Content-length is 0");
 		goto update_ota_exit;
 	}
-	startaddr = 0x1807E000;
-	// if compressed ota
-	//startaddr = 0x1809C000;
-	int ret1 = rda5981_write_partition_start(startaddr, towrite + (towrite % 4096));
+	else
+	{
+		towrite = request->contentLength;
+	}
+	startaddr = OTA_Offset;
+	int ret1 = rda5981_erase_flash(startaddr, towrite);
 	if(ret1 != 0)
 	{
-		//ret = -1;
-		ADDLOG_ERROR(LOG_FEATURE_OTA, "rda5981_write_partition_start failed. %i", ret);
-		//goto update_ota_exit;
+		ret = -1;
+		ADDLOG_ERROR(LOG_FEATURE_OTA, "rda5981_erase_flash failed. %i", ret);
+		goto update_ota_exit;
 	}
 
 	do
 	{
 		ADDLOG_DEBUG(LOG_FEATURE_OTA, "Writelen %i at %i", writelen, total);
-		ret1 = rda5981_write_partition(startaddr, (unsigned char*)writebuf, writelen);
+		ret1 = flash_write(startaddr, writebuf, writelen);
 		if(ret1 != 0)
 		{
 			ret = -1;
-			ADDLOG_ERROR(LOG_FEATURE_OTA, "rda5981_write_partition failed. %i", ret1);
+			ADDLOG_ERROR(LOG_FEATURE_OTA, "flash_write failed. %i", ret1);
 			goto update_ota_exit;
 		}
 		delay_ms(5);
 		total += writelen;
 		startaddr += writelen;
 		towrite -= writelen;
-		if (towrite > 0)
+		if(towrite > 0)
 		{
 			writebuf = request->received;
 			writelen = recv(request->fd, writebuf, 1024, 0);
@@ -59,16 +135,11 @@ int http_rest_post_flash(http_request_t* request, int startaddr, int maxaddr)
 			{
 				ADDLOG_DEBUG(LOG_FEATURE_OTA, "recv returned %d - end of data - remaining %d", writelen, towrite);
 				ret = -1;
+				goto update_ota_exit;
 			}
 		}
 	} while ((towrite > 0) && (writelen >= 0));
 
-	int check = rda5981_write_partition_end();
-	if(check != 0)
-	{
-		ADDLOG_ERROR(LOG_FEATURE_OTA, "rda5981_write_partition_end failed, %i", check);
-		ret = -1;
-	}
 update_ota_exit:
 	if (ret != -1)
 	{
