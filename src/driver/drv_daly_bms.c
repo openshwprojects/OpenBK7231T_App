7161fa719b4da1bb8dd37ac7354e5b9c96f4e008
Lukas Meisegeier <MeisegeierLukas@gmx.de>
2025-12-24 12:30:40 +0100

    added commands

--- src/driver/drv_daly_bms.c
+++ src/driver/drv_daly_bms.c
@@ -80,6 +80,12 @@ int getUartDataSize(unsigned char* receive_buffer,int expected_length){
 	UART_ConsumeBytes(len);
 	return len;
 }
+int emptyUart(){
+    rtos_delay_milliseconds(30);
+	int len = UART_GetDataSize();
+	UART_ConsumeBytes(len);
+	return len;
+}
 
 void readCellBalanceState(){
 	if(!DALY_BMS_Mutex_Take(10)){
@@ -554,75 +560,255 @@ void DALY_BMS_Deinit(){
 }
 
 
-void sendSetRequest(byte address,const byte* setBuffer){
-	unsigned char buffer[13];
-	buffer[0] = 0xA5;
-	buffer[1] = 0x40;
-	buffer[2] = address;
-	buffer[3] = 0x08;
-    MQTT_PublishMain_StringInt("daly_bms_debug_set", 3, 0);
-	for(int i=4;i<12;i++){
-		buffer[i]=setBuffer[i-4];
-	}
-	int checksum=0;
-	for(int i=0;i<11;i++){
-		checksum+=buffer[i];
-	}
-	buffer[12]=checksum;
+static uint16_t MODBUS_CRC16( const unsigned char *buf, unsigned int len )
+{
+	static const uint16_t table[256] = {
+	0x0000, 0xC0C1, 0xC181, 0x0140, 0xC301, 0x03C0, 0x0280, 0xC241,
+	0xC601, 0x06C0, 0x0780, 0xC741, 0x0500, 0xC5C1, 0xC481, 0x0440,
+	0xCC01, 0x0CC0, 0x0D80, 0xCD41, 0x0F00, 0xCFC1, 0xCE81, 0x0E40,
+	0x0A00, 0xCAC1, 0xCB81, 0x0B40, 0xC901, 0x09C0, 0x0880, 0xC841,
+	0xD801, 0x18C0, 0x1980, 0xD941, 0x1B00, 0xDBC1, 0xDA81, 0x1A40,
+	0x1E00, 0xDEC1, 0xDF81, 0x1F40, 0xDD01, 0x1DC0, 0x1C80, 0xDC41,
+	0x1400, 0xD4C1, 0xD581, 0x1540, 0xD701, 0x17C0, 0x1680, 0xD641,
+	0xD201, 0x12C0, 0x1380, 0xD341, 0x1100, 0xD1C1, 0xD081, 0x1040,
+	0xF001, 0x30C0, 0x3180, 0xF141, 0x3300, 0xF3C1, 0xF281, 0x3240,
+	0x3600, 0xF6C1, 0xF781, 0x3740, 0xF501, 0x35C0, 0x3480, 0xF441,
+	0x3C00, 0xFCC1, 0xFD81, 0x3D40, 0xFF01, 0x3FC0, 0x3E80, 0xFE41,
+	0xFA01, 0x3AC0, 0x3B80, 0xFB41, 0x3900, 0xF9C1, 0xF881, 0x3840,
+	0x2800, 0xE8C1, 0xE981, 0x2940, 0xEB01, 0x2BC0, 0x2A80, 0xEA41,
+	0xEE01, 0x2EC0, 0x2F80, 0xEF41, 0x2D00, 0xEDC1, 0xEC81, 0x2C40,
+	0xE401, 0x24C0, 0x2580, 0xE541, 0x2700, 0xE7C1, 0xE681, 0x2640,
+	0x2200, 0xE2C1, 0xE381, 0x2340, 0xE101, 0x21C0, 0x2080, 0xE041,
+	0xA001, 0x60C0, 0x6180, 0xA141, 0x6300, 0xA3C1, 0xA281, 0x6240,
+	0x6600, 0xA6C1, 0xA781, 0x6740, 0xA501, 0x65C0, 0x6480, 0xA441,
+	0x6C00, 0xACC1, 0xAD81, 0x6D40, 0xAF01, 0x6FC0, 0x6E80, 0xAE41,
+	0xAA01, 0x6AC0, 0x6B80, 0xAB41, 0x6900, 0xA9C1, 0xA881, 0x6840,
+	0x7800, 0xB8C1, 0xB981, 0x7940, 0xBB01, 0x7BC0, 0x7A80, 0xBA41,
+	0xBE01, 0x7EC0, 0x7F80, 0xBF41, 0x7D00, 0xBDC1, 0xBC81, 0x7C40,
+	0xB401, 0x74C0, 0x7580, 0xB541, 0x7700, 0xB7C1, 0xB681, 0x7640,
+	0x7200, 0xB2C1, 0xB381, 0x7340, 0xB101, 0x71C0, 0x7080, 0xB041,
+	0x5000, 0x90C1, 0x9181, 0x5140, 0x9301, 0x53C0, 0x5280, 0x9241,
+	0x9601, 0x56C0, 0x5780, 0x9741, 0x5500, 0x95C1, 0x9481, 0x5440,
+	0x9C01, 0x5CC0, 0x5D80, 0x9D41, 0x5F00, 0x9FC1, 0x9E81, 0x5E40,
+	0x5A00, 0x9AC1, 0x9B81, 0x5B40, 0x9901, 0x59C0, 0x5880, 0x9841,
+	0x8801, 0x48C0, 0x4980, 0x8941, 0x4B00, 0x8BC1, 0x8A81, 0x4A40,
+	0x4E00, 0x8EC1, 0x8F81, 0x4F40, 0x8D01, 0x4DC0, 0x4C80, 0x8C41,
+	0x4400, 0x84C1, 0x8581, 0x4540, 0x8701, 0x47C0, 0x4680, 0x8641,
+	0x8201, 0x42C0, 0x4380, 0x8341, 0x4100, 0x81C1, 0x8081, 0x4040 };
 
-    MQTT_PublishMain_StringInt("daly_bms_debug_set", 4, 0);
-	for(int i = 0; i < 13; i++)
+	uint8_t xor = 0;
+	uint16_t crc = 0xFFFF;
+
+	while(len--)
+	{
+		xor = (*buf++) ^ crc;
+		crc >>= 8;
+		crc ^= table[xor];
+	}
+
+	return crc;
+}
+
+void setMaxCellVoltage(float voltage1, float voltage2){
+	unsigned char buffer[14];
+	buffer[0] = 0x81;
+	buffer[1] = 0x10;
+	buffer[2] = 0x01;
+	buffer[3] = 0x30;
+	buffer[4] = 0x00;
+	buffer[5] = 0x03;
+
+    buffer[6] = ((int)(voltage1*1000))>>8;
+    buffer[7] = ((int)(voltage1*1000));
+
+    buffer[8] = ((int)(voltage2*1000))>>8;
+    buffer[9] = ((int)(voltage2*1000));
+
+    buffer[10] = ((int)(voltage1*1000))>>8;
+    buffer[11] = ((int)(voltage1*1000));
+
+
+	uint16_t crc = MODBUS_CRC16(buffer, 11);
+
+	buffer[12] = crc & 0xFF;
+	buffer[13] = (crc >> 8) & 0xFF;
+
+	for(int i = 0; i < 14; i++)
 	{
 		UART_SendByte(buffer[i]);
 	}
+    emptyUart();
 }
 
+void setMinCellVoltage(float voltage1, float voltage2){
+	unsigned char buffer[12];
+	buffer[0] = 0x81;
+	buffer[1] = 0x10;
+	buffer[2] = 0x01;
+	buffer[3] = 0xC6;
+	buffer[4] = 0x00;
+	buffer[5] = 0x02;
+
+    buffer[6] = ((int)(voltage1*1000))>>8;
+    buffer[7] = ((int)(voltage1*1000));
+
+    buffer[8] = ((int)(voltage2*1000))>>8;
+    buffer[9] = ((int)(voltage2*1000));
 
 
-void setCellVoltageThreshold(float voltageMinWarn,float voltageMinOff,float voltageMaxWarn,float voltageMaxOff)
-{
-    MQTT_PublishMain_StringInt("daly_bms_debug_set", 1, 0);
-	if(!DALY_BMS_Mutex_Take(10)){
-		return;
+	uint16_t crc = MODBUS_CRC16(buffer, 11);
+
+	buffer[10] = crc & 0xFF;
+	buffer[11] = (crc >> 8) & 0xFF;
+
+	for(int i = 0; i < 12; i++)
+	{
+		UART_SendByte(buffer[i]);
 	}
+    emptyUart();
+}
+void setMaxPackVoltage(float voltage1, float voltage2){
+	unsigned char buffer[14];
+	buffer[0] = 0x81;
+	buffer[1] = 0x10;
+	buffer[2] = 0x01;
+	buffer[3] = 0x38;
+	buffer[4] = 0x00;
+	buffer[5] = 0x03;
 
-    MQTT_PublishMain_StringInt("daly_bms_debug_set", 2, 0);
-	byte set_buffer[8];
-	set_buffer[0]= ((int) (voltageMaxWarn*1000))>>8;
-	set_buffer[1]= ((int) (voltageMaxWarn*1000));
+    buffer[6] = ((int)(voltage1*10))>>8;
+    buffer[7] = ((int)(voltage1*10));
 
-	set_buffer[2]= ((int) (voltageMaxOff*1000))>>8;
-	set_buffer[3]= ((int) (voltageMaxOff*1000));
+    buffer[8] = ((int)(voltage2*10))>>8;
+    buffer[9] = ((int)(voltage2*10));
 
-	set_buffer[4]= ((int) (voltageMinWarn*1000))>>8;
-	set_buffer[5]= ((int) (voltageMinWarn*1000));
+    buffer[10] = ((int)(voltage1*10))>>8;
+    buffer[11] = ((int)(voltage1*10));
 
-	set_buffer[6]= ((int) (voltageMinOff*1000))>>8;
-	set_buffer[7]= ((int) (voltageMinOff*1000));
 
-	sendSetRequest(0x59,set_buffer);
-    MQTT_PublishMain_StringInt("daly_bms_debug_set", 5, 0);
+	uint16_t crc = MODBUS_CRC16(buffer, 11);
 
-	unsigned char receive_buffer[256];
+	buffer[12] = crc & 0xFF;
+	buffer[13] = (crc >> 8) & 0xFF;
 
-	int len= getUartDataSize(receive_buffer,13);
-	DALY_BMS_Mutex_Free();
-	if(len==0){
-        return;
-    }
-    MQTT_PublishMain_StringInt("daly_bms_debug_set", 6, 0);
-
-    float maxCellThreshold1 = (float)((receive_buffer[4] << 8) | receive_buffer[5])*0.001;
-    MQTT_PublishMain_StringFloat("daly_bms_max_cell_threshold_1", maxCellThreshold1,3, 0);
-    float maxCellThreshold2 = (float)((receive_buffer[6] << 8) | receive_buffer[7])*0.001;
-    MQTT_PublishMain_StringFloat("daly_bms_max_cell_threshold_2", maxCellThreshold2,3, 0);
-
-    float minCellThreshold1 = (float)((receive_buffer[8] << 8) | receive_buffer[9])*0.001;
-    MQTT_PublishMain_StringFloat("daly_bms_min_cell_threshold_1", minCellThreshold1,3, 0);
-    float minCellThreshold2 = (float)((receive_buffer[10] << 8) | receive_buffer[11])*0.001;
-    MQTT_PublishMain_StringFloat("daly_bms_min_cell_threshold_2", minCellThreshold2,3, 0);
+	for(int i = 0; i < 14; i++)
+	{
+		UART_SendByte(buffer[i]);
+	}
+    emptyUart();
 }
 
+void setMinPackVoltage(float voltage1, float voltage2){
+	unsigned char buffer[14];
+	buffer[0] = 0x81;
+	buffer[1] = 0x10;
+	buffer[2] = 0x01;
+	buffer[3] = 0x3c;
+	buffer[4] = 0x00;
+	buffer[5] = 0x03;
+
+    buffer[6] = ((int)(voltage1*10))>>8;
+    buffer[7] = ((int)(voltage1*10));
+
+    buffer[8] = ((int)(voltage2*10))>>8;
+    buffer[9] = ((int)(voltage2*10));
+
+    buffer[10] = ((int)(voltage1*10))>>8;
+    buffer[11] = ((int)(voltage1*10));
+
+
+	uint16_t crc = MODBUS_CRC16(buffer, 11);
+
+	buffer[12] = crc & 0xFF;
+	buffer[13] = (crc >> 8) & 0xFF;
+
+	for(int i = 0; i < 14; i++)
+	{
+		UART_SendByte(buffer[i]);
+	}
+    emptyUart();
+}
+void setMaxChargeCurrent(float current1, float current2){
+	unsigned char buffer[12];
+	buffer[0] = 0x81;
+	buffer[1] = 0x10;
+	buffer[2] = 0x01;
+	buffer[3] = 0x40;
+	buffer[4] = 0x00;
+	buffer[5] = 0x02;
+
+    buffer[6] = ((int)(30000-current1*10))>>8;
+    buffer[7] = ((int)(30000-current1*10));
+
+    buffer[8] = ((int)(30000-current2*10))>>8;
+    buffer[9] = ((int)(30000-current2*10));
+
+	uint16_t crc = MODBUS_CRC16(buffer, 11);
+
+	buffer[10] = crc & 0xFF;
+	buffer[11] = (crc >> 8) & 0xFF;
+
+	for(int i = 0; i < 12; i++)
+	{
+		UART_SendByte(buffer[i]);
+	}
+    emptyUart();
+}
+
+void setMaxDischargeCurrent(float current1, float current2){
+	unsigned char buffer[12];
+	buffer[0] = 0x81;
+	buffer[1] = 0x10;
+	buffer[2] = 0x01;
+	buffer[3] = 0x40;
+	buffer[4] = 0x00;
+	buffer[5] = 0x02;
+
+    buffer[6] = ((int)(30000+current1*10))>>8;
+    buffer[7] = ((int)(30000+current1*10));
+
+    buffer[8] = ((int)(30000+current2*10))>>8;
+    buffer[9] = ((int)(30000+current2*10));
+
+	uint16_t crc = MODBUS_CRC16(buffer, 11);
+
+	buffer[10] = crc & 0xFF;
+	buffer[11] = (crc >> 8) & 0xFF;
+
+	for(int i = 0; i < 12; i++)
+	{
+		UART_SendByte(buffer[i]);
+	}
+    emptyUart();
+}
+void setMaxChargeTemp(int temp){
+	unsigned char buffer[12];
+	buffer[0] = 0x81;
+	buffer[1] = 0x10;
+	buffer[2] = 0x01;
+	buffer[3] = 0x4a;
+	buffer[4] = 0x00;
+	buffer[5] = 0x03;
+
+    buffer[6] = ((int)(30+temp))>>8;
+    buffer[7] = ((int)(30+temp));
+    buffer[8] = ((int)(40+temp))>>8;
+    buffer[9] = ((int)(40+temp));
+    buffer[10] = ((int)(35+temp))>>8;
+    buffer[11] = ((int)(35+temp));
+
+
+	uint16_t crc = MODBUS_CRC16(buffer, 11);
+
+	buffer[11] = crc & 0xFF;
+	buffer[12] = (crc >> 8) & 0xFF;
+
+	for(int i = 0; i < 12; i++)
+	{
+		UART_SendByte(buffer[i]);
+	}
+    emptyUart();
+}
 
 commandResult_t CMD_DALY_BMS_Set_Voltage_Thresholds(const void* context, const char* cmd, const char* args, int cmdFlags) {
 
@@ -632,7 +818,8 @@ commandResult_t CMD_DALY_BMS_Set_Voltage_Thresholds(const void* context, const c
 		float vMin2 = Tokenizer_GetArgFloat(1);
 		float vMax1 = Tokenizer_GetArgFloat(2);
 		float vMax2 = Tokenizer_GetArgFloat(3);
-        setCellVoltageThreshold(vMin1,vMin2,vMax1,vMax2);
+        setMaxCellVoltage(vMax1,vMax2);
+        setMinCellVoltage(vMin1,vMin2);
         return CMD_RES_OK;
 
 	}
