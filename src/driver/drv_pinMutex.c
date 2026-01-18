30834eb8f5d0e8709e86975eba27aef3346e5388
MaxineMuster <Max.Muster@kaffeeschluerfer.com>
2026-01-18 16:40:18 +0100

    Fix typo in drv_pinMutex.c

--- src/driver/drv_pinMutex.c
+++ src/driver/drv_pinMutex.c
@@ -105,8 +105,9 @@ static commandResult_t CMD_setMutex(const void *context, const char *cmd, const 
 	int delayMs = Tokenizer_GetArgInteger(2);
 //	int pinDown = Tokenizer_GetArgInteger(3);
 //	int pinUp = Tokenizer_GetArgInteger(4);
-	int pinDown = Tokenizer_GetPin(3);
-	int pinUp = Tokenizer_GetArgPin(4);
+	int pinDown = Tokenizer_GetPin(3,-1);
+	int pinUp = Tokenizer_GetPin(4,-1);
+	if (pinDown == -1 || pinUp == -1) return CMD_RES_BAD_ARGUMENT;
 
 	if (idx < 0 || idx >= MAX_PINMUTEX) {
 		addLogAdv(LOG_ERROR, LOG_FEATURE_GENERAL, "setMutex: index %d out of range (0..%d)", idx, MAX_PINMUTEX - 1);
