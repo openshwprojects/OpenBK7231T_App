openshwprojects/OpenBK7231T_App

commit d491a71a938135f92a7dc3f838e8f983e7101de7

made avty disable global

src/httpserver/hass.c
@@ -324,13 +324,13 @@ HassDeviceInfo* hass_init_device_info(ENTITY_TYPE type, int index, const char* p
 	cJSON_AddStringToObject(info->root, "~", CFG_GetMQTTClientId());      //base topic
 	// remove availability information for sensor to keep last value visible on Home Assistant
 	bool flagavty = false;
-	flagavty = CFG_HasFlag(OBK_FLAG_NOT_PUBLISH_AVAILABILITY_SENSOR);
+	flagavty = CFG_HasFlag(OBK_FLAG_NOT_PUBLISH_AVAILABILITY);
 	// if door sensor is running, then deep sleep will be invoked mostly, then we dont want availability
 #ifndef OBK_DISABLE_ALL_DRIVERS
 	if (DRV_IsRunning("DoorSensor") == false && DRV_IsRunning("tmSensor") == false)
 #endif
 	{
-		if (!isSensor || !flagavty) {
+		if (!isSensor && !flagavty) {
 			cJSON_AddStringToObject(info->root, "avty_t", "~/connected");   //availability_topic, `online` value is broadcasted
 		}
 	}

src/httpserver/http_fns.c
@@ -2641,7 +2641,7 @@ const char* g_obk_flagNames[] = {
 	"[LED] Use old linear brightness mode, ignore gamma ramp",
 	"[MQTT] Apply channel type multiplier on (if any) on channel value before publishing it",
 	"[MQTT] In HA discovery, add relays as lights",
-	"[HASS] Deactivate avty_t flag for sensor when publishing to HASS (permit to keep value). You must restart HASS discovery for change to take effect.",
+	"[HASS] Deactivate avty_t flag when publishing to HASS (permit to keep value). You must restart HASS discovery for change to take effect.",
 	"[DRV] Deactivate Autostart of all drivers",
 	"[WiFi] Quick connect to WiFi on reboot (TODO: check if it works for you and report on github)",
 	"[Power] Set power and current to zero if all relays are open",

src/new_pins.h
@@ -1097,7 +1097,7 @@ typedef struct pinsState_s {
 #define OBK_FLAG_LED_USE_OLD_LINEAR_MODE			32
 #define OBK_FLAG_PUBLISH_MULTIPLIED_VALUES			33
 #define OBK_FLAG_MQTT_HASS_ADD_RELAYS_AS_LIGHTS		34
-#define OBK_FLAG_NOT_PUBLISH_AVAILABILITY_SENSOR    35
+#define OBK_FLAG_NOT_PUBLISH_AVAILABILITY			 35
 #define OBK_FLAG_DRV_DISABLE_AUTOSTART              36
 #define OBK_FLAG_WIFI_FAST_CONNECT		            37
 #define OBK_FLAG_POWER_FORCE_ZERO_IF_RELAYS_OPEN    38

src/selftest/selftest_hass_discovery.c
@@ -413,6 +413,45 @@ void Test_HassDiscovery_digitalInput() {
 	SELFTEST_ASSERT_HAS_MQTT_JSON_SENT_ANY("homeassistant", true, 0, 0, "name", "myDigitalValue");
 
 }
+
+void Test_HassDiscovery_digitalInputNoAVTY() {
+	const char *shortName = "DigitalInputTest";
+	const char *fullName = "Windows Fake DINput";
+	const char *mqttName = "fakeDin";
+
+	SIM_ClearOBK(shortName);
+	SIM_ClearAndPrepareForMQTTTesting(mqttName, "bekens");
+
+	CFG_SetShortDeviceName(shortName);
+	CFG_SetDeviceName(fullName);
+
+	CFG_SetFlag(OBK_FLAG_NOT_PUBLISH_AVAILABILITY, true); // disabled avty
+
+	PIN_SetPinRoleForPinIndex(24, IOR_DigitalInput);
+	PIN_SetPinChannelForPinIndex(24, 5);
+
+	// this label will be sent via HASS Discovery to Home Assistant
+	CMD_ExecuteCommand("setChannelLabel 5 myDigitalValue", 0);
+	CMD_ExecuteCommand("scheduleHADiscovery 1", 0);
+	Sim_RunSeconds(10, false);
+
+	// generic tests to see if something power-related was published
+	// OBK device should publish JSON on MQTT topic "homeassistant"
+	SELFTEST_ASSERT_HAS_MQTT_JSON_SENT("homeassistant", true);
+	SELFTEST_ASSERT_JSON_VALUE_STRING("dev", "name", shortName);
+	SELFTEST_ASSERT_JSON_VALUE_STRING("dev", "sw", USER_SW_VER);
+	SELFTEST_ASSERT_JSON_VALUE_STRING("dev", "mf", MANUFACTURER);
+	SELFTEST_ASSERT_JSON_VALUE_STRING("dev", "mdl", PLATFORM_MCU_NAME);
+	SELFTEST_ASSERT_JSON_VALUE_STRING_NOT_PRESENT(NULL, "avty_t"); // disabled avty
+	SELFTEST_ASSERT_JSON_VALUE_STRING(NULL, "pl_on", "0");
+	SELFTEST_ASSERT_JSON_VALUE_STRING(NULL, "pl_off", "1");
+	// we have used channel index 1
+	SELFTEST_ASSERT_HAS_MQTT_JSON_SENT_ANY("homeassistant", true, 0, 0, "stat_t", "~/5/get");
+	// this label will be sent via HASS Discovery to Home Assistant
+	SELFTEST_ASSERT_HAS_MQTT_JSON_SENT_ANY("homeassistant", true, 0, 0, "name", "myDigitalValue");
+
+}
+
 void Test_HassDiscovery() {
 	Test_HassDiscovery_SHTSensor();
 	Test_HassDiscovery_BL0942();
@@ -427,6 +466,7 @@ void Test_HassDiscovery() {
 	Test_HassDiscovery_LED_RGBW();
 	Test_HassDiscovery_DHT11();
 	Test_HassDiscovery_digitalInput();
+	Test_HassDiscovery_digitalInputNoAVTY();
 }
 
 

src/selftest/selftest_local.h
@@ -24,6 +24,7 @@ void SelfTest_Failed(const char *file, const char *function, int line, const cha
 #define SELFTEST_ASSERT_ARGUMENT_INTEGER(argumentIndex, res) SELFTEST_ASSERT((Tokenizer_GetArgInteger(argumentIndex)== res));
 #define SELFTEST_ASSERT_ARGUMENTS_COUNT(wantedCount) SELFTEST_ASSERT((Tokenizer_GetArgsCount()==wantedCount));
 #define SELFTEST_ASSERT_JSON_VALUE_STRING(obj, varName, res) SELFTEST_ASSERT(!strcmp(Test_GetJSONValue_String(varName,obj), res));
+#define SELFTEST_ASSERT_JSON_VALUE_STRING_NOT_PRESENT(obj, varName) SELFTEST_ASSERT((*Test_GetJSONValue_String(varName,obj))==0);
 #define SELFTEST_ASSERT_JSON_VALUE_EXISTS(obj, varName) SELFTEST_ASSERT(Test_GetJSONValue_Generic(varName,obj));
 #define SELFTEST_ASSERT_JSON_VALUE_INTEGER(obj, varName, res) SELFTEST_ASSERT((Test_GetJSONValue_Integer(varName,obj) == res));
 #define SELFTEST_ASSERT_JSON_VALUE_INTEGER_NESTED2(par1, par2, varName, res) SELFTEST_ASSERT((Test_GetJSONValue_Integer_Nested2(par1, par2,varName) == res));

commit cff75834d67f46c69b45f7b30c8a786f46490d74

fix plausible regression from d491a71a938135f9, made avty disable global
It's easier to reason about the change if we consider when the
availability topic is not added.
Originally that was when isSensor && flagavty, that is, when an entity
was considered to be a sensor and flag 35 was set.
After commit d491a71a938135f9, it became isSensor || flagavty.
That is, (A) if flag 35 is set then all entities are stripped of the
availability topic and (B) even when the flag is not set, then any
entity considered to be a sensor would also be stripped of the topic.
I am confident that (A) was the intent and (B) is a bug.
So, we should simply check only flagavty.

src/httpserver/hass.c
@@ -635,7 +635,7 @@ HassDeviceInfo* hass_init_device_info(ENTITY_TYPE type, int index, const char* p
 	if (DRV_IsRunning("DoorSensor") == false && DRV_IsRunning("tmSensor") == false)
 #endif
 	{
-		if (!isSensor && !flagavty) {
+		if (!flagavty) {
 			cJSON_AddStringToObject(info->root, "avty_t", "~/connected");   //availability_topic, `online` value is broadcasted
 		}
 	}