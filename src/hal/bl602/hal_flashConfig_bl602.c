68e3270d93942973005d988c3a8b132248b72883
NonPIayerCharacter <18557343+NonPIayerCharacter@users.noreply.github.com>
2026-03-29 00:01:13 +0300

    bl602 fv (#2053)

--- src/hal/bl602/hal_flashConfig_bl602.c
+++ src/hal/bl602/hal_flashConfig_bl602.c
@@ -2,56 +2,23 @@
 
 #include "../hal_flashConfig.h"
 #include "../../logging/logging.h"
-
 #include <easyflash.h>
 
-static int g_easyFlash_Ready = 0;
+#define EASYFLASH_MY_OBK_CONF "mY0bcFg"
 
-void BL602_InitEasyFlashIfNeeded(){
-	if(g_easyFlash_Ready==0){
-		easyflash_init();
-		g_easyFlash_Ready = 1;
-	}
+extern void InitEasyFlashIfNeeded();
+
+int HAL_Configuration_ReadConfigMemory(void *target, int dataLen)
+{
+	InitEasyFlashIfNeeded();
+	return ef_get_env_blob(EASYFLASH_MY_OBK_CONF, target, dataLen, NULL);
 }
 
-#define EASYFLASH_MY_OBK_CONF     "mY0bcFg"
-
-int HAL_Configuration_ReadConfigMemory(void *target, int dataLen){
-	int readLen;
-
-	ADDLOG_DEBUG(LOG_FEATURE_CFG, "HAL_Configuration_ReadConfigMemory: will read %d bytes", dataLen);
-	readLen = ef_get_env_blob(EASYFLASH_MY_OBK_CONF, target, dataLen , NULL);
-	ADDLOG_DEBUG(LOG_FEATURE_CFG, "HAL_Configuration_ReadConfigMemory: really loaded %d bytes", readLen);
-
-    return dataLen;
+int HAL_Configuration_SaveConfigMemory(void *src, int dataLen)
+{
+	InitEasyFlashIfNeeded();
+	ef_set_env_blob(EASYFLASH_MY_OBK_CONF, src, dataLen);
+	return dataLen;
 }
 
-
-void HAL_Configuration_GenerateMACForThisModule(unsigned char *out) {
-}
-
-
-
-int HAL_Configuration_SaveConfigMemory(void *src, int dataLen){
-
-	EfErrCode  res;
-
-	res = ef_set_env_blob(EASYFLASH_MY_OBK_CONF, src, dataLen);
-	if(res == EF_ENV_INIT_FAILED) {
-		ADDLOG_DEBUG(LOG_FEATURE_CFG, "HAL_Configuration_SaveConfigMemory: EF_ENV_INIT_FAILED for %d bytes", dataLen);
-		return 0;
-	}
-	if(res == EF_ENV_ARG_ERR) {
-		ADDLOG_DEBUG(LOG_FEATURE_CFG, "HAL_Configuration_SaveConfigMemory: EF_ENV_ARG_ERR for %d bytes", dataLen);
-		return 0;
-	}
-	ADDLOG_DEBUG(LOG_FEATURE_CFG, "HAL_Configuration_SaveConfigMemory: saved %d bytes", dataLen);
-    return dataLen;
-}
-
-
-
-
-#endif // PLATFORM_XR809
-
-
+#endif // PLATFORM_BL602
--- src/hal/bl602/hal_flashVars_bl602.c
+++ src/hal/bl602/hal_flashVars_bl602.c
@@ -1,140 +1,217 @@
-#ifdef PLATFORM_BL602
+#if PLATFORM_BL602
 
-#include "../hal_flashConfig.h"
+#include "../../new_common.h"
 #include "../hal_flashVars.h"
 #include "../../logging/logging.h"
-
 #include <easyflash.h>
 
+static int g_easyFlash_Ready = 0;
+void InitEasyFlashIfNeeded()
+{
+	if(g_easyFlash_Ready == 0)
+	{
+		easyflash_init();
+		g_easyFlash_Ready = 1;
+	}
+}
 
-void BL602_InitEasyFlashIfNeeded();
-
-#define EASYFLASH_MY_BOOTCOUNTS     "myBtCnts"
-#define BL602_SAVED_CHANNELS_MAX 8
-
-typedef struct bl602_bootCounts_s {
-    unsigned short boot_count; // number of times the device has booted
-    unsigned short boot_success_count; // if a device boots completely (>30s), will equal boot_success_count
-    short channelStates[BL602_SAVED_CHANNELS_MAX];
-    ENERGY_METERING_DATA emetering;
-} bl602_bootCounts_t;
-
-static bl602_bootCounts_t g_bootCounts;
+FLASH_VARS_STRUCTURE flash_vars;
 static int g_loaded = 0;
 
-static int BL602_ReadFlashVars(void *target, int dataLen){
+#define KV_KEY_FLASH_VARS "OBK_FV"
+#define SAVE_CHANGE_IF_REQUIRED_AND_COUNT(target, source, counter) \
+	if((target) != (source)) { \
+		(target) = (source); \
+		counter++; \
+	}
+
+extern void InitEasyFlashIfNeeded();
+
+static int ReadFlashVars(void* target, int dataLen)
+{
+	InitEasyFlashIfNeeded();
 	int readLen;
-
-	BL602_InitEasyFlashIfNeeded();
-
+	ADDLOG_DEBUG(LOG_FEATURE_CFG, "%s: to read %d b", __func__, dataLen);
+	readLen = ef_get_env_blob(KV_KEY_FLASH_VARS, target, dataLen, NULL);
+	ADDLOG_DEBUG(LOG_FEATURE_CFG, "%s: read %d b", __func__, readLen);
 	g_loaded = 1;
-	ADDLOG_DEBUG(LOG_FEATURE_CFG, "BL602_ReadFlashVars: will read %d bytes", dataLen);
-	readLen = ef_get_env_blob(EASYFLASH_MY_BOOTCOUNTS, target, dataLen , NULL);
-	ADDLOG_DEBUG(LOG_FEATURE_CFG, "BL602_ReadFlashVars: really loaded %d bytes", readLen);
-
-    return dataLen;
+	return dataLen;
 }
 
+static int SaveFlashVars(void* src, int dataLen)
+{
+	InitEasyFlashIfNeeded();
+	EfErrCode res;
 
-
-static int BL602_SaveFlashVars(void *src, int dataLen){
-
-	EfErrCode  res;
-
-	BL602_InitEasyFlashIfNeeded();
-
-	res = ef_set_env_blob(EASYFLASH_MY_BOOTCOUNTS, src, dataLen);
-	if(res == EF_ENV_INIT_FAILED) {
-		ADDLOG_DEBUG(LOG_FEATURE_CFG, "BL602_SaveFlashVars: EF_ENV_INIT_FAILED for %d bytes", dataLen);
+	res = ef_set_env_blob(KV_KEY_FLASH_VARS, src, dataLen);
+	if(res == EF_ENV_INIT_FAILED)
+	{
+		ADDLOG_DEBUG(LOG_FEATURE_CFG, "%s: init failed for %d b", __func__, dataLen);
 		return 0;
 	}
-	if(res == EF_ENV_ARG_ERR) {
-		ADDLOG_DEBUG(LOG_FEATURE_CFG, "BL602_SaveFlashVars: EF_ENV_ARG_ERR for %d bytes", dataLen);
+	if(res == EF_ENV_NAME_ERR)
+	{
+		ADDLOG_DEBUG(LOG_FEATURE_CFG, "%s: name err for %d b", __func__, dataLen);
 		return 0;
 	}
-	ADDLOG_DEBUG(LOG_FEATURE_CFG, "BL602_SaveFlashVars: saved %d bytes", dataLen);
-    return dataLen;
+	ADDLOG_DEBUG(LOG_FEATURE_CFG, "%s: saved %d b", __func__, dataLen);
+	return dataLen;
 }
 
-
-void HAL_FlashVars_SaveBootComplete(){
-	g_bootCounts.boot_success_count = g_bootCounts.boot_count;
-	// save after set
-	BL602_SaveFlashVars(&g_bootCounts,sizeof(g_bootCounts));
+// call at startup
+void HAL_FlashVars_IncreaseBootCount()
+{
+	memset(&flash_vars, 0, sizeof(flash_vars));
+	if(g_loaded == 0)
+	{
+		ReadFlashVars(&flash_vars, sizeof(flash_vars));
+	}
+	flash_vars.boot_count++;
+	SaveFlashVars(&flash_vars, sizeof(flash_vars));
 }
 
-int HAL_FlashVars_GetBootCount(){
-	return g_bootCounts.boot_count;
-}
-int HAL_FlashVars_GetBootFailures(){
-    int diff = 0;
-    diff = g_bootCounts.boot_count - g_bootCounts.boot_success_count;
-    return diff;
-}
-void HAL_FlashVars_IncreaseBootCount(){
-	// defaults - in case read fails
-	memset(&g_bootCounts,0,sizeof(g_bootCounts));
-	// read saved
-	BL602_ReadFlashVars(&g_bootCounts,sizeof(g_bootCounts));
-	g_bootCounts.boot_count++;
-	// save after increase
-	BL602_SaveFlashVars(&g_bootCounts,sizeof(g_bootCounts));
-}
-
-
-void HAL_FlashVars_SaveChannel(int index, int value) {
-	if(index<0||index>=BL602_SAVED_CHANNELS_MAX)
+void HAL_FlashVars_SaveChannel(int index, int value)
+{
+	if(index < 0 || index >= MAX_RETAIN_CHANNELS)
 		return;
-	if(g_loaded==0) {
-		BL602_ReadFlashVars(&g_bootCounts,sizeof(g_bootCounts));
+	if(g_loaded == 0)
+	{
+		ReadFlashVars(&flash_vars, sizeof(flash_vars));
 	}
-	g_bootCounts.channelStates[index] = value;
+	flash_vars.savedValues[index] = value;
 	// save after increase
-	BL602_SaveFlashVars(&g_bootCounts,sizeof(g_bootCounts));
-}
-void HAL_FlashVars_SaveLED(byte mode, short brightness, short temperature, byte r, byte g, byte b, byte bEnableAll) {
-
-}
-void HAL_FlashVars_ReadLED(byte *mode, short *brightness, short *temperature, byte *rgb, byte *bEnableAll) {
-
+	SaveFlashVars(&flash_vars, sizeof(flash_vars));
 }
 
-int HAL_FlashVars_GetChannelValue(int ch) {
-	if(ch<0||ch>=BL602_SAVED_CHANNELS_MAX)
-		return 0;
-	if(g_loaded==0) {
-		BL602_ReadFlashVars(&g_bootCounts,sizeof(g_bootCounts));
+void HAL_FlashVars_ReadLED(byte* mode, short* brightness, short* temperature, byte* rgb, byte* bEnableAll)
+{
+	if(g_loaded == 0)
+	{
+		ReadFlashVars(&flash_vars, sizeof(flash_vars));
 	}
-	return g_bootCounts.channelStates[ch];
+	*bEnableAll = flash_vars.savedValues[MAX_RETAIN_CHANNELS - 4];
+	*mode = flash_vars.savedValues[MAX_RETAIN_CHANNELS - 3];
+	*temperature = flash_vars.savedValues[MAX_RETAIN_CHANNELS - 2];
+	*brightness = flash_vars.savedValues[MAX_RETAIN_CHANNELS - 1];
+	rgb[0] = flash_vars.rgb[0];
+	rgb[1] = flash_vars.rgb[1];
+	rgb[2] = flash_vars.rgb[2];
 }
 
-int HAL_GetEnergyMeterStatus(ENERGY_METERING_DATA *data)
+void HAL_FlashVars_SaveLED(byte mode, short brightness, short temperature, byte r, byte g, byte b, byte bEnableAll)
 {
-    if (data != NULL)
-    {
-        if (g_loaded==0) 
-        {
-            BL602_ReadFlashVars(&g_bootCounts,sizeof(g_bootCounts));
-        }
-        memcpy(data, &g_bootCounts.emetering, sizeof(ENERGY_METERING_DATA));
-    }
-    return 0;
+	int iChangesCount = 0;
+
+	if(g_loaded == 0)
+	{
+		ReadFlashVars(&flash_vars, sizeof(flash_vars));
+	}
+
+	SAVE_CHANGE_IF_REQUIRED_AND_COUNT(flash_vars.savedValues[MAX_RETAIN_CHANNELS - 1], brightness, iChangesCount);
+	SAVE_CHANGE_IF_REQUIRED_AND_COUNT(flash_vars.savedValues[MAX_RETAIN_CHANNELS - 2], temperature, iChangesCount);
+	SAVE_CHANGE_IF_REQUIRED_AND_COUNT(flash_vars.savedValues[MAX_RETAIN_CHANNELS - 3], mode, iChangesCount);
+	SAVE_CHANGE_IF_REQUIRED_AND_COUNT(flash_vars.savedValues[MAX_RETAIN_CHANNELS - 4], bEnableAll, iChangesCount);
+	SAVE_CHANGE_IF_REQUIRED_AND_COUNT(flash_vars.rgb[0], r, iChangesCount);
+	SAVE_CHANGE_IF_REQUIRED_AND_COUNT(flash_vars.rgb[1], g, iChangesCount);
+	SAVE_CHANGE_IF_REQUIRED_AND_COUNT(flash_vars.rgb[2], b, iChangesCount);
+
+	if(iChangesCount > 0)
+	{
+		SaveFlashVars(&flash_vars, sizeof(flash_vars));
+	}
 }
 
-int HAL_SetEnergyMeterStatus(ENERGY_METERING_DATA *data)
+short HAL_FlashVars_ReadUsage()
 {
-    if (data != NULL)
-    {
-        memcpy(&g_bootCounts.emetering, data, sizeof(ENERGY_METERING_DATA));
-        BL602_SaveFlashVars(&g_bootCounts,sizeof(g_bootCounts));
-    }
-    return 0;
+	return flash_vars.savedValues[MAX_RETAIN_CHANNELS - 1];
+}
+
+void HAL_FlashVars_SaveTotalUsage(short usage)
+{
+	flash_vars.savedValues[MAX_RETAIN_CHANNELS - 1] = usage;
+}
+
+// call once started (>30s?)
+void HAL_FlashVars_SaveBootComplete()
+{
+	flash_vars.boot_success_count = flash_vars.boot_count;
+	SaveFlashVars(&flash_vars, sizeof(flash_vars));
+}
+
+// call to return the number of boots since a HAL_FlashVars_SaveBootComplete
+int HAL_FlashVars_GetBootFailures()
+{
+	int diff = 0;
+	diff = flash_vars.boot_count - flash_vars.boot_success_count;
+	return diff;
+}
+
+int HAL_FlashVars_GetBootCount()
+{
+	return flash_vars.boot_count;
+}
+
+int HAL_FlashVars_GetChannelValue(int ch)
+{
+	if(ch < 0 || ch >= MAX_RETAIN_CHANNELS)
+		return 0;
+	if(g_loaded == 0)
+	{
+		ReadFlashVars(&flash_vars, sizeof(flash_vars));
+	}
+	return flash_vars.savedValues[ch];
+}
+
+int HAL_GetEnergyMeterStatus(ENERGY_METERING_DATA* data)
+{
+	if(data != NULL)
+	{
+		if(g_loaded == 0)
+		{
+			ReadFlashVars(&flash_vars, sizeof(flash_vars));
+		}
+		memcpy(data, &flash_vars.emetering, sizeof(ENERGY_METERING_DATA));
+	}
+	return 0;
+}
+
+int HAL_SetEnergyMeterStatus(ENERGY_METERING_DATA* data)
+{
+	if(data != NULL)
+	{
+		memcpy(&flash_vars.emetering, data, sizeof(ENERGY_METERING_DATA));
+		SaveFlashVars(&flash_vars, sizeof(flash_vars));
+	}
+	return 0;
 }
 
 void HAL_FlashVars_SaveTotalConsumption(float total_consumption)
 {
-    g_bootCounts.emetering.TotalConsumption = total_consumption;
+	flash_vars.emetering.TotalConsumption = total_consumption;
 }
 
-#endif // PLATFORM_BL602
+//#ifdef ENABLE_DRIVER_HLW8112SPI
+void HAL_FlashVars_SaveEnergy(ENERGY_DATA** data, int channel_count)
+{
+	if(data != NULL)
+	{
+		for(int i = 0; i < channel_count; i++)
+		{
+			char buffer[20];
+			sprintf(buffer, "HLW_CH%i", i);
+			ef_set_env_blob(buffer, data[i], sizeof(ENERGY_DATA));
+		}
+	}
+}
+void HAL_FlashVars_GetEnergy(ENERGY_DATA* data, ENERGY_CHANNEL channel)
+{
+	if(data != NULL)
+	{
+		char buffer[20];
+		sprintf(buffer, "HLW_CH%i", channel);
+		ef_get_env_blob(buffer, data, sizeof(ENERGY_DATA), NULL);
+	}
+}
+//#endif
 
+#endif
