a775cc32995f027b848abd42ff4b4ba93877ac44
Ben Knight <bkk@nerdboy.net.au>
2026-04-04 01:28:54 +0000

    Add drv_pulseclock

--- src/driver/drv_pulseclock.c
+++ src/driver/drv_pulseclock.c
@@ -112,7 +112,7 @@ void PulseClock_onEverySec() {
         
         addLogAdv(LOG_INFO, LOG_FEATURE_DRV, "PulseClock: diff=%i, diffnormal=%i", diff, DaysecNormalise(diff));
 
-        if (( (diff > 0) || (DaysecNormalise(diff) < phys_maxsec/2) )
+        if (( (diff > 0) && (DaysecNormalise(diff) < phys_maxsec/2) )
            && PulseClock_CanRev() )
         {
             if ((phys_daysec / phys_resolution) % 2)
