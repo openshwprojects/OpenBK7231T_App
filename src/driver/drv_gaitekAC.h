1616265439d665e745158cfa997fa77e352e7114
Dylan <70382811+Ripthulhu@users.noreply.github.com>
2026-04-27 22:19:54 +0200

    Add GaitekAC driver header with function declarations

---
+++ src/driver/drv_gaitekAC.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void GaitekAC_Init(void);
+void GaitekAC_Shutdown(void);
+void GaitekAC_RunQuickTick(void);
+void GaitekAC_RunEverySecond(void);
+void GaitekAC_OnChannelChanged(int ch, int val);
