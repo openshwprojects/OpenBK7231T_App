91a0fd2ef0b336ebf41850dc1baa9513af8809e4
MaxineMuster <146550015+MaxineMuster@users.noreply.github.com>
2026-06-02 14:11:56 +0200

    Log start2 lfs (#2105)
    * Add logging of start to LFS
    Add some more comments to reflect actual offstes inside mainConfig_t
    Add two conditional "short"s to make (implicit) padding visible inside mainConfig_t
    
    For testing enable LOG2LFS for all platforms with LFS
    
    * Two more #if to disable vars if LOG2LFS is not enabled
    Disable feature ENABLE_LOG2LFS by default

--- src/new_cfg.c
+++ src/new_cfg.c
@@ -789,6 +789,16 @@ uint32_t CFG_GetLFS_Size() {
 	}
 	return size;
 }
+// time to write log messages to lfs (0=disabled)
+uint8_t CFG_Get_log2lfs() {
+	return g_cfg.log2lfs;
+}
+void CFG_Set_log2lfs(uint8_t value) {
+	if(g_cfg.log2lfs != value) {
+		g_cfg.log2lfs = value;
+		g_cfg_pendingChanges++;
+	}
+}
 #endif
 
 #if MQTT_USE_TLS
